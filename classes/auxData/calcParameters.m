5ae1ad948d315a71581ded753350adc321738e55
pabloi <pabloi@users.noreply.github.com>
2014-07-03 13:27:35 -0400

    Fixed bug in calcParams

--- classes/auxData/calcParameters.m
+++ classes/auxData/calcParameters.m
@@ -144,10 +144,10 @@ end
 lastFTOtime=eventsTime(find(FTO,1,'last'));
 lastFHS=(find(FHS,1,'last'));
 lastSTO=(find(STO,1,'last'));
-lastSHStime=eventsTime(find(SHS,1,'last'));
+lastSHStime=eventsTime(find(SHS,1,'last'));svn up
 
 
-if ~isempty(lastFTOtime) && ~isempty(lastFHStime) && ~isempty(lastSTOtime) && ~isempty(SHS) 
+if ~isempty(lastFTOtime) && ~isempty(lastFHS) && ~isempty(lastSTO) && ~isempty(SHS) 
     Nstrides=sum(SHS(eventsTime<lastFTOtime))-1;
     inds=find(SHS(eventsTime<lastFTOtime));
     while inds(Nstrides)>lastFHS || inds(Nstrides)>lastSTO  %to avoid index errors later on
