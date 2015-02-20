fc7bda6d26d61202c7cf341f909458fe314797cc
dmariscal1 <mariscaldulce@gmail.com>
2015-02-20 12:04:20 -0500

    Changed event detection from FORCES!
    Previous processing might have been throwing out the first step. Now
    there is a higher chance of bad event detection at the beginning, but
    that should be detected when computing the parameter 'good'.

--- fun/eventExtraction/getEventsFromForces.m
+++ fun/eventExtraction/getEventsFromForces.m
@@ -10,21 +10,25 @@ function [LHS,RHS,LTO,RTO]= getEventsFromForces(FzL,FzR,fsample)
 [LHS,RHS,LTO,RTO] = getEventsFromStance(stanceL,stanceR);
 
 %% Eliminate any events that ocurr prior to actual activity in the trial
-Rheel=FzR;
-Lheel=FzL;
-aux=Rheel(:,1)-Rheel(1,1); %Zero initial value
-aux2=cumsum(aux.^2); %Cumulative energy 
-begin_index1=find(aux2>.001*aux2(end),1); %First element whose cumulative energy is at least .1% of total energy
-aux=Lheel(:,1)-Lheel(1,1); %Zero initial value
-aux2=cumsum(aux.^2); %Cumulative energy 
-begin_index2=find(aux2>.001*aux2(end),1); %First element whose cumulative energy is at least .1% of total energy
+% This section was commented out by Pablo on 2/20/2015 because it lead to
+% the very first step not being properly detected, which can be a big
+% problem.
 
-begin_index=max([begin_index1,begin_index2]);
-
-LHS(1:begin_index)=false;
-RHS(1:begin_index)=false;
-LTO(1:begin_index)=false;
-RTO(1:begin_index)=false;
+% Rheel=FzR;
+% Lheel=FzL;
+% aux=Rheel(:,1)-Rheel(1,1); %Zero initial value
+% aux2=cumsum(aux.^2); %Cumulative energy 
+% begin_index1=find(aux2>.001*aux2(end),1); %First element whose cumulative energy is at least .1% of total energy
+% aux=Lheel(:,1)-Lheel(1,1); %Zero initial value
+% aux2=cumsum(aux.^2); %Cumulative energy 
+% begin_index2=find(aux2>.001*aux2(end),1); %First element whose cumulative energy is at least .1% of total energy
+% 
+% begin_index=max([begin_index1,begin_index2]);
+% 
+% LHS(1:begin_index)=false;
+% RHS(1:begin_index)=false;
+% LTO(1:begin_index)=false;
+% RTO(1:begin_index)=false;
 
 %% Check consistency
 %[consistent] = checkEventConsistency(LHS,RHS,LTO,RTO);
