66e184a83ce2bf4690de5dbccb082f44a3254aa5
pabloi <pabloi@users.noreply.github.com>
2015-04-04 09:51:25 -0400

    Reverted calcParams to older version that should work for everyone

--- fun/parameterCalculation/calcParameters.m
+++ fun/parameterCalculation/calcParameters.m
@@ -1,6 +1,6 @@
 function out = calcParameters(trialData,subData)
-%out = calcParametersNew(trialData,subData); %Calling new function
-out = calcParametersNew_test(trialData,subData); %Calling new function
+out = calcParametersNew(trialData,subData); %Calling new function
+%out = calcParametersNew_test(trialData,subData); %Calling new function
 
 %Uncomment this line to compute the old way:
 %out = calcParameters_legacy(trialData,subData);
