60e01e962c9864f01c8f5d656af32309f5cc2536
pabloi <pabloi@users.noreply.github.com>
2015-04-28 14:19:31 -0400

    Changed the save lines in ReviewEventsGUI with clearer code

--- eventReview/ReviewEventsGUI.m
+++ eventReview/ReviewEventsGUI.m
@@ -784,7 +784,9 @@ drawnow
 
 %Write to disk
 eval([handles.varName '=expData;']); %Assigning same var name
-eval(['save(''' handles.Dir handles.filename ''',''' handles.varName ''');']); %Saving with same var name
+%eval(['save(''' handles.Dir handles.filename ''',''' handles.varName
+%''');']); %PI: replaced this with the line below on 28/4/2015
+save([handles.Dir handles.filename],handles.varName);%Saving with same var name
 handles.changed=false;
 
 %re-create adaptation parameters object
@@ -847,7 +849,8 @@ if handles.changed
     
     %write to disk
     eval([handles.varName '=expData;']); %Assigning same var name
-    eval(['save(''' handles.Dir handles.filename ''',''' handles.varName ''');']); %Saving with same var name
+    %eval(['save(''' handles.Dir handles.filename ''',''' handles.varName ''');']); %PI: replaced this with the line below on 28/4/2015
+    save([handles.Dir handles.filename],handles.varName);%Saving with same var name
     %re-create adaptation parameters object
     adaptData=expData.makeDataObj([handles.Dir handles.filename(1:end-4)]);
     %eval(['save(''' handles.Dir handles.filename(1:end-4) 'params' ''',''adaptData'');']); %Saving with same var name
