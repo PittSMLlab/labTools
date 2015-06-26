ea593008f4f642ca2fb11485d1fdc5c1ebf19eaf
pabloi <pabloi@users.noreply.github.com>
2015-06-26 13:34:54 -0400

    Added a mode input argument to barGroups to plot things in an errorbar style, instead of bar plot

--- fun/misc/hmhFun/barGroups.m
+++ fun/misc/hmhFun/barGroups.m
@@ -1,8 +1,11 @@
-function barGroups(Study,results,groups,params,epochs,indivFlag,colorOrder)
+function barGroups(Study,results,groups,params,epochs,indivFlag,colorOrder,mode)
 %Make a bar plot to compare groups for a given epoch and parameter
 %   TO DO: make function be able to accept a group array that is different
 %   thand the groups in the results matrix
 
+if nargin<8 || isempty(mode)
+    mode=1;
+end
 if nargin<7 || isempty(colorOrder) || size(colorOrder,2)~=3    
     poster_colors;
     colorOrder=[p_red; p_orange; p_fade_green; p_fade_blue; p_plum; p_green; p_blue; p_fade_red; p_lime; p_yellow; p_gray; p_black;[1 1 1]];         
@@ -31,18 +34,28 @@ for p=1:length(params)
            abrevGroups{b}=[group(1) abrevGroup];
                       
            ind=find(strcmp(fields(Study),groups{b}));
-           if nargin>5 && indivFlag
-               bar(b,results.(epochs{t}).avg(b,p),'facecolor',greyOrder(ind,:));
-               for s=1:nSubs
-                   aux=results.(epochs{t}).indiv.(params{p});
-                   aux=aux(aux(:,1)==b,2);                   
-                   plot(b,aux(s),'*','Color',colorOrder(s,:))                 
-               end
-           else
-               bar(b,results.(epochs{t}).avg(b,p),'facecolor',colorOrder(ind,:));
+           switch mode
+               case 1
+                   if nargin>5 && indivFlag
+                       bar(b,results.(epochs{t}).avg(b,p),'facecolor',greyOrder(ind,:));
+                       for s=1:nSubs
+                           aux=results.(epochs{t}).indiv.(params{p});
+                           aux=aux(aux(:,1)==b,2);                   
+                           plot(b,aux(s),'*','Color',colorOrder(s,:))                 
+                       end
+                   else
+                       bar(b,results.(epochs{t}).avg(b,p),'facecolor',colorOrder(ind,:));
+                   end
+               case 2
+                   %nop
            end
        end
-       errorbar(results.(epochs{t}).avg(:,p),results.(epochs{t}).se(:,p),'.','LineWidth',2,'Color','k')
+       switch mode
+           case 1
+                errorbar(results.(epochs{t}).avg(:,p),results.(epochs{t}).se(:,p),'.','LineWidth',2,'Color','k')
+           case 2
+               errorbar(results.(epochs{t}).avg(:,p),results.(epochs{t}).se(:,p),'LineWidth',2,'Color','k')
+       end
        set(gca,'Xtick',1:ngroups,'XTickLabel',abrevGroups,'fontSize',12)
        axis tight
        limy=[limy get(gca,'Ylim')];
