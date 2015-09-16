09119f5ce356087c6654074bcf6a1f08127351c2
willpower2727 <willpower2727@yahoo.com>
2015-09-16 14:24:49 -0400

    hgjghjgjh

--- classes/Biofeedback/BiofeedbackSL.m
+++ classes/Biofeedback/BiofeedbackSL.m
@@ -36,6 +36,8 @@ classdef BiofeedbackSL
     %                      category, input num is the # of trials, or rows in the list
     %
     %   saveit() -- saves the instance with filename equal to the ID
+    %
+    %   comparedays() -- makes bar plots comparing day1 and day2
 
     
     properties
@@ -320,7 +322,6 @@ classdef BiofeedbackSL
                    for z=1:length(rhits)
                        temp = rhits{z};
                        temp(abs(temp) > 0.1) = [];
-                       meanrhits0(z) = nanmean(temp);
                        meanrhits1(z) = mean(temp(1:3));
                        stdrhits1(z) = std(temp(1:3));
                        meanrhits2(z) = mean(temp(end-2:end));
@@ -329,7 +330,6 @@ classdef BiofeedbackSL
                    for z=1:length(lhits)
                        temp = lhits{z};
                        temp(abs(temp) > 0.1) = [];
-                       meanlhits0(z) = nanmean(temp);
                        meanlhits1(z) = mean(temp(1:3));
                        stdlhits1(z) = std(temp(1:3));
                        meanlhits2(z) = mean(temp(end-2:end));
@@ -390,22 +390,9 @@ classdef BiofeedbackSL
                    ylim([-0.25 0.25]);
                    title('Slow Leg Errors')
                    ylabel('Error (m)');
-                   
-                   figure(7)
-                   hold on
-                   fill([0 2*length(train)+0.5 2*length(train)+0.5 0],[0.255 0.255 -0.255 -0.255],[230 230 230]./256);
-                   fill([2*length(train)+0.5+2*length(base) 2*length(train)+2*length(base)+2*length(adapt)+0.5 2*length(train)+2*length(base)+2*length(adapt)+0.5 2*length(train)+0.5+2*length(base)],[0.255 0.255 -0.255 -0.255],[230 230 230]./256);
-                   for z = 1:length(meanrhits0)
-                       bar(meanrhits0(z),0.5);
-                   end
-                   
-                   
+                                      
                    %save rhits and lhits
                    save('Results','rhits','lhits');
-                   
-                   
-                   
-                   
                    
                end
             end
@@ -486,11 +473,162 @@ classdef BiofeedbackSL
             if isempty(this.subjectcode)
                 cprintf('err','WARNING: save failed, no valid ID present');
             else
-                savename = [this.subjectcode '_SLBF.mat'];
+                savename = [this.subjectcode '_SLBF_day' num2str(this.day) '.mat'];
                 eval([this.subjectcode '=this;']);
                 save(savename,this.subjectcode);
             end
         end
+        
+        function []=comparedays(this)
+            %make bar plots of day1 and day2 means to compare performances
+            %no inputs required, data is located from a uitable
+            %
+            %creates figures of bar plots for fast and slow legs, day 1 and
+            %day 2
+            
+            %use a uitable to assign the locations of each Results.mat
+            %file, should be 4 of them, 2 per day
+
+            [file1,path1] = uigetfile('*.*','Select Day 1 File');
+            [file2,path2] = uigetfile('*.*','Select Day 2 File');
+            
+            %deal with day 1
+            load([path1,'Results.mat']);
+            tlist1 = this.triallist;% must be day1 instance loaded as this
+            if strcmp(this.dominantleg,'R')
+                fast1 = rhits;
+                slow1 = lhits;
+            else
+                fast1 = lhits;
+                slow1 = rhits;
+            end
+            
+            clear rhits lhits
+            
+            load([path2,'Results.mat']);
+            load([path2,file2]);%make sure this is the day2 instance otherwise will probably crash
+            file3 = [file2(1:6) file2(end-8:end-4)];
+            eval(['tlist2 = ' file3 '.triallist;']);
+            if strcmp(this.dominantleg,'R')
+                fast2 = rhits;
+                slow2 = lhits;
+            else
+                fast2 = lhits;
+                slow2 = rhits;
+            end
+            clear rhits lhits
+            
+            if length(tlist1)==length(tlist2)
+                train = find(strcmp(tlist1(:,4),'training'));%logicals of where training trials are
+                base = find(strcmp(tlist1(:,4),'baseline'));
+                adapt = find(strcmp(tlist1(:,4),'adaptation'));
+                wash = find(strcmp(tlist1(:,4),'washout'));
+                
+                colors1 = cell(length(tlist1),1);
+                colors2 = cell(length(tlist1),1);
+                for z=1:length(tlist1)
+                   if strcmp('train',tlist1(z,3))
+                       colors1{z}=[0 102/256 204/256];
+                       colors2{z}=[0 1 1];
+                   else
+                       colors1{z}=[1 0 0];
+                       colors2{z}=[1 128/256 0];
+                   end
+                end
+                
+                for z=1:length(fast1)
+                    temp1 = fast1{z};
+                    temp2 = fast2{z};
+                    temp1(abs(temp1) > 0.1) = [];
+                    temp2(abs(temp2) > 0.1) = [];
+                    meanfast1(z) = mean(temp1);
+                    meanfast2(z) = mean(temp2);
+                    stdfast1(z) = std(temp1);
+                    stdfast2(z) = std(temp2);
+                end
+                
+                for z=1:length(slow1)
+                    temp1 = slow1{z};
+                    temp2 = slow2{z};
+                    temp1(abs(temp1) > 0.1) = [];
+                    temp2(abs(temp2) > 0.1) = [];
+                    meanslow1(z) = mean(temp1);
+                    meanslow2(z) = mean(temp2);
+                    stdslow1(z) = std(temp1);
+                    stdslow2(z) = std(temp2);
+                end
+                
+                hand = zeros(4,1);
+                %fast leg
+                figure(7)
+                hold on
+                fill([0 2*length(train)+0.5 2*length(train)+0.5 0],[0.255 0.255 -0.255 -0.255],[230 230 230]./256);
+                fill([2*length(train)+0.5+2*length(base) 2*length(train)+2*length(base)+2*length(adapt)+0.5 2*length(train)+2*length(base)+2*length(adapt)+0.5 2*length(train)+0.5+2*length(base)],[0.255 0.255 -0.255 -0.255],[230 230 230]./256);
+                h = 1:2:2*length(meanfast1);
+                h2 = 1.5:2:2*length(meanfast2);
+                errorbar(h,meanfast1,stdfast1,'k','LineWidth',1.5);
+                errorbar(h2,meanfast2,stdfast2,'k','LineWidth',1.5);
+                plot([0 2*length(meanfast1)+1],[0.0375 0.0375],'--k','LineWidth',2);%tolerance lines
+                plot([0 2*length(meanfast1)+1],[-0.0375 -0.0375],'--k','LineWidth',2);
+                for z = 1:2:length(h)%trainings
+                    figure(7)
+                    hand(1) = bar(h(z),meanfast1(z),0.5,'FaceColor',colors1{z});
+                end
+                for z = 2:2:length(h)%evaluations
+                    figure(7)
+                    hand(2) = bar(h(z),meanfast1(z),0.5,'FaceColor',colors1{z});
+                end
+                for z=1:2:length(h2)
+                    figure(7)
+                    hand(3) = bar(h2(z),meanfast2(z),0.5,'FaceColor',colors2{z});
+                end
+                for z=2:2:length(h2)
+                    figure(7)
+                    hand(4) = bar(h2(z),meanfast2(z),0.5,'FaceColor',colors2{z});
+                end
+                ylim([-0.25 0.25]);
+                title([this.subjectcode ' Fast Leg Day 1 vs. Day 2 Errors']);
+                ylabel('Error (m)');
+                legend(hand,'train D1','eval D1','train D2','eval D2')
+%                 keyboard
+
+                figure(8)
+                hold on
+                fill([0 2*length(train)+0.5 2*length(train)+0.5 0],[0.255 0.255 -0.255 -0.255],[230 230 230]./256);
+                fill([2*length(train)+0.5+2*length(base) 2*length(train)+2*length(base)+2*length(adapt)+0.5 2*length(train)+2*length(base)+2*length(adapt)+0.5 2*length(train)+0.5+2*length(base)],[0.255 0.255 -0.255 -0.255],[230 230 230]./256);
+                h = 1:2:2*length(meanslow1);
+                h2 = 1.5:2:2*length(meanslow2);
+                errorbar(h,meanslow1,stdslow1,'k','LineWidth',1.5);
+                errorbar(h2,meanslow2,stdslow2,'k','LineWidth',1.5);
+                plot([0 2*length(meanslow1)+1],[0.0375 0.0375],'--k','LineWidth',2);%tolerance lines
+                plot([0 2*length(meanslow1)+1],[-0.0375 -0.0375],'--k','LineWidth',2);
+                for z = 1:2:length(h)%trainings
+                    figure(8)
+                    hand(1) = bar(h(z),meanslow1(z),0.5,'FaceColor',colors1{z});
+                end
+                for z = 2:2:length(h)%evaluations
+                    figure(8)
+                    hand(2) = bar(h(z),meanslow1(z),0.5,'FaceColor',colors1{z});
+                end
+                for z=1:2:length(h2)
+                    figure(8)
+                    hand(3) = bar(h2(z),meanslow2(z),0.5,'FaceColor',colors2{z});
+                end
+                for z=2:2:length(h2)
+                    figure(8)
+                    hand(4) = bar(h2(z),meanslow2(z),0.5,'FaceColor',colors2{z});
+                end
+                ylim([-0.25 0.25]);
+                title([this.subjectcode ' Slow Leg Day 1 vs. Day 2 Errors']);
+                ylabel('Error (m)');
+                legend(hand,'train D1','eval D1','train D2','eval D2')
+
+                            end
+            
+%             keyboard
+            
+        end
+            
     end
     
 end
