fea88a46c4112e0423b2f109a276a2a9f492a375
willpower2727 <willpower2727@yahoo.com>
2015-10-27 13:28:11 -0400

    progress on adding python data to labtools

--- Biofeedback/AddPythonData.m
+++ Biofeedback/AddPythonData.m
@@ -14,9 +14,10 @@
 %gait (it doesn't make sense to be doing this if there is no gait).
 %
 %written 10/26/2015 WDA
-
+clear
+clc
 %select processed subject data to add biofeedback info to:
-[notname,LTpathname]=uigetfile('*.mat*','Select Subject File:');%you can pick any of the matlab files SUB.RAW or sub.info or sub.params etc.
+[notname,LTpathname]=uigetfile('.mat','Select Subject File:');%you can pick any of the matlab files SUB.RAW or sub.info or sub.params etc.
 cd(LTpathname);
 %enter the subject code
 LTfilename = inputdlg('Please enter the subject code:','',1,{notname(1:6)});%notname is a guess, make sure to check or this will crash
@@ -33,18 +34,85 @@ pause(0.25)
 close(WB)
 
 
+global t;%variable for uitable later on
+global mdata;
+global condition%get condition names
+condition= adaptData.metaData.conditionName;
+global trialsincond
+trialsincond = adaptData.metaData.trialsInCondition;
 
-
-
-
-%select python file(s) to process
-% [filenames,~] = uigetfiles('*.*','Select filenames');
+% select python file(s) to process
+[filenames,path] = uigetfiles('*.*','Select python files');
 % 
-% if iscell(filenames)
-%     
-%     
-% else
-%     
-% end
-% 
-% end
+if iscell(filenames)
+    %construct a uitable as a makeshift GUI that will help user assign
+    %python files to nexus trials
+    f = figure;
+    mdata = cell(length(filenames),3);
+    mdata(:,1)=filenames;
+    
+    colnames = {'Filename','Nexus Trial #','condition'};
+    columnformat = {'char','numeric',condition};
+
+    t=uitable(f,'Position',[10,10,375,375],'Data',mdata,'ColumnName',colnames,'ColumnFormat',columnformat,'ColumnEditable',[false false true],'CellSelectionCallback',@(src,evnt)set(src,'UserData',evnt.Indices));
+    set(t,'celleditcallback','global condition;global trialsincond;global t;temp = get(t,''Data'');cel=get(t,''UserData'');tcond = temp(cel(1),cel(2));[~,~,ind]=intersect(tcond,condition);temp{cel(1),2}=trialsincond{ind};set(t,''Data'',temp)');
+    set(t,'DeleteFcn','global mdata;mdata = get(t,''Data'');');
+    waitfor(t)%wait until user closes the table to continue
+    
+    
+    WB = waitbar(0,['Processing Trial ' num2str(1)]);
+    
+    %proceed to synchronize trials
+    for z=1:1%length(filenames)
+        
+        waitbar((z-1)/length(filenames),WB,['Processing Trial ' num2str(z)]);
+        %load python file
+        [Pheader,Pdata] = JSONtxt2cell([path{z} filenames{z}]);
+        
+        %get nexus data
+        GRRz=getDataAsVector(expData.data{mdata{z,2}}.GRFData,'RFz');
+        GRLz=getDataAsVector(expData.data{mdata{z,2}}.GRFData,'LFz');
+        
+        %TO DO, enable variable sampling frequency inputs, since some BF is
+        %collected at 120 Hz
+        %downsample from 1000 to 100Hz
+        NexusRlowFreq=resample(GRRz,1,10);
+        NexusLlowFreq=resample(GRLz,1,10);
+        
+        %Creating NaN matrix with the lenght of the data
+        newData=nan((((Pdata(end,1)-Pdata(1,1)))+1),size(Pheader,2));
+        newData2=nan((((Pdata(end,1)-Pdata(1,1)))+1),size(Pheader,2));
+        
+        %Making frames from Pyton start at 1
+        Pdata(:,1)=Pdata(:,1)-Pdata(1,1)+1;
+        Pdata2=unique(Pdata(:,1));
+        
+        %finding unique colums
+        for zz=1:length(Pdata2)
+            [datarows(zz),~]=find(Pdata(:,1)==Pdata2(zz),1,'first');
+        end
+        Pdata=Pdata(datarows,:);
+        
+    end
+    close(WB)
+else
+   
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
