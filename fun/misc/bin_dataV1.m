7c323de76c853a9d753bde6b0403eee695a0a01f
pabloi <itu.pablo@gmail.com>
2016-09-16 19:51:23 -0400

    Fixed bin_dataV1 bug
    bin_dataV1 was used for running averages on removeBiasOG, and it could
    fail if binwidth was larger than number of available strides (but not if
    number of strides was 0!). Now it gives a warning and averages what it
    can.

--- fun/misc/bin_dataV1.m
+++ fun/misc/bin_dataV1.m
@@ -4,14 +4,14 @@ function [xnew, xnewstd]=bin_dataV1(x,binwidth)
 % binwidth indicates the duration of each time bin
 %writen by GTO April 14th 2009
 
-if ~isempty(x)
+if size(x,1)>=binwidth
     if binwidth==1
         xnew=x;
         xnewstd=zeros(size(x,1),size(x,2));
     else
-        bstart=[1:size(x,1)-(binwidth-1)];
-        bend=[bstart+binwidth-1];
-        bend(find(bend>size(x,1)))=size(x,1);
+        bstart=[1:size(x,1)-(binwidth-1)]; %First value to be considered, 
+        bend=[bstart+binwidth-1]; %Last value to be considered in the window for each element
+        bend((bend>size(x,1)))=size(x,1); %This should do nothing. Changed to logical indexing 9/16/16. Pablo
 
         for  l=1:length(bstart)
             t1 = bstart(l);
@@ -29,7 +29,17 @@ if ~isempty(x)
             end
         end
     end
-else
-    xnew=[];
-    xnewstd=[];
+else %Averaging how ever many strides we have even if it is very few
+    warning(['Running average could not use the provided binwidth (' num2str(binwidth) ') because of too little strides (' num2str(size(x,1)) '). Using available strides only.'])
+    xnew=nanmean(x);
+    xnewstd=nanstd(x);
 end
+
+%Alt function:
+%Computes running average along the first dimension of x, but doesn't deal
+%with NaNs
+%if size(x,1)>=binwidth %We can do it!
+%   N=binwidth;
+%   xnew=conv2(x,ones(N,1)/N,'valid');
+%   xnewstd=conv2(x.^2,ones(N,1)/N,'valid') -xnew.^2;
+%end
