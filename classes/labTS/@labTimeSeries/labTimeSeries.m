a7c16555a4eaba947906b3ab1c20508eaa9ba623
Unknown <itu.pablo@gmail.com>
2017-09-11 19:48:53 -0400

    More descriptive errors for labTS.plus

--- classes/labTS/@labTimeSeries/labTimeSeries.m
+++ classes/labTS/@labTimeSeries/labTimeSeries.m
@@ -534,11 +534,17 @@ classdef labTimeSeries  < timeseries
 
         function newThis=plus(this,other)
             M=size(this.Data,2);
+            if size(other.Data,2)~=M
+                error('Inconsistent sizes for sum')
+            end
+            newLabels=cell(size(this.labels));
             for i=1:M
                 newLabels{i}=['(' this.labels{i} ' + ' other.labels{i} ')'];
             end
             if abs(this.Time(1)-other.Time(1))<eps && abs(this.sampPeriod-other.sampPeriod)<eps && length(this.labels)==length(other.labels)
-                newThis=labTimeSeries(this.Data+other.Data,this.Time(1),this.sampPeriod,this.labels);
+                newThis=labTimeSeries(this.Data+other.Data,this.Time(1),this.sampPeriod,newLabels);
+            else
+                error('')
             end
         end
 
