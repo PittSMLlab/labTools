6f490a1f8b56a4f3f331e6d89ef9545b639eff25
Unknown <itu.pablo@gmail.com>
2017-09-19 14:54:50 -0400

    Improved checkerboards example

--- example/TSdiscretizationAndCheckerboards.m
+++ example/TSdiscretizationAndCheckerboards.m
@@ -1,17 +1,24 @@
 %% Load
 mList={'TA','PER','SOL','MG','LG','SEMT','SEMB','BF','VM','VL','RF','ADM','HIP','GLU','TFL'};
+mList=mList(end:-1:1);
 labels=[strcat('R',mList) strcat('L',mList)];
-ts=allEMG{1,4}.getDataAsTS(labels);
+labels1=[strcat('F',mList) strcat('S',mList)];
+ts=allEMG{1,4}.renameLabels(labels1,labels).getDataAsTS(labels);
 e=allEvents{1,4};
 %From here on we assume there is a TS loaded on ts, and events on e
-ts2=allEMG{1,1}.getDataAsTS(labels);
-e2=allEvents{1,1};
+ts2=allEMG{1,3}.renameLabels(labels1,labels).getDataAsTS(labels);
+e2=allEvents{1,3};
 
 %% Discretize timeseries:
 eventLabel={'FHS','STO','SHS','FTO'};
 N=[2,4,2,4];
-[DTS,bad]=ts.discretize(e,eventLabel,N);
-[DTS2,bad]=ts2.discretize(e2,eventLabel,N);
+%[DTS,bad]=ts.discretize(e,eventLabel,N);
+%[DTS2,bad]=ts2.discretize(e2,eventLabel,N);
+
+%% Align instead:
+N=[16,48,16,48]*2;
+[DTS,bad]=ts.align(e,eventLabel,N);
+[DTS2,bad]=ts2.align(e2,eventLabel,N);
 
 %% Plot a checkerboard:
 DTS.plotCheckerboard
