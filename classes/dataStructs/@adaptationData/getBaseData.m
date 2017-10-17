1bf6d9917081e3d393b67a910bc24690216135c3
Unknown <itu.pablo@gmail.com>
2017-10-16 21:01:01 -0400

    Added getBaseData() function to adaptData

---
+++ classes/dataStructs/@adaptationData/getBaseData.m
@@ -0,0 +1,21 @@
+function [data,baseConds,trialTypes]=getBaseData(this,baseConds,labels)
+%Returns data from last 40 strides of baseline, exempting the last 5.
+%If baseConds is not given, it will find conditions that match the string 'base' for each trial type (same as removeBiasV3)
+
+if nargin<2 || isempty(baseConds)
+    [baseConds,trialTypes]=getBaseConditions(this);
+else
+    trialTypes=[]; %Doxy
+end
+if nargin<3
+  labels=[];
+end
+
+numberOfStrides=-40;
+exemptFirst=10;
+exemptLast=5;
+removeBiasFlag=0;
+[data]=getEarlyLateData_v2(this,labels,baseConds,removeBiasFlag,numberOfStrides,exemptLast,exemptFirst);
+
+
+end
