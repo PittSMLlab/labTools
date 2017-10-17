3d7cdb954068c054d35c70c8e1ac4b030fbfce7d
Unknown <itu.pablo@gmail.com>
2017-10-16 20:20:50 -0400

    Improved example & visualization for marker reconstruction

--- example/testMarkerOutlierDetectAndCorrect.m
+++ example/testMarkerOutlierDetectAndCorrect.m
@@ -26,25 +26,59 @@ figure; plot(scores(5,:)'); legend(labels); hold on; plot(scoresD(5,:)');
 %Uncertainty matrix:
 P=.1*eye(N*D);
 
+%To simulate an absolutely missing LANK marker:
+idx=find(strcmp(labels,'LANK'));
+pos2=pos;
+pos2(idx,:,:)=NaN;
+
 %Find new marker positions:
-M1=1000;
+M1=M;
 xMLE=nan(N*D,M1);
 xMLEd=nan(N*D,M1);
 for i=1:M1
-    [xMLE(:,i)] = sk3Denforce(pos(:,:,i),P,m(:),R+1e3*abs(max(R(:)))*[eye(N) eye(N) eye(N)]);
-    [xMLEd(:,i)] = skDistenforce(pos(:,:,i),P,md(:),Rd+1e3*abs(max(Rd(:)))*eye(N));
+    [xMLE(:,i)] = sk3Denforce(pos2(:,:,i),P,m(:),R+1e3*abs(max(R(:)))*[eye(N) eye(N) eye(N)]);
+    [xMLEd(:,i)] = skDistenforce(pos2(:,:,i),P,md(:),Rd+1e3*abs(max(Rd(:)))*eye(N));
 end
 xMLE=reshape(xMLE,N,D,M1);
-xMLEd=reshape(xMLEd,N,D,M1);
+%xMLEd=reshape(xMLEd,N,D,M1);
 %% Show old & new scores:
 [correctedScores] = sk3Ddetect(xMLE,m,R);
-figure; plot(scores'); hold on; plot(correctedScores(5,:),'LineWidth',4);
+figure; plot(scores(5,:)); hold on; plot(correctedScores(5,:),'LineWidth',4);
+
+%% Show old & new positions:
+pos(idx,:,1:10000)=NaN; %This are bad samples
+figure;
+for i=1:3
+    subplot(3,3,3*(i-1)+[1:2])
+    plot(squeeze(xMLE(idx,i,:)))
+    hold on
+    plot(squeeze(pos(idx,i,:)))
+    if i==1
+        title('Reconstruction values')
+    elseif i==3
+        xlabel('Time (s)')
+    end
+    ylabel([('x'+i-1) ' (mm)']);
+    
+    subplot(3,3,3*i)
+    dd=squeeze(xMLE(idx,i,:)-pos(idx,i,:));
+    histogram(dd,[-20:1:20])
+    if i==1
+        title('Histogram of errors')
+    elseif i==3
+        xlabel('Error (mm)')
+    end
+    hold on
+    text(-19,1500,['\mu=' num2str(nanmean(dd),2)])
+    text(-19,1000,['\sigma=' num2str(nanstd(dd),2)])
+    text(-19,500,['m=' num2str(nanmedian(dd),2)])
+end
 
 
 %% Show old & new mean pos (notice change in LANK position!):
-auxPos=mean(pos(:,:,1:M1),3);
-auxNewPos=mean(xMLEd(:,:,1:M1),3);
-figure; plot3(auxPos(:,1),auxPos(:,2),auxPos(:,3),'x'); hold on; plot3(auxNewPos(:,1),auxNewPos(:,2),auxNewPos(:,3),'o');
+auxPos=nanmean(pos(:,:,1:M1),3);
+auxNewPos=nanmean(xMLE(:,:,1:M1),3);
+figure; plot3(auxPos(:,1),auxPos(:,2),auxPos(:,3),'x'); hold on; plot3(auxNewPos(:,1),auxNewPos(:,2),auxNewPos(:,3),'o'); axis equal
 
 %% Compare
 
