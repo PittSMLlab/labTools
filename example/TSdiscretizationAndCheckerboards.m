a7ca170ed5aefe130b021adb7288eafd9b9cf8cd
Unknown <itu.pablo@gmail.com>
2017-11-07 16:11:26 -0500

    Minor changes to TS discretization and checkerboard examples

--- example/TSdiscretizationAndCheckerboards.m
+++ example/TSdiscretizationAndCheckerboards.m
@@ -1,19 +1,19 @@
-%% Load
+%% Load some data:
 mList={'TA','PER','SOL','MG','LG','SEMT','SEMB','BF','VM','VL','RF','ADM','HIP','GLU','TFL'};
 mList=mList(end:-1:1);
 labels=[strcat('R',mList) strcat('L',mList)];
 labels1=[strcat('F',mList) strcat('S',mList)];
-ts=allEMG{1,4}.renameLabels(labels1,labels).getDataAsTS(labels);
-e=allEvents{1,4};
+ts=expData.data{6}.procEMGData.renameLabels(labels1,labels).getDataAsTS(labels);
+e=expData.data{6}.gaitEvents;
 %From here on we assume there is a TS loaded on ts, and events on e
-ts2=allEMG{1,3}.renameLabels(labels1,labels).getDataAsTS(labels);
-e2=allEvents{1,3};
+ts2=expData.data{4}.procEMGData.renameLabels(labels1,labels).getDataAsTS(labels);
+e2=expData.data{4}.gaitEvents;
 
 %% Discretize timeseries:
-eventLabel={'FHS','STO','SHS','FTO'};
+eventLabel={'LHS','RTO','RHS','LTO'};
 N=[2,4,2,4];
-%[DTS,bad]=ts.discretize(e,eventLabel,N);
-%[DTS2,bad]=ts2.discretize(e2,eventLabel,N);
+[DTS,bad]=ts.discretize(e,eventLabel,N);
+[DTS2,bad]=ts2.discretize(e2,eventLabel,N);
 
 %% Align instead:
 N=[16,48,16,48]*2;
