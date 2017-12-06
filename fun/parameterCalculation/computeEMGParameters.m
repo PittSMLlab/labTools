136a82190a2a523f073c64e9f78c31dafc463488
Unknown <itu.pablo@gmail.com>
2017-12-06 18:05:27 -0500

    Added median()  based EMG parameters

--- fun/parameterCalculation/computeEMGParameters.m
+++ fun/parameterCalculation/computeEMGParameters.m
@@ -23,6 +23,9 @@ warning('on','labTS:renameLabels:dont')
 arrayedEvents=labTimeSeries.getArrayedEvents(gaitEvents,[slowLeg 'HS']);
 [statEMG] = computeTSstatParameters(EMGData,arrayedEvents); %Stat parameters for raw EMG
 [EMG_alt] = computeTSdiscreteParameters(newEMG,gaitEvents,slowLeg);
-out=cat(statEMG,EMG_alt);    
+[EMG_alt2] = computeTSdiscreteParameters(newEMG,gaitEvents,slowLeg,'nanmedian');
+EMG_alt2=EMG_alt2.renameLabels(EMG_alt2.labels,strcat('med',EMG_alt2.labels));
+out=cat(statEMG,EMG_alt);   
+out=cat(out,EMG_alt2);
 end
 
