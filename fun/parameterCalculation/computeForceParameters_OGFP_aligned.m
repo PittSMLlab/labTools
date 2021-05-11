ba5c5b5a386fd02dd6320b0e157da181e95f6355
Dulce Mariscal <37150306+dum5@users.noreply.github.com>
2021-05-11 12:11:27 -0400

    Update computeForceParameters_OGFP_aligned.m
    Remove a extra "if" whne looking for the type of data

--- fun/parameterCalculation/computeForceParameters_OGFP_aligned.m
+++ fun/parameterCalculation/computeForceParameters_OGFP_aligned.m
@@ -182,7 +182,7 @@ else
     fast_frames = 700+endcutting;
 end
 
-if if strcmp(trialData.metaData.type,'OG') || strcmp(trialData.metaData.type,'NIM')
+if strcmp(trialData.metaData.type,'OG') || strcmp(trialData.metaData.type,'NIM')
     filteredSlow_align = FilteredS.align(gaitEvents,{[slowleg 'HS'],[fastleg 'TO'],[fastleg 'HS']},[floor(0.2*slow_frames),floor(0.4*slow_frames),floor(0.4*slow_frames)]);
     filteredSlow_align.Data = filteredSlow_align.Data(1:slow_frames-endcutting,:,:);
     filteredFast_align = FilteredF.align(gaitEvents,{[fastleg 'HS'],[slowleg 'TO'],[slowleg 'HS']},[floor(0.2*fast_frames),floor(0.5*fast_frames),floor(0.3*fast_frames)]);
