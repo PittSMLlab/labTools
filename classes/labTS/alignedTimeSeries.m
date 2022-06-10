49c481c07d855c5667848d65b6adbedd302e6179
DulceMariscal <mariscaldulce@gmail.com>
2022-06-10 09:28:40 -0400

    Update alignedTimeSeries.m

--- classes/labTS/alignedTimeSeries.m
+++ classes/labTS/alignedTimeSeries.m
@@ -599,7 +599,7 @@ classdef alignedTimeSeries %<labTimeSeries %TODO: make this inherit from labTime
         function [this,iC,iI]=getSym(this)
             [this,iC,iI]=this.flipLR; %First, flip the non-aligned side.
             %Then: compute sym/asym data and replace it.
-            this.Data=.5*[this.Data(:,iI)-this.Data(:,iC) this.Data(:,iI)+this.Data(:,iC)];
+            this.Data=[this.Data(:,iI)-this.Data(:,iC) this.Data(:,iI)+this.Data(:,iC)];
             %Update labels:
             this.labels=[regexprep(this.labels(iI),['^' this.labels{iI(1)}(1)],'a') regexprep(this.labels(iI),['^' this.labels{iI(1)}(1)],'b')];
       
