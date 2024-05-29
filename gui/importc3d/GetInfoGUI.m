c019f1f424726d847135520152a6da0245105479
Marcela Gonzalez-Rubio <33523484+MarcelaGonzalezRubio@users.noreply.github.com>
2024-05-29 11:18:22 -0400

    Add fast leg to the GUI (#152)
    * Experiment Description for Weber Faster
    
    * Fix mistake from Perceptual cues
    
    I was saving the start cues as the stop cues
    
    * Add fast leg option on the GUI
    
    Add the pop up menu for fast leg option.

--- gui/importc3d/GetInfoGUI.m
+++ gui/importc3d/GetInfoGUI.m
@@ -5,7 +5,7 @@ function varargout = GetInfoGUI(varargin)
 %
 % See also: importc3d/ExpDetails, errorProofInfo
 
-% Last Modified by GUIDE v2.5 23-Jan-2024 11:56:03
+% Last Modified by GUIDE v2.5 29-May-2024 10:45:34
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -1650,3 +1650,26 @@ end
 % % handles    structure with handles and user data (see GUIDATA)
 % 
 % % Hint: get(hObject,'Value') returns toggle state of perceptualTasks
+
+
+% --- Executes on selection change in popupmenu9.
+function popupmenu9_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu9 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu9
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu9_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
