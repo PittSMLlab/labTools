b922db99c13cf1202d2fe6a1c0468faa9eb60cb0
nwb18 <nwbrantly@pitt.edu>
2024-06-19 11:01:44 -0400

    Update H-reflex amplitude computations
    Update H-reflex amplitude computations to store relevant raw EMG window start and stop indices in an array rather than individual variables, and loop through all EMG windows to reduce code duplication. Move noise amplitude computation into same for loop as other amplitude computations. Update TODO notes and comments for clarity. NWB

--- fun/+Hreflex/computeHreflexAmplitudes.m
+++ fun/+Hreflex/computeHreflexAmplitudes.m
@@ -1,7 +1,8 @@
 function amps = computeHreflexAmplitudes(rawEMG_MG,indsStimArtifact)
 %COMPUTEHREFLEXAMPLITUDES Compute amplitudes of interest from H-reflex
 %   Compute the peak-to-peak amplitudes of the M-wave, H-wave, noise floor,
-% and the background EMG.
+% and the mean absolute value (MAV) and root mean square (RMS) of the
+% background EMG (for possible future normalization).
 %
 % input:
 %   rawEMG_MG: 2 x 1 cell array of number of samples x 1 arrays for right
@@ -16,14 +17,13 @@ function amps = computeHreflexAmplitudes(rawEMG_MG,indsStimArtifact)
 %       (column 1), H-wave (column 2), noise (column 3), background EMG MAV
 %       (column 4), and background EMG RMS (column 5)
 
-% TODO: reject measurements if GRF reveals not in single stance
-% TODO: accept threshold as optional input to overwrite noise threshold
-% threshWaveAmp = 0.00015;    % 0.15 mV peak-to-peak voltage threshold
+% TODO: reject measurements if GRF reveals not in single stance (prefer
+% outside of this function so do not have to pass as input parameter and
+% this function gives more or less the 'raw' amplitudes besides more basic
+% rejection based on the H-reflex waveform itself)
 
-narginchk(2,2); % verify correct number of input arguments
-
-% instantiate and initialize output amplitudes cell array
-amps = cell(2,5);
+narginchk(2,2);     % verify correct number of input arguments
+amps = cell(2,5);   % instantiate output amplitudes cell array
 
 % if both cells are empty arrays for either input argument, ...
 if all(cellfun(@isempty,indsStimArtifact)) || ...
@@ -31,109 +31,95 @@ if all(cellfun(@isempty,indsStimArtifact)) || ...
     warning(['There is data missing necessary to compute the H-reflex ' ...
         'amplitudes.']);
     return;
-end 
+end
+
+numStimR = length(indsStimArtifact{1}); % number of right leg stimuli
+numStimL = length(indsStimArtifact{2}); % number of left leg stimuli
+% initialize output amplitudes arrays
+amps(1,:) = cellfun(@(x) nan(numStimR,1),amps(1,:),'UniformOutput',false);
+amps(2,:) = cellfun(@(x) nan(numStimL,1),amps(2,:),'UniformOutput',false);
 
 % NOTE: should always be same across trials and should be same for forces
 % TODO: make optional input argument
 period = 0.0005; % EMG.sampPeriod;    % sampling period
 % TODO: make indices optional input argument with default option to avoid
 % having these values duplicated in multiple locations
-% M-wave is contained by interval:           4ms -  23ms
+% M-wave is contained by interval:           4ms -  23ms after stim. art.
 % H-wave is contained by interval:          24ms -  43ms
-% Noise is contained by interval:           50ms -  99ms
+% Noise is contained by interval:           50ms -  99ms % TODO: is noise window right?
 % Background EMG is contained by interval: -99ms - -50ms (neg. = before)
-indStartM = 0.004 / period;        % 4 ms after stim artifact in samples
-indEndM = 0.023 / period;          % 23 ms
-indStartH = 0.024 / period;        % 24 ms
-indEndH = 0.043 / period;          % 43 ms
-indStartN = 0.050 / period;        % 50 ms  % TODO: is noise window right?
-indEndN = 0.099 / period;          % 99 ms
-indStartB = -0.099 / period;       % -99 ms
-indEndB = -0.050 / period;         % -50 ms
+indsStart = [0.004 0.024 0.050 -0.099] ./ period;   % convert to samples
+indsEnd = [0.023 0.043 0.099 -0.050] ./ period;
+% NOTE: number of start and end indices MUST be identical
+numWins = length(indsStart);    % number of windows to extract data from
 
-numStimR = length(indsStimArtifact{1});
-numStimL = length(indsStimArtifact{2});
-
-amps(1,:) = cellfun(@(x) nan(numStimR,1),amps(1,:),'UniformOutput',false);
-amps(2,:) = cellfun(@(x) nan(numStimL,1),amps(2,:),'UniformOutput',false);
-
-% TODO: consider using noise mean to compute threshold to set edge case
-% values; otherwise, move the noise computation into same for loops
-for stR = 1:numStimR                % for each right leg stimulus, ...
-    winEMGN = rawEMG_MG{1}((indsStimArtifact{1}(stR)+indStartN): ...
-        (indsStimArtifact{1}(stR)+indEndN));
-    amps{1,3}(stR) = max(winEMGN) - min(winEMGN);
-end
-
-for stL = 1:numStimL                % for each left leg stimulus, ...
-    winEMGN = rawEMG_MG{2}((indsStimArtifact{2}(stL)+indStartN): ...
-        (indsStimArtifact{2}(stL)+indEndN));
-    amps{2,3}(stL) = max(winEMGN) - min(winEMGN);
-end
-
+% TODO: consider using noise mean to compute threshold for edge case values
 % TODO: put into a helper function and use for loop through the number of
 % legs present to reduce code duplication (handle case of only one leg)
-for stR = 1:numStimR                % for each right leg stimulus, ...
-    % extract EMG data for relevant time windows from stim artifact onset
-    winEMGM = rawEMG_MG{1}((indsStimArtifact{1}(stR)+indStartM): ...
-        (indsStimArtifact{1}(stR)+indEndM));
-    winEMGH = rawEMG_MG{1}((indsStimArtifact{1}(stR)+indStartH): ...
-        (indsStimArtifact{1}(stR)+indEndH));
-    winEMGB = rawEMG_MG{1}((indsStimArtifact{1}(stR)+indStartB): ...
-        (indsStimArtifact{1}(stR)+indEndB));
-    [maxM,indMaxM] = max(winEMGM);
-    [minM,indMinM] = min(winEMGM);
-    [maxH,indMaxH] = max(winEMGH);
-    [minH,indMinH] = min(winEMGH);
-    % if min or max index is not within first or last two two samples, ...
-    if ~(any(indMinM == [1 2]) || any(indMaxM == [1 2]) || ...
-            any(indMinM == [length(winEMGM)-1 length(winEMGM)]) || ...
-            any(indMaxM == [length(winEMGM)-1 length(winEMGM)]))
-        amps{1,1}(stR) = maxM - minM;  % compute peak-to-peak voltage
-    else    % otherwise, ...
-        % leave value as NaN (previously set to the noise floor threshold)
-        % ampsMwaveR(stR) = threshWaveAmp;
+for stR = 1:numStimR                    % for each right leg stimulus, ...
+    indStim = indsStimArtifact{1}(stR); % stimulation index
+    for win = 1:numWins                 % for each stim snippet window, ...
+        % extract EMG data for time windows from stim artifact onset
+        winEMG = rawEMG_MG{1}((indStim+indsStart(win)): ...
+            (indStim+indsEnd(win)));
+        [valMax,indMax] = max(winEMG);
+        [valMin,indMin] = min(winEMG);
+        % TODO: add check that M-wave and H-wave are not too broad in lieu
+        % of or in addition to check that min or max are not early or late
+        % in EMG window
+        switch win
+            case {1,2}                  % M-wave or H-wave
+                % if min or max index not w/in 1st or last two samps, ...
+                numPnts = length(winEMG);   % number of points in window
+                if ~(any(indMin == [1 2]) || any(indMax == [1 2]) || ...
+                        any(indMin == [numPnts-1 numPnts]) || ...
+                        any(indMax == [numPnts-1 numPnts]))
+                    % compute peak-to-peak voltage
+                    amps{1,win}(stR) = valMax - valMin;
+                else                    % otherwise, ...
+                    % leave as NaN (previously set to noise floor thresh.)
+                end
+            case 3                      % noise window
+                amps{1,win}(stR) = valMax - valMin;
+            case 4                      % background EMG window
+                % compute mean absolute value (MAV) of background EMG wind.
+                amps{1,win}(stR) = mean(abs(winEMG));
+                % compute root mean square (RMS) of background EMG window
+                amps{1,win+1}(stR) = sqrt(mean(winEMG.^2));
+            otherwise
+                % not possible to enter this statement
+        end
     end
-    if ~(any(indMinH == [1 2]) || any(indMaxH == [1 2]) || ...
-            any(indMinH == [length(winEMGH)-1 length(winEMGH)]) || ...
-            any(indMaxH == [length(winEMGH)-1 length(winEMGH)]))
-        amps{1,2}(stR) = maxH - minH;
-    else
-        % ampsHwaveR(stR) = threshWaveAmp;
-    end
-    % compute mean absolute value (MAV) of background EMG window
-    amps{1,4}(stR) = mean(abs(winEMGB));
-    % compute root mean square (RM) of background EMG window
-    amps{1,5}(stR) = sqrt(mean(winEMGB.^2));
 end
 
-for stL = 1:numStimL                % for each left leg stimulus, ...
-    winEMGM = rawEMG_MG{2}((indsStimArtifact{2}(stL)+indStartM): ...
-        (indsStimArtifact{2}(stL)+indEndM));
-    winEMGH = rawEMG_MG{2}((indsStimArtifact{2}(stL)+indStartH): ...
-        (indsStimArtifact{2}(stL)+indEndH));
-    winEMGB = rawEMG_MG{2}((indsStimArtifact{2}(stL)+indStartB): ...
-        (indsStimArtifact{2}(stL)+indEndB));
-    [maxM,indMaxM] = max(winEMGM);
-    [minM,indMinM] = min(winEMGM);
-    [maxH,indMaxH] = max(winEMGH);
-    [minH,indMinH] = min(winEMGH);
-    if ~(any(indMinM == [1 2]) || any(indMaxM == [1 2]) || ...
-            any(indMinM == [length(winEMGM)-1 length(winEMGM)]) || ...
-            any(indMaxM == [length(winEMGM)-1 length(winEMGM)]))
-        amps{2,1}(stL) = maxM - minM;
-    else
-        % ampsMwaveL(stL) = threshWaveAmp;
+for stL = 1:numStimL                    % for each left leg stimulus, ...
+    indStim = indsStimArtifact{2}(stL); % stimulation index
+    for win = 1:numWins                 % for each stim snippet window, ...
+        winEMG = rawEMG_MG{2}((indStim+indsStart(win)): ...
+            (indStim+indsEnd(win)));    % extract EMG time window data
+        [valMax,indMax] = max(winEMG);
+        [valMin,indMin] = min(winEMG);
+        switch win
+            case {1,2}                  % M-wave or H-wave
+                % if min or max index not w/in 1st or last two samps, ...
+                numPnts = length(winEMG);   % number of points in window
+                if ~(any(indMin == [1 2]) || any(indMax == [1 2]) || ...
+                        any(indMin == [numPnts-1 numPnts]) || ...
+                        any(indMax == [numPnts-1 numPnts]))
+                    % compute peak-to-peak voltage
+                    amps{2,win}(stL) = valMax - valMin;
+                else                    % otherwise, ...
+                    % leave as NaN (previously set to noise floor thresh.)
+                end
+            case 3                      % noise window
+                amps{2,win}(stL) = valMax - valMin;
+            case 4                      % background EMG window
+                amps{2,win}(stL) = mean(abs(winEMG));       % compute MAV
+                amps{2,win+1}(stL) = sqrt(mean(winEMG.^2)); % compute RMS
+            otherwise
+                % not possible to enter this statement
+        end
     end
-    if ~(any(indMinH == [1 2]) || any(indMaxH == [1 2]) || ...
-            any(indMinH == [length(winEMGH)-1 length(winEMGH)]) || ...
-            any(indMaxH == [length(winEMGH)-1 length(winEMGH)]))
-        amps{2,2}(stL) = maxH - minH;
-    else
-        % ampsHwaveL(stL) = threshWaveAmp;
-    end
-    amps{2,4}(stL) = mean(abs(winEMGB));
-    amps{2,5}(stL) = sqrt(mean(winEMGB.^2));
 end
 
 end
