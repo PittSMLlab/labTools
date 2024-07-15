06b6c8a66de08717e1f01b83a17a0cfc6adba94c
Nate Brantly <54079862+nwbrantly@users.noreply.github.com>
2024-07-15 15:26:37 -0400

    Update H-reflex curve script (#171)
    * Update recruitment curve script to call function
    
    Update H-reflex calibration script to call the 'plotCal' function rather than including all of the recruitment curve code directly in the script. Update the 'plotCal' function by adding comments, including both noise lines, and omitting NaN values to ensure the line connects. NWB

--- fun/+Hreflex/plotCal.m
+++ fun/+Hreflex/plotCal.m
@@ -1,49 +1,78 @@
 function fig = plotCal(amplitudesStim,values,yLabel,leg,id, ...
     trialNum,varargin)
-%PLOTCAL Summary of this function goes here
-%   Detailed explanation goes here
+%PLOTCAL Plot either the recruitment or ratio H-reflex calibration curve
+%   Plot the calibration curve (H-wave and M-wave recruitment or H-to-M
+% ratio) for a single leg.
+%
+% input:
+%   amplitudesStim: 1 x number of samples array of stimulation current
+%       intensities (in mA)
+%   values: 2 x 1 or 1 x 1 cell array of number of samples x 1 array(s) of
+%       the H-wave and M-wave amplitudes (in mV) or ratios
+%   yLabel: character array or string of the y-axis label
+%   leg: either 'Right Leg' or 'Left Leg' are the two possible values
+%   id: string or character array of participant / session ID for naming
+%   trialNum: string or character array of the trial number for naming
+%   noise: OPTIONAL input for the background noise level for the
+%       eligibility threshold
+%   path: OPTIONAL input for saving figures (not saved if not provided)
+% output:
+%   fig: handle object to the figure generated
 
 narginchk(6,8); % verify correct number of input arguments
 
 numOptArgs = length(varargin);
 switch numOptArgs
     case 0
-        thresh = nan;   % default to Not-a-Number
+        noise = nan;   % default to Not-a-Number
         path = '';      % default to empty
     case 1  % one optional argument provided
         if isnumeric(varargin{1})   % if a number, ...
-            thresh = varargin{1};   % it is the threshold
+            noise = varargin{1};   % it is the threshold
             path = '';
         else                        % otherwise, ...
             path = varargin{1};     % is is the file saving path
-            thresh = nan;
+            noise = nan;
         end
     case 2  % both optional arguments provided
-        thresh = varargin{1};   % first always threshold
+        noise = varargin{1};   % first always threshold
         path = varargin{2};     % second always file saving path
 end
 
 isRatio = contains(yLabel,'ratio','IgnoreCase',true);
 
 amplitudesStimU = unique(amplitudesStim);
-avgs = arrayfun(@(x) mean(values(amplitudesStim == x)),amplitudesStimU);
-[valMax,indMax] = max(avgs);
-I_max = ampsStimLU(indMax);
+avgs = cellfun(@(x1) arrayfun(@(x2) mean(x1(amplitudesStim == x2), ...
+    'omitmissing'),amplitudesStimU),values,'UniformOutput',false);
+hasVals = cellfun(@(x) ~isnan(x),avgs,'UniformOutput',false);
+% compute Hmax or Ratiomax along with current at which it occurs
+[valMax,indMax] = max(avgs{end});
+I_max = amplitudesStimU(indMax);
+
+% TODO: is it necessary to sort the data?
+% [ampsStimR,indsOrderR] = sort(ampsStimR);
+% ampsHwaveR = ampsHwaveR(indsOrderR);
+% ampsMwaveR = ampsMwaveR(indsOrderR);
 
 fig = figure;
 hold on;
-if ~isnan(thresh)
-    yline(thresh,'r','Threshold');
+if ~isnan(noise)
+    yline(noise,'r--');
+    yline(4*noise,'r','H-Wave V_{pp} Threshold');
 end
 
 if isRatio  % if this is an H:M ratio curve, ...
-    plot(amplitudesStim,values,'ok','MarkerSize',10);   % individual points
-    plot(amplitudesStimU,avgs,'k','LineWidth',2);       % line through means
+    plot(amplitudesStim,values{1},'ok','MarkerSize',10);% individual points
+    plot(amplitudesStimU(hasVals{1}),avgs{1}(hasVals{1}),'k', ...
+        'LineWidth',2);
 else        % otherwise, this is an H- and M-wave recruitment curve
-    plot(amplitudesStim,values{1},'x','Color',[0.5 0.5 0.5],'MarkerSize',10);
-    p1 = plot(amplitudesStimU,avgs{1},'LineWidth',2,'Color',[0.5 0.5 0.5]);
+    plot(amplitudesStim,values{1},'x','Color',[0.5 0.5 0.5], ...
+        'MarkerSize',10);
+    p1 = plot(amplitudesStimU(hasVals{1}),avgs{1}(hasVals{1}), ...
+        'LineWidth',2,'Color',[0.5 0.5 0.5]);
     plot(amplitudesStim,values{2},'ok','MarkerSize',10);
-    p2 = plot(amplitudesStimU,avgs{2},'k','LineWidth',2);
+    p2 = plot(amplitudesStimU(hasVals{2}),avgs{2}(hasVals{2}),'k', ...
+        'LineWidth',2);
     % p3 = plot(xL,yL,'b--','LineWidth',2);
 end
 
@@ -53,7 +82,7 @@ plot([I_max I_max],[0 valMax],'k-.');  % vertical line from I_max to valMax
 % TODO: do not hardcode x offset for label
 % TODO: add handle of title
 if isRatio
-    text(I_max + 0.1,0 + (0.05*max(values)), ...
+    text(I_max + 0.1,0 + (0.05*max(cell2mat(values))), ...
         sprintf('I_{Ratio_{max}} = %.1f mA',I_max));
 else
     text(I_max + 0.1,0 + (0.05*max(cell2mat(values))), ...
@@ -65,7 +94,7 @@ plot([min(amplitudesStim)-1 I_max],[valMax valMax],'k-.');
 % add label to horizontal line (valMax)
 if isRatio
     text(min(amplitudesStim)-1 + 0.1,valMax + ...
-        (0.05*max(values)),sprintf('Ratio_{max} = %.2f',valMax));
+        (0.05*max(cell2mat(values))),sprintf('Ratio_{max} = %.2f',valMax));
 else
     text(min(amplitudesStim)-1 + 0.1,valMax + ...
         (0.05*max(cell2mat(values))),sprintf('H_{max} = %.2f mV',valMax));
@@ -81,13 +110,8 @@ else
     legend([p1 p2],'M-wave','H-wave','Location','best');
     title([id ' - Trial' trialNum ' - ' leg ' - Recruitment Curve']);
 end
-saveas(gcf,[pathFigs id '_HreflexRatioCurve_Trial' trialNum ...
-    '_LeftLeg.png']);
-saveas(gcf,[pathFigs id '_HreflexRatioCurve_Trial' trialNum ...
-    '_LeftLeg.fig']);
 
 % TODO: make figure title and filename optional inputs
-
 if ~isempty(path)   % if figure saving path provided as input argument, ...
     % save figure
     if isRatio
