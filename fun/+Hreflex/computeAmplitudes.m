7c8ccb03b2f8e2bc01c9ba9979a9711b276d3500
nwb18 <nwbrantly@pitt.edu>
2024-10-04 14:59:31 -0400

    Update H-reflex amplitudes computation
    Update comments and 'TODO' notes in 'Hreflex.computeAmplitudes' for clarity. Consolidate start and end indices for the H-reflex snippet windows of interest into a single array, 'indsWindows', rather than two separate arrays, and update helper function call accordingly. Add input argument checks, and handle case of EMG data for only one leg. Rename variables for clarity. NWB

--- fun/+Hreflex/computeAmplitudes.m
+++ fun/+Hreflex/computeAmplitudes.m
@@ -13,26 +13,40 @@ function [amplitudes,durations] = computeAmplitudes(rawEMG_MG, ...
 %       right (cell 1) and left (cell 2) leg stimulus artifact indices for
 %       H-reflex alignment
 % output:
-%   amps: 2 x 5 cell array of number of stimuli x 1 arrays for right (row
-%       1) and left (row 2) leg H-reflex amplitudes: M-wave (column 1),
-%       H-wave (column 2), noise (column 3), background EMG MAV (column 4),
-%       and background EMG RMS (column 5)
-%   durs: 2 x 2 cell array of number of stimuli x 1 arrays for right (row
-%       1) and left (row 2) leg M-wave (column 1) and H-wave (column 2)
-%       durations (i.e., absolute time difference between the minimum and
-%       maximum values) for determining whether a valid wave
+%   amplitudes: 2 x 5 cell array of number of stimuli x 1 arrays for right
+%       (row 1) and left (row 2) leg H-reflex amplitudes: M-wave (column
+%       1), H-wave (column 2), noise (column 3), background EMG MAV (column
+%       4), and background EMG RMS (column 5)
+%   durations: 2 x 2 cell array of number of stimuli x 1 arrays for right
+%       (row 1) and left (row 2) leg M-wave (column 1) and H-wave (column
+%       2) durations (i.e., absolute time difference between the minimum
+%       and maximum values) for determining whether a valid wave
 
-% TODO: Incorporate call to H-reflex snippet extraction here for modularity
-% so that there is a clean linear process of extracting stimulation
-% indices, then snippets, then amplitudes?
-% TODO: reject measurements if GRF reveals not in single stance (prefer
-% outside of this function so do not have to pass as input parameter and
-% this function gives more or less the 'raw' amplitudes besides more basic
-% rejection based on the H-reflex waveform itself)
+% TODO:
+%   1. incorporate call to H-reflex snippet extraction here for modularity
+%   so that there is a clean, linear process of extracting stimulation
+%   indices, then snippets, then amplitudes?
+%   2. reject measurements if GRF reveals not in single stance
+%       NOTE: I prefer to reject measurements (i.e., implement quality
+%       checks) outside of this function so that this function gives more
+%       or less the 'raw' amplitudes besides basic rejection to avoid
+%       passing more input parameters
+%       NOTE: it may be beneficial to store the indices at which the min
+%       and max occur to determine the mode (or median?) samples at which
+%       the wave peak and trough occur to compute values in cases where
+%       errant indices are selected (for getting a value from noisy data)
+%       it may be inappropriate to set the value to NaN when it is merely a
+%       low value comparable to noise floor (more stringent for invalid)
+%   3. make window indices optional input argument with default to avoid
+%   duplication across multiple code locations
+%   4. consider using noise mean to compute threshold for edge case values
+%   5. dynamically set the window bounds (especially the noise window)
+%   based on the H-reflex data (this may allow the noise window to be
+%   broader than it currently is)
 
-narginchk(2,2);     % verify correct number of input arguments
-amplitudes = cell(2,5);   % instantiate output amplitudes cell array
-durations = cell(2,2);   % instantiate output wave durations cell array
+narginchk(2,2);             % verify correct number of input arguments
+amplitudes = cell(2,5);     % instantiate output amplitudes cell array
+durations = cell(2,2);      % instantiate output wave durations cell array
 
 % if both cells are empty arrays for either input argument, ...
 if all(cellfun(@isempty,indsStimArtifact)) || ...
@@ -44,40 +58,25 @@ end
 
 % NOTE: should always be same across trials and should be same for forces
 % TODO: make optional input argument
-period = 0.0005; % EMG.sampPeriod;    % sampling period
-% TODO: update to include the M- and H-wave value by default and only set
-% to NaN if determined to be an invalid measurement by more stringent
-% criteria, store the indices at which the min and max occur to determine
-% the mode (or median?) samples at which the wave peak and trough occur to
-% compute values in the cases when errant points are selected
-
-% TODO: make indices optional input argument with default to avoid
-% duplication across multiple code locations
-% TODO: consider combining into single matrix for simplicity
+period = 0.0005;                                    % sampling period
 % M-wave is contained by interval:           4ms -  23ms after stim. art.
 % H-wave is contained by interval:          24ms -  43ms
 % TODO: is noise window correct? GTO requested between M- & H-waves
 % Noise is contained by interval:           20ms -  25ms
 % Background EMG is contained by interval: -99ms - -50ms (neg. = before)
-indsStart = [0.004 0.024 0.020 -0.099] ./ period;   % convert to samples
-indsEnd = [0.023 0.049 0.025 -0.050] ./ period;
-% NOTE: number of start and end indices MUST be identical
+indsWindows = [0.004 0.024 0.020 -0.099;
+    0.023 0.049 0.025 -0.050] ./ period; % convert to samples
 
-% TODO: consider using noise mean to compute threshold for edge case values
-% TODO: loop through number of legs present to handle case of only one leg
 % compute H-reflex amplitudes for each leg
 [amplitudes(1,:),durations(1,:)] = computeAmpsOneLeg(rawEMG_MG{1}, ...
-    indsStimArtifact{1},indsStart,indsEnd,period);
-[amplitudes(1,:),durations(1,:)] = computeAmpsOneLeg(rawEMG_MG{1}, ...
-    indsStimArtifact{1},indsStart,indsEnd,period);
+    indsStimArtifact{1},indsWindows,period);
 [amplitudes(2,:),durations(2,:)] = computeAmpsOneLeg(rawEMG_MG{2}, ...
-    indsStimArtifact{2},indsStart,indsEnd,period);
+    indsStimArtifact{2},indsWindows,period);
 
 end
 
-function [amps,durs] = computeAmpsOneLeg(rawEMG,indsStimArt, ...
-    indsWinStart,indsWinEnd,per)
-%COMPUTEAMPSONESTIM Computes H-reflex amplitudes for single stimulus (leg)
+function [amps,durs] = computeAmpsOneLeg(rawEMG,indsStimArt,indsWinsRel,per)
+%COMPUTEAMPSONESTIM Computes H-reflex amplitudes for a single leg
 %   This function accepts the raw EMG data (from the MG muscle of a single
 % leg) as input along with the stimulation artifact indices for alignment
 % and computes the H-reflex amplitudes for the M-wave, H-wave, noise, and
@@ -90,10 +89,10 @@ function [amps,durs] = computeAmpsOneLeg(rawEMG,indsStimArt, ...
 %       signal
 %   indsStimArt: number of stimuli x 1 array of right or left leg stimulus
 %       artifact indices for H-reflex alignment
-%   indsWinStart:
-%   indsWinEnd:
+%   indsWinsRel: 2 x 5 array of snippet window of interest start and stop
+%       indices
 %   per: EMG data sampling period
-% output:
+% output(s):
 %   amps: 1 x 5 cell array of number of stimuli x 1 arrays for right or
 %       left leg H-reflex amplitudes: M-wave (column 1), H-wave (column 2),
 %       noise (column 3), background EMG MAV (column 4), and background EMG
@@ -103,38 +102,49 @@ function [amps,durs] = computeAmpsOneLeg(rawEMG,indsStimArt, ...
 %       absolute time difference between the minimum and maximum values)
 %       for determining whether a valid wave
 
-% TODO: add inputs checks (e.g., indStim not outside rawEMG array bounds)
-narginchk(5,5);                     % verify correct # of input arguments
-amps = cell(1,5);   % instantiate output amplitudes cell array
-durs = cell(1,2);   % instantiate output wave durations cell array
+narginchk(4,4);                 % verify correct number of input arguments
 
-numStim = length(indsStimArt);          % number of stimuli
+if isempty(indsStimArt)         % if no stimulation artifact indices, ...
+    error(['There are no stimulation artifact indices for which to ' ...
+        'extract the H-reflex amplitudes.']);   % return with error message
+end
+
+numStim = length(indsStimArt);  % number of stimuli
+% NOTE: increased threshold from 8ms to 10ms to include more waveforms
+threshDur = 0.010;              % 10ms is longest valid M/H-wave duration
+numWins = size(indsWinsRel,2);  % number of windows to extract data from
+% TODO: add inputs checks (e.g., indStim not outside rawEMG array bounds)
+
+amps = cell(1,5);           % instantiate output amplitudes cell array
+durs = cell(1,2);           % instantiate output wave durations cell array
 % initialize output amplitudes and durations arrays
-% TODO: is below initialization necessary or beneficial?
 amps = cellfun(@(x) nan(numStim,1),amps,'UniformOutput',false);
 durs = cellfun(@(x) nan(numStim,1),durs,'UniformOutput',false);
-% NOTE: increased threshold from 8ms to 10ms to include more waveforms
-threshDur = 0.010;                  % 10ms is longest valid M/H-wave dur.
-numWins = length(indsWinStart);     % # of windows to extract data from
+
+if isempty(rawEMG)              % if no EMG data provided, ...
+    warning(['There is no EMG data provided from which to extract the ' ...
+        'H-reflex amplitudes.']);   % TODO: indicate which leg missing data
+    return;                     % return with 'NaN' value output
+end
 
 for st = 1:numStim                      % for each stimulus, ...
     indStim = indsStimArt(st);          % stimulation index
     for win = 1:numWins                 % for each stim snippet window, ...
-        indsWin = (indStim+indsWinStart(win)):(indStim+indsWinEnd(win));
+        indsWinAbs = (indStim + indsWinsRel(1,win)): ...
+            (indStim + indsWinsRel(2,win));
         % ensure do not index outside of EMG bounds if stim near end trial
-        indsWin = indsWin(indsWin < length(rawEMG));
+        indsWinAbs = indsWinAbs(indsWinAbs < length(rawEMG));
         % extract EMG data for time windows from stim artifact onset
-        if ~isempty(indsWin)
-            winEMG = rawEMG(indsWin);   % extract EMG time window data
+        if ~isempty(indsWinAbs)
+            winEMG = rawEMG(indsWinAbs);   % extract EMG time window data
             [valMax,indMax] = max(winEMG);
             [valMin,indMin] = min(winEMG);
         else
             valMax = nan; indMax = nan;
             valMin = nan; indMin = nan;
         end
-        % TODO: add check that M-wave and H-wave are not too broad in lieu
-        % of or in addition to check that min or max are not early or late
-        % in EMG window
+        % TODO: add check that M/H-wave are not too broad in lieu of or in
+        % addition to check that min or max are not early or late in window
         switch win
             case {1,2}                  % M-wave or H-wave
                 durs{win}(st) = per * abs(indMax - indMin);
