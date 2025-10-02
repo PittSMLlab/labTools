ea4235e4743e19becae4a99ad17b8f156ac2ed4f
nwb18 <nwbrantly@pitt.edu>
2025-10-02 13:10:01 -0400

    Clean up code
    Clean up main processing C3D2MAT code by reformatting and adding more comments for clarity and readability. NOTE that these are aesthetic changes only with no functional modifications. NWB

--- gui/importc3d/c3d2mat.m
+++ gui/importc3d/c3d2mat.m
@@ -1,22 +1,29 @@
-%c3d2mat -- The main script for parsing .c3d files into a subject .mat file.
+%c3d2mat -- The main script for parsing C3D session files into a MAT file.
 %
-%This script collects information regarding the experiment conducted,
-%transforms the data from .c3d format into labTools objects,
-%and then processes that data to give HS and TO events, limb angles,
-%adaptation parameters, and processed EMG data (if EMG data are present)
+% This script collects information regarding the experiment conducted and
+% transforms the data from the .c3d format into 'labTools' objects and then
+% processes that data to give heel strike and toe off events, limb angles,
+% adaptation parameters, and processed EMG data (if EMG data are present).
 %
-%See also: GetInfoGUI, loadSubject
+% See also: GetInfoGUI, loadSubject
 
-% Begin by running the GetInfoGUI, this will collect subject data,
-% experiment data and notes, as well as trial conditions.
+% Begin by running the GetInfoGUI, which collects participant data,
+% experiment data, and notes, as well as trial conditions.
 info = GetInfoGUI;
 
-%Ask which event class to use
-if ~isempty(info)
-    eventClass={'','kin','force'};%possible methods to determine gait events, 'kin' for over ground, 'force' for treadmill. '' is use trial type or default
-    answer=menu('Which events should be used to compute parameters?','default (use force for TM trials, kinematics otherwise)','strictly from kinematics','strictly from forces');
-    if answer==0
-        answer=1;
+% Ask which event class to use
+%   (either default, kinematics only, or forces only)
+if ~isempty(info)               % if experimental information provided, ...
+    % available methods to determine gait events:
+    %   '' is default option (force for TM trials, kinematics otherwise)
+    %   'kin' for overground trials (limited/no force plate data)
+    %   'force' for treadmill trials (consistent/reliable force plate data)
+    eventClass = {'','kin','force'};
+    answer = menu('Which events should be used to compute parameters?', ...
+        'default (use force for TM trials, kinematics otherwise)', ...
+        'strictly from kinematics','strictly from forces');
+    if answer == 0  % if GUI menu window exited ('X'), ...
+        answer = 1; % set to 'default'
     end
 
     %Do the actual loading
@@ -24,8 +31,8 @@ if ~isempty(info)
         [expData,rawExpData,adaptData]=loadSubject(info,eventClass{answer});
     end
 
-    clear answer eventClass
-else
-    return
+    clear answer eventClass;    % clear workspace variables
+else                            % otherwise, ...
+    return;
 end
 
