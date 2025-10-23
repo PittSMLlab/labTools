5f39e3a1839a31bb1ae2ddb3bacc4b13314c3852
nwb18 <nwbrantly@pitt.edu>
2025-10-23 15:01:21 -0400

    Clean up experiment template
    Update code comments and formatting of the template script for generating an experimental protocol MAT file (for the experimental session information GUI dropdown menu to autopopulate all fields). Create new 'maxConds' variable rather than manually hardcoding number of experimental conditions. NWB

--- gui/importc3d/ExpDetails/template/Template.m
+++ gui/importc3d/ExpDetails/template/Template.m
@@ -1,86 +1,90 @@
-%EDIT THIS COMMENT: INCLUDE A BREIF DESCRIPTION OF WHAT STUDY THIS IS
-% ex - OG study: old subjects adapted abruptly
+%EDIT THIS COMMENT: INCLUDE A BREIF DESCRIPTION OF THE STUDY
+% Example: OG study: older participants adapted abruptly
 
-% The different fields of the contition info are difined here.
-% Save this code to a different name and then run it once 
-% finished editing for changes to appear in GetInfoGUI
+% The different fields of the condition info are defined here.
+% Save this code to a different name, and then run it once
+% Finish editing for changes to appear in 'GetInfoGUI'
 
-% Define Name of group
-expDes.group='New Group';
+% define name of group
+expDes.group = 'New Group';
 
-% Define Condition numbers (maximum number of conditions is 15 as of 4/2015)
-for cond = 1:10 %<-- edit this number
-    expDes.(['condition' num2str(cond)])=num2str(cond);
+% define maximum number of conditions
+maxConds = 10;                          % EDIT FOR YOUR EXPERIMENT
+
+% define total number of conditions
+expDes.numofconds = num2str(maxConds);  % matches defined maximum
+
+% define condition numbers
+for cond = 1:maxConds                   % for each condition, ...
+    expDes.(['condition' num2str(cond)]) = num2str(cond);   % set numbers
 end
-% Define total number of conditions
-expDes.numofconds='10'; %<-- this should match the number above
 
-% Define trial types. These descibe the general condition of the trial. For
-% example, if the subject is walking overground or at an incline, it would
-% be specified here. Types should only be difined for general conditions
-% that require a separate baseline for bias removal. Note that even though 
-% there may be a slow, fast, and medium baseline trial on the treadmill, 
-% if only the medium baseline is used to remove a bias from other treadmill
-% trials, then these can all be grouped into a general treadmill type.
-
-%set trial types
-for t=[1 9]
-    expDes.(['type' num2str(t)])='OG'; % for overground walking
+% define trial types
+%   Trial types descibe the general condition of the trial (e.g, walking
+% overground or on an incline).
+% NOTE: types should only be defined for general conditions that require a
+% separate baseline for bias removal (e.g., even though there may be a
+% slow, fast, and medium baseline treadmill trial, if only the medium
+% baseline is used for bias removal from other treadmill trials, then these
+% can all be grouped into a general treadmill trial type)
+for t = [1 9]                               % for OG trial indices, ...
+    expDes.(['type' num2str(t)]) = 'OG';    % overground walking trials
 end
-for t=[2:8 10]
-    expDes.(['type' num2str(t)])='TM'; % for treadmill walking
+for t = [2:8 10]                            % for TM trial indices, ...
+    expDes.(['type' num2str(t)]) = 'TM';    % treadmill walking trials
 end
-%note: 'IN' is typically used in the treadmill was inclined
+% NOTE: 'IN' is typically used for inclined treadmill trial types
 
-% Define condition names. These should be short but descriptive and should
-% follow lab conventions, if possible. IMPORTANT: The condition that will 
+% define condition names. These should be short but descriptive and should
+% follow lab conventions, if possible. IMPORTANT: The condition that will
 % be used to remove a bias from all other trials of the same type MUST
-% be named such that one of the type strings and the string ‘base’ are 
+% be named such that one of the type strings and the string ‘base’ are
 % both included in the condition's name.
-expDes.condName1='OG base';%<-- data from this condition will be used to remove bias from all other conditions of type OG
-expDes.condName2='slow base';
-expDes.condName3='short split';
-expDes.condName4='fast base';
-expDes.condName5='TM base'; %<-- data from this condition will be used to remove bias from all other conditions of type TM
-expDes.condName6='adaptation';
-expDes.condName7='catch';
-expDes.condName8='re-adaptation';
-expDes.condName9='OG post';
-expDes.condName10='TM post';
+expDes.condName1 = 'OG base';%<-- data from this condition will be used to remove bias from all other conditions of type OG
+expDes.condName2 = 'slow base';
+expDes.condName3 = 'short split';
+expDes.condName4 = 'fast base';
+expDes.condName5 = 'TM base'; %<-- data from this condition will be used to remove bias from all other conditions of type TM
+expDes.condName6 = 'adaptation';
+expDes.condName7 = 'catch';
+expDes.condName8 = 're-adaptation';
+expDes.condName9 = 'OG post';
+expDes.condName10 = 'TM post';
 
-% Define condition descriptions. Be as descriptive as possible. Include 
-% whatever information seems important to someone who is unfamiliar with 
-% what you did, such as the number of strides and possibly the speeds the 
-% belts were set at. 
-expDes.description1='8m walkway for 6 min';
-expDes.description2='150 strides at 0.5 m/s';
-expDes.description3='10 strides 2:1, 1 m/s and 0.5 m/s';
-expDes.description4='150 strides at 1 m/s';
-expDes.description5='150 strides at 0.75 m/s';
-expDes.description6='600 strides 2:1, 1 m/s and 0.5 m/s';
-expDes.description7='10 strides at 0.75 m/s';
-expDes.description8='300 strides 2:1, 1 m/s and 0.5 m/s';
-expDes.description9='8 m walkway for 6 min';
-expDes.description10='450 strides at 0.75 m/s';
+% Define condition descriptions. Be as descriptive as possible. Include
+% whatever information seems important to someone who is unfamiliar with
+% what you did, such as the number of strides and possibly the speeds the
+% belts were set at.
+expDes.description1 = '8m walkway for 6 min';
+expDes.description2 = '150 strides at 0.5 m/s';
+expDes.description3 = '10 strides 2:1, 1 m/s and 0.5 m/s';
+expDes.description4 = '150 strides at 1 m/s';
+expDes.description5 = '150 strides at 0.75 m/s';
+expDes.description6 = '600 strides 2:1, 1 m/s and 0.5 m/s';
+expDes.description7 = '10 strides at 0.75 m/s';
+expDes.description8 = '300 strides 2:1, 1 m/s and 0.5 m/s';
+expDes.description9 = '8 m walkway for 6 min';
+expDes.description10 = '450 strides at 0.75 m/s';
 
-% Define (expected) trial numbers for each condition. Multiple numbers can 
+% Define (expected) trial numbers for each condition. Multiple numbers can
 % be set as '1:5', '1,2,3' or '4 5' but not '1-5'). These will most
 % likely need edited on a per-subject basis. Numbers correspond to names of
 % files generated by Vicon Nexus during experiment.
-expDes.trialnum1='1:6';
-expDes.trialnum2='7';
-expDes.trialnum3='8';
-expDes.trialnum4='9';
-expDes.trialnum5='10';
-expDes.trialnum6='11:14';
-expDes.trialnum7='15';
-expDes.trialnum8='16 17';
-expDes.trialnum9='18:23';
-expDes.trialnum10='24:26';
+expDes.trialnum1 = '1:6';
+expDes.trialnum2 = '7';
+expDes.trialnum3 = '8';
+expDes.trialnum4 = '9';
+expDes.trialnum5 = '10';
+expDes.trialnum6 = '11:14';
+expDes.trialnum7 = '15';
+expDes.trialnum8 = '16 17';
+expDes.trialnum9 = '18:23';
+expDes.trialnum10 = '24:26';
 
-%-----------------------DO NOT EDIT BELOW THIS LINE ----------------------%
-groupName=expDes.group;
-groupName=groupName(ismember(groupName,['A':'Z' 'a':'z']));  
-path=which('GetInfoGUI');
-path=strrep(path,'GetInfoGUI.m','ExpDetails');
-save([path filesep groupName],'expDes')
+% --------------------- DO NOT EDIT BELOW THIS LINE --------------------- %
+groupName = expDes.group;
+groupName = groupName(ismember(groupName,['A':'Z' 'a':'z']));
+path = which('GetInfoGUI');
+path = strrep(path,'GetInfoGUI.m','ExpDetails');
+save([path filesep groupName],'expDes');
+
