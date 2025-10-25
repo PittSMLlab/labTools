99da9f97ba5271c4590ed1fc4b9c635b69f49fb0
nwb18 <nwbrantly@pitt.edu>
2025-10-25 17:03:32 -0400

    Update code formatting
    Update code comments and formatting (including using MATLAB autoformatting keyboard shortcut) for consistency and clarity. Comment unused code line. NWB

--- fun/eventExtraction/getEventsFromSwitches.m
+++ fun/eventExtraction/getEventsFromSwitches.m
@@ -1,16 +1,14 @@
 function [LHS,RHS,LTO,RTO] = getEventsFromSwitches(Lft_sw,Rft_sw,fsample)
 
-%% Get stance phases
-[stanceL] = getStanceFromSwitches(Lft_sw, fsample);
-[stanceR] = getStanceFromSwitches(Rft_sw, fsample);
+% retrieve left and right leg stance phases
+stanceL = getStanceFromSwitches(Lft_sw,fsample);
+stanceR = getStanceFromSwitches(Rft_sw,fsample);
 
-%% Get events from stance
+% retrieve gait events from stance phases
 [LHS,RHS,LTO,RTO] = getEventsFromStance(stanceL,stanceR);
 
-%% Check consistency
-[consistent] = checkEventConsistency(LHS,RHS,LTO,RTO);
-
-
+% verify that all gait events are consistent
+% consistent = checkEventConsistency(LHS,RHS,LTO,RTO);
 
 end
 
--- fun/eventExtraction/getStanceFromSwitches.m
+++ fun/eventExtraction/getStanceFromSwitches.m
@@ -1,10 +1,8 @@
- function [stance] = getStanceFromSwitches(ft_sw, fsample)
-%Get stance from acceleration
+function stance = getStanceFromSwitches(ft_sw,fsample)
+%GETSTANCEFROMSWITCHES Retrieve stance phase from acceleration
 
-stance=ft_sw>.2;
-
-
-%% STEP N: Eliminate stance & swing phases shorter than 200 ms
+stance = ft_sw > 0.2;
+% remove stance and swing phases shorter than 200 ms
 stance = deleteShortPhases(stance,fsample,0.2);
 
 end
