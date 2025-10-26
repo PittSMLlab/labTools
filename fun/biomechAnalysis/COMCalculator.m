33bedcb644d5f7b95e60a914f84ceaaf8b56c2ed
nwb18 <nwbrantly@pitt.edu>
2025-10-26 16:02:55 -0400

    Update code formatting
    Update code comments and formatting (including using MATLAB autoformatting keyboard shortcut) for consistency and clarity (and in attempt to better understand the code). NWB

--- fun/biomechAnalysis/COMCalculator.m
+++ fun/biomechAnalysis/COMCalculator.m
@@ -57,60 +57,53 @@ end
 % LToe=markerData.getDataAsVector({['LTOE' orientation.sideAxis],['LTOE' orientation.foreaftAxis],['LTOE' orientation.updownAxis]});
 % LToe=[orientation.sideSign*LToe(:,1),orientation.foreaftSign*LToe(:,2),orientation.updownSign*LToe(:,3)];
 
-
-%% Need to calculate the position of the center of mass of each segment
-
-%Foot:
-fcomR=(RAnk+RToe)/2;
-fcomL=(LAnk+LToe)/2;
+%% Step 2: Compute the Center of Mass Position of Each Leg Segment
+% foot:
+fcomR = (RAnk + RToe) / 2;
+fcomL = (LAnk + LToe) / 2;
 % fcomxR=(RAnk(:,1)-RToe(:,1)).*.5+RToe(:,1); %m
 % fcomxL=(LAnk(:,1)-LToe(:,1)).*.5+LToe(:,1); %m
 % fcomyR=(RAnk(:,2)-RToe(:,2)).*.5+RToe(:,2); %m
 % fcomzR=(RAnk(:,3)-RToe(:,3)).*.5+RToe(:,3); %m
 % fcomyL=(LAnk(:,2)-LToe(:,2)).*.5+LToe(:,2); %m
 % fcomzL=(LAnk(:,3)-LToe(:,3)).*.5+LToe(:,3); %m
-%
 % fcomR=[fcomxR,fcomyR,fcomzR]; %foot
 % fcomL=[fcomxL,fcomyL,fcomzL];
 
-%Shank:
-scomR=(RKne-RAnk)+.394*RAnk;
-scomL=(LKne-LAnk)+.394*LAnk;
-
+% shank:
+scomR = (RKne - RAnk) + 0.394 * RAnk;
+scomL = (LKne - LAnk) + 0.394 * LAnk;
 % scomxR=(RKnee(:,1)-RAnk(:,1)).*.394+RAnk(:,1);
 % scomxL=(LKnee(:,1)-LAnk(:,1)).*.394+LAnk(:,1);
 % scomyR=(RKnee(:,2)-RAnk(:,2)).*.394+RAnk(:,2);
 % scomzR=(RKnee(:,3)-RAnk(:,3)).*.394+RAnk(:,3);
 % scomyL=(LKnee(:,2)-LAnk(:,2)).*.394+LAnk(:,2);
 % scomzL=(LKnee(:,3)-LAnk(:,3)).*.394+LAnk(:,3);
-%
 % scomR=[scomxR,scomyR,scomzR]; %Shank
 % scomL=[scomxL,scomyL,scomzL];
 
-%Thigh:
-tcomR=(RHip-RKne)+.567*RKne;
-tcomL=(LHip-LKne)+.567*LKne;
+% thigh:
+tcomR = (RHip - RKne) + 0.567 * RKne;
+tcomL = (LHip - LKne) + 0.567 * LKne;
 % tcomxR=(RHip(:,1)-RKnee(:,1)).*.567+RKnee(:,1);
 % tcomxL=(LHip(:,1)-LKnee(:,1)).*.567+LKnee(:,1);
 % tcomyR=(RHip(:,2)-RKnee(:,2)).*.567+RKnee(:,2);
 % tcomzR=(RHip(:,3)-RKnee(:,3)).*.567+RKnee(:,3);
 % tcomyL=(LHip(:,2)-LKnee(:,2)).*.567+LKnee(:,2);
 % tcomzL=(LHip(:,3)-LKnee(:,3)).*.567+LKnee(:,3);
-%
 % tcomR=[tcomxR,tcomyR,tcomzR]; %Thigh
 % tcomL=[tcomxL,tcomyL,tcomzL];
 
-%% Save everything in an orientedLabTS
-COMData=[fcomR fcomL scomR scomL tcomR tcomL]; %CJS note to self, change here to change what is stored in the COM
-%COM accelerations for each of the different body parts in each of the
-%directions.
+%% Step 3: Save Computations in an 'orientedLabTS' Object
+COMData = [fcomR fcomL scomR scomL tcomR tcomL];
+% COM accelerations for each leg segment in each directions
 
-labels={'RfCOM','LfCOM','RsCOM','LsCOM','RtCOM','LtCOM'};
-labels=[strcat(labels,'x'); strcat(labels,'y'); strcat(labels,'z')];
+labels = {'RfCOM','LfCOM','RsCOM','LsCOM','RtCOM','LtCOM'};
+labels = [strcat(labels,'x'); strcat(labels,'y'); strcat(labels,'z')];
 
-%Pablo: creating orientedLabTS
-COMTS=orientedLabTimeSeries(COMData,markerData.Time(1),markerData.sampPeriod,labels(:),markerData.orientation);
-
+% create 'orientedLabTS' object to output
+COMTS = orientedLabTimeSeries(COMData,markerData.Time(1), ...
+    markerData.sampPeriod,labels(:),markerData.orientation);
 
 end
 
