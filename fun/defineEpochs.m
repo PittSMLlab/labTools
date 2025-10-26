d4972ab86d136c74577ac8465ecd5bcc62acab73
nwb18 <nwbrantly@pitt.edu>
2025-10-26 17:40:01 -0400

    Fix typo bug
    Fix bug in first conditional statement with variable name 'condtion' rather than 'condition'. Update code comments and formatting (including using MATLAB autoformatting keyboard shortcut) for consistency and clarity (and in attempt to better understand the code). NWB

--- fun/defineEpochs.m
+++ fun/defineEpochs.m
@@ -1,4 +1,4 @@
-function [epochs] = defineEpochs(epochNames,condition,strideNo,exemptFirst,exemptLast,summaryMethod,shortName)
+function epochs = defineEpochs(epochNames,condition,strideNo,exemptFirst,exemptLast,summaryMethod,shortName)
 %defineEpochs is used to create a dataset object that defines relevant
 %epochs in the analysis of experimental data. It is used by several
 %functions within studyData, groupAdaptationData and adaptationData
@@ -15,39 +15,47 @@ function [epochs] = defineEpochs(epochNames,condition,strideNo,exemptFirst,exemp
 %used to summarize accross strides, default is 'nanmean'
 %Ex: [epochs] = defineEpochs({'Initial_A1','Last_A1','Initial_A2','Last_A2'},{'Adaptation 1','Adaptation 1','Adaptation 1(2nd time)','Adaptation 1(2nd time)'},[5 -40 5 -40],5,5,{'nanmean'})
 
-N=length(epochNames);
-if isa(condition,'char')
-    condition={condtion};
+N = length(epochNames);         % number of epoch names
+if isa(condition,'char')        % if character array, ...
+    condition = {condition};    % create cell array string
 end
-if nargin<6 || isempty(summaryMethod)
-    summaryMethod='nanmean';
+
+if nargin < 6 || isempty(summaryMethod)
+    summaryMethod = 'nanmean';
 end
 
 if isa(summaryMethod,'char') %To allow for summaryMethod to be given as string directly
     summaryMethod={summaryMethod};
 end
-if numel(condition)==1
+
+if numel(condition) == 1
     condition=repmat(condition,N,1);
 end
-if numel(strideNo)==1
+
+if numel(strideNo) == 1
     strideNo=repmat(strideNo,N,1);
 end
-if numel(exemptFirst)==1
+
+if numel(exemptFirst) == 1
     exemptFirst=repmat(exemptFirst,N,1);
 end
-if numel(exemptLast)==1
+
+if numel(exemptLast) == 1
     exemptLast=repmat(exemptLast,N,1);
 end
-if numel(summaryMethod)==1
+
+if numel(summaryMethod) == 1
     summaryMethod=repmat(summaryMethod,N,1);
 end
+
 earlyOrLate=sign(strideNo)==1;
 if nargin<7 || isempty(shortName)
     shortName=cell(size(epochNames));
-elseif numel(shortName)==1
+elseif numel(shortName) == 1
     shortName=repmat(shortName,N,1);
 end
 
-epochs=dataset(condition(:),abs(strideNo(:)),exemptFirst(:),exemptLast(:),earlyOrLate(:),summaryMethod(:),shortName(:),'VarNames',{'Condition','Stride_No','ExemptFirst','ExemptLast','EarlyOrLate','summaryMethod','shortName'},'ObsNames',epochNames);
+epochs = dataset(condition(:),abs(strideNo(:)),exemptFirst(:),exemptLast(:),earlyOrLate(:),summaryMethod(:),shortName(:),'VarNames',{'Condition','Stride_No','ExemptFirst','ExemptLast','EarlyOrLate','summaryMethod','shortName'},'ObsNames',epochNames);
+
 end
 
