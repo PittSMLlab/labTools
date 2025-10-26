5468ea8f4468a2996e4139ccc741a16fe2004ec7
nwb18 <nwbrantly@pitt.edu>
2025-10-26 17:44:10 -0400

    Update code formatting
    Update code comments (including adding function documentation comments) and formatting (including using MATLAB autoformatting keyboard shortcut) for consistency and clarity (and in attempt to better understand the code). Add line terminating semicolon characters for consistency. Remove unused and not output final 'adaptationData' object (can be re-added if desire to output this variable from this function in addition to saving the updated 'params' file). NOTE that there are no functional changes in this commit, only aesthetic changes. NWB

--- fun/reviseExperimentInfo.m
+++ fun/reviseExperimentInfo.m
@@ -1,15 +1,12 @@
-function reviseExperimentInfo
+function reviseExperimentInfo()
+%REVISEEEXPERIMENTINFO Revise experimental session information in MAT files
 
-%reviseSubjectInfo
-
-% Get info!
-info = GetInfoGUI;
-
+info = GetInfoGUI();    % retrieve information files
 
 %%
 %Compare to old subject file
-load([info.save_folder filesep info.ID 'RAW.mat'])
-load([info.save_folder filesep info.ID '.mat'])
+load([info.save_folder filesep info.ID 'RAW.mat']);
+load([info.save_folder filesep info.ID '.mat']);
 
 expDate = labDate(info.day,info.month,info.year);
 %% Experiment info
@@ -54,7 +51,7 @@ end
 
 DOB = labDate(info.DOBday,info.DOBmonth,info.DOByear);
 
-%age calc 
+%age calc
 age = expDate.year - DOB.year;
 if expDate.month < DOB.month
     age = age-1;
@@ -66,68 +63,70 @@ end
 
 if ~isfield(info,'isStroke') || info.isStroke==0
     subData=subjectData(DOB,info.gender,info.domleg,info.domhand,info.height,...
-    info.weight,age,info.ID,info.fastLeg);
+        info.weight,age,info.ID,info.fastLeg);
 else
     subData=strokeSubjectData(DOB,info.gender,info.domleg,info.domhand,info.height,...
-    info.weight,age,info.ID,info.fastLeg,info.affectedSide); %TO DO: add stroke date
+        info.weight,age,info.ID,info.fastLeg,info.affectedSide); %TO DO: add stroke date
 end
 
 %% Trial Data
 
 % Generate meta data for each trial
-for cond = sort(info.cond) 
-    for t = expData.metaData.trialsInCondition{cond}               
+for cond = sort(info.cond)
+    for t = expData.metaData.trialsInCondition{cond}
         if ~isfield(info,'trialObs')
             info.trialObs=cell(info.numoftrials,1);
         end
         % constructor: (name,desc,obs,refLeg,cond,filename,type)
         trialMD{t}=trialMetaData(info.conditionNames{cond},info.conditionDescriptions{cond},...
-            info.trialObs{t},info.refLeg,cond,expData.data{t}.metaData.rawDataFilename,info.type{cond});        
-    end    
+            info.trialObs{t},info.refLeg,cond,expData.data{t}.metaData.rawDataFilename,info.type{cond});
+    end
 end
 
 % Load trials
-for t=cell2mat(expData.metaData.trialsInCondition)     
+for t=cell2mat(expData.metaData.trialsInCondition)
     GRFData=rawExpData.data{t}.GRFData;
     EMGData=rawExpData.data{t}.EMGData;
     accData=rawExpData.data{t}.accData;
-    markerData=rawExpData.data{t}.markerData;    
+    markerData=rawExpData.data{t}.markerData;
     %rawTrialData(metaData,markerData,EMGData,GRFData,beltSpeedSetData,beltSpeedReadData,accData,EEGData,footSwitches)
     trials{t}=rawTrialData(trialMD{t},markerData,EMGData,GRFData,[],[],accData,[],[]);
-end    
-    
+end
+
 rawExpData=experimentData(expMD,subData,trials); %Overwrites old!
 
 %save raw
 save([info.save_folder filesep info.ID 'RAW.mat'],'rawExpData','-v7.3')
 
 %% "Process" data
-for t=1:length(rawExpData.data)    
-    if ~isempty(rawExpData.data{t})        
-            procEMGData=expData.data{t}.procEMGData;
-            filteredEMGData=expData.data{t}.EMGData;
-            angleData=expData.data{t}.angleData;
-            events = expData.data{t}.gaitEvents;            
-            beltSpeedReadData = expData.data{t}.beltSpeedReadData; 
-            jointMomentsData = expData.data{t}.jointMomentsData;
-            COPData = expData.data{t}.COPData;
-            COMData = expData.data{t}.COMData; 
-            
-            % Generate processedTrial object
-            processedData{t}=processedTrialData(trialMD{t},expData.data{t}.markerData,...
-                filteredEMGData,expData.data{t}.GRFData,expData.data{t}.beltSpeedSetData,beltSpeedReadData,...
-                expData.data{t}.accData,expData.data{t}.EEGData,expData.data{t}.footSwitchData,events,procEMGData,angleData,COPData,COMData,jointMomentsData);
-            processedData{t}.adaptParams=expData.data{t}.adaptParams;     
+for t=1:length(rawExpData.data)
+    if ~isempty(rawExpData.data{t})
+        procEMGData=expData.data{t}.procEMGData;
+        filteredEMGData=expData.data{t}.EMGData;
+        angleData=expData.data{t}.angleData;
+        events = expData.data{t}.gaitEvents;
+        beltSpeedReadData = expData.data{t}.beltSpeedReadData;
+        jointMomentsData = expData.data{t}.jointMomentsData;
+        COPData = expData.data{t}.COPData;
+        COMData = expData.data{t}.COMData;
+
+        % Generate processedTrial object
+        processedData{t}=processedTrialData(trialMD{t},expData.data{t}.markerData,...
+            filteredEMGData,expData.data{t}.GRFData,expData.data{t}.beltSpeedSetData,beltSpeedReadData,...
+            expData.data{t}.accData,expData.data{t}.EEGData,expData.data{t}.footSwitchData,events,procEMGData,angleData,COPData,COMData,jointMomentsData);
+        processedData{t}.adaptParams = expData.data{t}.adaptParams;
     else
-        processedData{t}=[];
+        processedData{t} = [];
     end
 end
-expData=experimentData(expMD,subData,processedData); %Overwrites old!
+% NOTE: the below line overwrites any previous file of the same name!
+expData = experimentData(expMD,subData,processedData);
 
-%Save processed
-save([info.save_folder filesep info.ID '.mat'],'expData','-v7.3')
+% save updated processed 'experimentalData' object
+save([info.save_folder filesep info.ID '.mat'],'expData','-v7.3');
 
-%create adaptationData object
-adaptData=expData.makeDataObj([info.save_folder filesep info.ID]);
+% update and save 'adaptationData' object
+expData.makeDataObj([info.save_folder filesep info.ID]);
 
 end
+
