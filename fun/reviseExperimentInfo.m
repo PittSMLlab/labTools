d08b747c238a6b4422c67ccd3763f2115b5ba381
nwb18 <nwbrantly@pitt.edu>
2025-10-26 20:07:52 -0400

    Update to match latest version
    Update to match latest version of 'loadSubjects' (e.g., handling reference leg for participants with stroke, calculating age to the nearest month rather than year). Output all data objects since already available in case user would like to access the updated objects without reloading from the newly saved files. Update code comments and formatting for consistency and clarity. NWB

--- fun/reviseExperimentInfo.m
+++ fun/reviseExperimentInfo.m
@@ -1,75 +1,80 @@
-function reviseExperimentInfo()
+function  [expData,rawExpData,adaptData] = reviseExperimentInfo()
 %REVISEEEXPERIMENTINFO Revise experimental session information in MAT files
 
-info = GetInfoGUI();    % retrieve information files
-
-%%
-%Compare to old subject file
+info = GetInfoGUI();            % retrieve experimental session information
+% load previous raw and processed experimental session data for comparison
 load([info.save_folder filesep info.ID 'RAW.mat']);
 load([info.save_folder filesep info.ID '.mat']);
-
 expDate = labDate(info.day,info.month,info.year);
-%% Experiment info
 
-expMD=experimentMetaData(info.ExpDescription,expDate,info.experimenter,...
-    info.exp_obs,info.conditionNames,info.conditionDescriptions,expData.metaData.trialsInCondition,expData.metaData.Ntrials);
-%Constructor(ID,date,experimenter,obs,conds,desc,trialLst,Ntrials)
+%% Experiment Information
+expMD = experimentMetaData(info.ExpDescription,expDate, ...
+    info.experimenter,info.exp_obs,info.conditionNames, ...
+    info.conditionDescriptions,expData.metaData.trialsInCondition, ...
+    expData.metaData.Ntrials);
+% constructor(ID,date,experimenter,obs,conds,desc,trialLst,Ntrials);
 
-%% Subject info
-
-% find reference leg
-%This assumes
-%   1) that the leg on the fast belt is the dominant leg
-%   2) that info.domleg is either 'left' or 'right'
-%   3) that the reference leg is the leg on the slow belt
-
-if isfield(info,'fastLeg')
+%% Participant Information
+% determine the reference leg, which assumes that:
+%   1) the leg on the fast belt is the dominant leg
+%   2) 'info.domleg' is either 'left' or 'right'
+%   3) the reference leg is the leg on the slow belt
+if isfield(info,'fastLeg')                  % if fast leg specified, ...
     if strcmpi(info.fastLeg,'right')
-        info.refLeg='L';
+        info.refLeg = 'L';
     elseif strcmpi(info.fastLeg,'left')
         info.refLeg = 'R';
     else
-        warning('Reference leg could not be determined from information given. Make sure info.fastLeg is either ''Left'' or ''Right''.')
+        warning(['Reference leg could not be determined from ' ...
+            'information given. Make sure info.fastLeg is either ' ...
+            '''Left'' or ''Right''.']);
     end
-elseif isfield(info,'isStroke') && info.isStroke==1 %For stroke patients, reference leg is equal to affected side
+elseif isfield(info,'isStroke') && info.isStroke == 1   % if stroke, ...
+    % reference leg is affected side when leg / belt speed is not provided
+    % TODO: add condition in case fast leg field does not exist
     if strcmpi(info.affectedSide,'right')
-        info.refLeg='R';
+        info.refLeg = 'R';
+        info.fastLeg = 'Left';
     elseif strcmpi(info.affectedSide,'left')
         info.refLeg = 'L';
+        info.fastLeg = 'Right';
     else
-        warning('Reference leg could not be determined from information given. Make sure info.affectedSide is either ''Left'' or ''Right''.')
+        warning(['Reference leg could not be determined from ' ...
+            'information given. Make sure info.affectedSide is either ' ...
+            '''Left'' or ''Right''.']);
     end
-else %For non-stroke patients, we are assuming that the reference leg is their non-dominant leg
+else                                                    % otherwise, ...
+    % assume reference leg is non-dominant leg when information not given
     if strcmpi(info.domleg,'right')
         info.refLeg = 'L';
+        info.fastLeg = 'Right';
     elseif strcmpi(info.domleg,'left')
         info.refLeg = 'R';
+        info.fastLeg = 'Left';
     else
-        warning('Reference leg could not be determined from information given. Make sure info.domleg is either ''Left'' or ''Right''.')
+        warning(['Reference leg could not be determined from ' ...
+            'information given. Make sure info.domleg is either ' ...
+            '''Left'' or ''Right''.']);
     end
 end
 
 DOB = labDate(info.DOBday,info.DOBmonth,info.DOByear);
+% compute age at time of experimental session to nearest month
+% TODO: why not compute age to the nearest day?
+ageInMonths = round(expDate.timeSince(DOB));    % round to closest month
+age = ageInMonths / 12;
 
-%age calc
-age = expDate.year - DOB.year;
-if expDate.month < DOB.month
-    age = age-1;
-elseif expDate.month == DOB.month
-    if expDate.day < DOB.day
-        age = age-1;
-    end
+if ~isfield(info,'isStroke') || info.isStroke == 0  % if no stroke, ...
+    subData = subjectData(DOB,info.gender,info.domleg, ...
+        info.domhand,info.height,info.weight,age,info.ID,info.fastLeg);
+else                                                % otherwise, stroke
+    % TODO: add date of stroke to participant data object
+    subData = strokeSubjectData(DOB,info.gender,info.domleg, ...
+        info.domhand,info.height,info.weight,age,info.ID,info.fastLeg, ...
+        info.affectedSide);
 end
 
-if ~isfield(info,'isStroke') || info.isStroke==0
-    subData=subjectData(DOB,info.gender,info.domleg,info.domhand,info.height,...
-        info.weight,age,info.ID,info.fastLeg);
-else
-    subData=strokeSubjectData(DOB,info.gender,info.domleg,info.domhand,info.height,...
-        info.weight,age,info.ID,info.fastLeg,info.affectedSide); %TO DO: add stroke date
-end
-
-%% Trial Data
+%% Process Trial Data
 
 % Generate meta data for each trial
 for cond = sort(info.cond)
@@ -84,7 +89,7 @@ for cond = sort(info.cond)
 end
 
 % Load trials
-for t=cell2mat(expData.metaData.trialsInCondition)
+for t = cell2mat(expData.metaData.trialsInCondition)
     GRFData=rawExpData.data{t}.GRFData;
     EMGData=rawExpData.data{t}.EMGData;
     accData=rawExpData.data{t}.accData;
@@ -92,14 +97,13 @@ for t=cell2mat(expData.metaData.trialsInCondition)
     %rawTrialData(metaData,markerData,EMGData,GRFData,beltSpeedSetData,beltSpeedReadData,accData,EEGData,footSwitches)
     trials{t}=rawTrialData(trialMD{t},markerData,EMGData,GRFData,[],[],accData,[],[]);
 end
-
-rawExpData=experimentData(expMD,subData,trials); %Overwrites old!
-
-%save raw
+% NOTE: the below line overwrites any previous file of the same name!
+rawExpData = experimentData(expMD,subData,trials);
+% save updated raw 'rawExperimentalData' object
 save([info.save_folder filesep info.ID 'RAW.mat'],'rawExpData','-v7.3')
 
 %% "Process" data
-for t=1:length(rawExpData.data)
+for t = 1:length(rawExpData.data)
     if ~isempty(rawExpData.data{t})
         procEMGData=expData.data{t}.procEMGData;
         filteredEMGData=expData.data{t}.EMGData;
@@ -121,12 +125,10 @@ for t=1:length(rawExpData.data)
 end
 % NOTE: the below line overwrites any previous file of the same name!
 expData = experimentData(expMD,subData,processedData);
-
 % save updated processed 'experimentalData' object
 save([info.save_folder filesep info.ID '.mat'],'expData','-v7.3');
-
 % update and save 'adaptationData' object
-expData.makeDataObj([info.save_folder filesep info.ID]);
+adaptData = expData.makeDataObj([info.save_folder filesep info.ID]);
 
 end
 
