8dec2fce10839ab4df1722e43e3255b496ae7387
nwb18 <nwbrantly@pitt.edu>
2026-01-30 10:40:56 -0500

    Update class formatting
    Use Claude AI and MATLAB's autoformatting tool to consistently format the 'trialMetaData' class using conventional MATLAB whitespace formatting for consistency and clarity. Add section headers. NOTE that there are no functional changes in this commit, only aesthetic changes. NWB

--- classes/dataStructs/metaData/trialMetaData.m
+++ classes/dataStructs/metaData/trialMetaData.m
@@ -40,71 +40,109 @@ classdef trialMetaData
         datlog = '';
     end
 
+    %% Constructor
     methods
-        %Constructor
-        %trialMetaData(desc,obs,refLeg,cond,filename,type)
-        function this=trialMetaData(name,desc,obs,refLeg,cond,filename,type,schenleyLab,perceptualTasks,ID,datlog)
-            if isa(name,'char')
-                this.name=name;
-            end
-            if nargin>1 && isa(desc,'char')
-                this.description=desc;
-            end
-            if nargin>2 && isa(obs,'char')
-                this.observations=obs;
-            end
-            if nargin>3 && (isa(refLeg,'char')) %Must be either 'L' or 'R'
-                if strcmpi(refLeg,'R') || strcmpi(refLeg,'L')
-                    this.refLeg=refLeg;
-                else
-                    ME = MException('experimentMetaData:Constructor','refLeg must be either ''L'' or ''R''.');
-                    throw(ME);
-                end
-            end
-            if nargin>4 && isa(cond,'double');
-                this.condition=cond;
-            end
-            if nargin>5 && (isa(filename,'char') || (isa(filename,'cell')&& isa(filename{1},'char')) )
-                this.rawDataFilename=filename;
-            end
-            if nargin>6 && (isa(type,'char'))
-                if strcmpi(type,'TM') || strcmpi(type,'OG') || strcmpi(type,'NIM') || strcmpi(type,'IN')
-                    this.type=type;
-                else
-                    ME = MException('labMetaData:Constructor','type must be either ''OG'' or ''TM'' , ''NIM'' or ''IN''.');
-                    throw(ME);
-                end
-            else
-                this.type='TM';
-                warning('Assuming trial is conducted on the treadmill')
-            end
+        function this = trialMetaData(name, desc, obs, refLeg, cond, ...
+                filename, type, schenleyLab, perceptualTasks, ID, datlog)
+            %trialMetaData  Constructor for trialMetaData class
+            %
+            %   this = trialMetaData(name) creates a trial metadata object
+            %   with the specified trial name
+            %
+            %   this = trialMetaData(name, desc, obs, refLeg, cond, filename,
+            %   type, schenleyLab, perceptualTasks, ID, datlog) creates a
+            %   trial metadata object with all specified parameters
+            %
+            %   Inputs:
+            %       name - short name/label for the trial condition
+            %       desc - detailed description of the condition (optional)
+            %       obs - observations or notes about the trial (optional)
+            %       refLeg - reference leg, 'L' or 'R' (optional)
+            %       cond - condition number (optional)
+            %       filename - raw data filename(s), string or cell array
+            %                  (optional)
+            %       type - trial type: 'TM' (treadmill), 'OG' (overground),
+            %              'NIM' (Nimbus), or 'IN' (instrumented) (optional,
+            %              default: 'TM')
+            %       schenleyLab - flag for Schenley lab data (optional,
+            %                     default: 0)
+            %       perceptualTasks - flag for perceptual tasks (optional,
+            %                         default: 0)
+            %       ID - trial identifier (optional)
+            %       datlog - data log structure (optional)
+            %
+            %   Outputs:
+            %       this - trialMetaData object
 
-            if nargin>7 && (isa(schenleyLab,'double'))
-                this.schenleyLab=schenleyLab;
-            else
-                this.schenleyLab=0;
-                warning('Assuming this data was not collected on Schenley lab. This will only affect overground trial turn removal.')
+            if isa(name, 'char')
+                this.name = name;
             end
-            if nargin>8 && (isa(perceptualTasks,'double'))
-                this.perceptualTasks=perceptualTasks;
-            else
-                this.perceptualTasks=0;
-                warning('Assuming this experiment does not have any perceptual tasks (2AFC tasks).')
+            if nargin > 1 && isa(desc, 'char')
+                this.description = desc;
             end
-            if nargin>9 && (isa(ID,'char'))
+            if nargin > 2 && isa(obs, 'char')
+                this.observations = obs;
+            end
+            if nargin > 3 && (isa(refLeg, 'char')) % Must be either 'L' or 'R'
+                if strcmpi(refLeg, 'R') || strcmpi(refLeg, 'L')
+                    this.refLeg = refLeg;
+                else
+                    ME = MException('experimentMetaData:Constructor', ...
+                        'refLeg must be either ''L'' or ''R''.');
+                    throw(ME);
+                end
+            end
+            if nargin > 4 && isa(cond, 'double')
+                this.condition = cond;
+            end
+            if nargin > 5 && (isa(filename, 'char') || ...
+                    (isa(filename, 'cell') && isa(filename{1}, 'char')))
+                this.rawDataFilename = filename;
+            end
+            if nargin > 6 && (isa(type, 'char'))
+                if strcmpi(type, 'TM') || strcmpi(type, 'OG') || ...
+                        strcmpi(type, 'NIM') || strcmpi(type, 'IN')
+                    this.type = type;
+                else
+                    ME = MException('labMetaData:Constructor', ...
+                        ['type must be either ''OG'' or ''TM'', ''NIM'' ' ...
+                        'or ''IN''.']);
+                    throw(ME);
+                end
+            else
+                this.type = 'TM';
+                warning('Assuming trial is conducted on the treadmill');
+            end
+            if nargin > 7 && (isa(schenleyLab, 'double'))
+                this.schenleyLab = schenleyLab;
+            else
+                this.schenleyLab = 0;
+                warning(['Assuming this data was not collected on ' ...
+                    'Schenley lab. This will only affect overground ' ...
+                    'trial turn removal.'])
+            end
+            if nargin > 8 && (isa(perceptualTasks, 'double'))
+                this.perceptualTasks = perceptualTasks;
+            else
+                this.perceptualTasks = 0;
+                warning(['Assuming this experiment does not have any ' ...
+                    'perceptual tasks (2AFC tasks).']);
+            end
+            if nargin > 9 && (isa(ID, 'char'))
                 this.ID = ID;
             else
-                this.ID='';
+                this.ID = '';
             end
-            if nargin>10 && (isa(datlog,'struct'))
+            if nargin > 10 && (isa(datlog, 'struct'))
                 this.datlog = datlog;
-                if isfield(datlog,'datlog') %datlog is saved in datlog.datlog, this will be true depends on how the datlog was loaded.
+                % datlog is saved in datlog.datlog, this will be true
+                % depends on how the datlog was loaded.
+                if isfield(datlog, 'datlog')
                     this.datlog = this.datlog.datlog;
                 end
             else
-                this.datlog='';
+                this.datlog = '';
             end
-
         end
     end
 
