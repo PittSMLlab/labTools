4a801722f6a6d503d533daa8010112f1c0b68d3d
nwb18 <nwbrantly@pitt.edu>
2026-01-29 22:39:43 -0500

    Update class formatting
    Use Claude AI and MATLAB's autoformatting tool to consistently format the 'labData' class using conventional MATLAB whitespace formatting for consistency and clarity. Remove code after verifying individual method files contain the same code refortmatted, and replace with the corresponding method prototypes. Add line ending semicolons, and remove unnecessary brackets. NOTE that there are no functional changes in this commit, only aesthetic changes. NWB

--- classes/dataStructs/@labData/checkMarkerDataHealth.m
+++ classes/dataStructs/@labData/checkMarkerDataHealth.m
@@ -23,110 +23,103 @@ for i = 1:length(ll)
     l = ll{i};
     aux(:, i) = any(isnan(dd(:, i, :)), 3);
     if any(aux(i, :))
-        warning('labData:checkMarkerDataHealth', ...
-            ['Marker ' l ' is missing for ' ...
-            num2str(sum(aux) * ts.sampPeriod) ' secs.'])
+        warning('labData:checkMarkerDataHealth', ['Marker ' l ...
+            ' is missing for ' num2str(sum(aux) * ts.sampPeriod) ' secs.']);
         for j = 1:3
             % Filling gaps just for healthCheck purposes
             dd(aux, i, j) = nanmean(dd(:, i, j));
         end
     end
 end
-figure
-subplot(2, 2, 1) % Missing frames as % of total
-hold on
-c = sum(aux, 1) / size(aux, 1);
-bar(c)
-set(gca, 'XTick', 1:numel(ll), 'XTickLabel', ll, ...
-    'XTickLabelRotation', 90)
-ylabel('Missing frames (%)')
 
-subplot(2, 2, 2) % Distribution of gap lengths
-hold on
+figure;
+subplot(2, 2, 1); % Missing frames as % of total
+hold on;
+c = sum(aux, 1) / size(aux, 1);
+bar(c);
+set(gca, 'XTick', 1:numel(ll), 'XTickLabel', ll, 'XTickLabelRotation', 90);
+ylabel('Missing frames (%)');
+
+subplot(2, 2, 2); % Distribution of gap lengths
+hold on;
 h = [];
 s = {};
 for i = 1:length(ll)
-    if c(i) > .01
+    if c(i) > 0.01
         w = [0; aux(:, i); 0]; % auxiliary vector
         runs_ones = find(diff(w) == -1) - find(diff(w) == 1);
-        h(end + 1) = histogram(runs_ones, [.5:1:50], ...
-            'DisplayName', ll{i});
+        h(end + 1) = histogram(runs_ones, .5:1:50, 'DisplayName', ll{i});
         s{end + 1} = ll{i};
     end
 end
-legend(h, s)
-title('Distribution of gap lenghts')
-ylabel('Gap count')
-xlabel('Gap length')
-axis tight
+legend(h, s);
+title('Distribution of gap lenghts');
+ylabel('Gap count');
+xlabel('Gap length');
+axis tight;
 
-subplot(2, 2, 3)
-hold on
+subplot(2, 2, 3);
+hold on;
 d = sum(aux, 2);
-histogram(d, [-.5:1:5.5], 'Normalization', 'probability')
-title('Distribution of # missing in each frame')
-ylabel('% of frames with missing markers')
-xlabel('# missing markers')
-axis tight
-
+histogram(d, -.5:1:5.5, 'Normalization', 'probability');
+title('Distribution of # missing in each frame');
+ylabel('% of frames with missing markers');
+xlabel('# missing markers');
+axis tight;
 
 % Two: create a model to determine if outliers are present
 dd = permute(dd, [2, 3, 1]);
 [D, sD] = createZeroModel(dd);
 [lp, ~] = determineLikelihoodFromZeroModel(dd, D, sD);
-subplot(2, 2, 4)
-hold on
+subplot(2, 2, 4);
+hold on;
 minLike = min(lp, [], 1);
-plot(minLike)
+plot(minLike);
 medLike = nanmedian(lp, 1);
-plot(medLike, 'r')
-legend(ll)
+plot(medLike, 'r');
+legend(ll);
 ii = find(medLike < -1);
 for j = 1:length(ii)
-    figure
-    plot3(dd(:, 1, ii(j)), dd(:, 2, ii(j)), dd(:, 3, ii(j)), ...
-        'o')
-    text(dd(:, 1, ii(j)), dd(:, 2, ii(j)), dd(:, 3, ii(j)), ll)
-    hold on
+    figure;
+    plot3(dd(:, 1, ii(j)), dd(:, 2, ii(j)), dd(:, 3, ii(j)), 'o');
+    text(dd(:, 1, ii(j)), dd(:, 2, ii(j)), dd(:, 3, ii(j)), ll);
+    hold on;
     [~, zz] = min(lp(:, ii(j)));
-    plot3(dd(zz, 1, ii(j)), dd(zz, 2, ii(j)), ...
-        dd(zz, 3, ii(j)), 'ro')
-    title(['median likeli = ' num2str(medLike(ii(j))) ...
-        ', frame ' num2str(ii(j))])
-    pause
+    plot3(dd(zz, 1, ii(j)), dd(zz, 2, ii(j)), dd(zz, 3, ii(j)), 'ro');
+    title(['median likeli = ' num2str(medLike(ii(j))) ', frame ' ...
+        num2str(ii(j))]);
+    pause;
 end
 
-%             % Check for outliers:
-%             % Do a data translation:
-%             % refMarker = squeeze(mean(ts.getOrientedData({'LHIP','RHIP'}), 2)); % Assuming these markers exist
-%             % Do a label-agnostic data translation:
-%             refMarker = squeeze(nanmean(dd, 2));
-%             newTS = ts.translate([-refMarker(:, 1:2), zeros(size(refMarker, 1), 1)]); % Assuming z is a known fixed axis
-%             % Not agnostic rotation:
-%             % relData = squeeze(markerData.getOrientedData('RHIP'));
-%             % Label agnostic data rotation:
-%             newTS = newTS.alignRotate([refMarker(:, 2), -refMarker(:, 1), zeros(size(refMarker, 1), 1)], [0, 0, 1]);
-%             medianTS = newTS.median; % Gets the median skeleton of the markers
+% % Check for outliers:
+% % Do a data translation:
+% % refMarker = squeeze(mean(ts.getOrientedData({'LHIP','RHIP'}), 2)); % Assuming these markers exist
+% % Do a label-agnostic data translation:
+% refMarker = squeeze(nanmean(dd, 2));
+% newTS = ts.translate([-refMarker(:, 1:2), zeros(size(refMarker, 1), 1)]); % Assuming z is a known fixed axis
+% % Not agnostic rotation:
+% % relData = squeeze(markerData.getOrientedData('RHIP'));
+% % Label agnostic data rotation:
+% newTS = newTS.alignRotate([refMarker(:, 2), -refMarker(:, 1), zeros(size(refMarker, 1), 1)], [0, 0, 1]);
+% medianTS = newTS.median; % Gets the median skeleton of the markers
 %
-%             % With this median skeleton, a minimization can be done to
-%             % find another label agnostic data rotation that does not
-%             % depend on estimating the translation velocity:
+% % With this median skeleton, a minimization can be done to
+% % find another label agnostic data rotation that does not
+% % depend on estimating the translation velocity:
 %
-%             % Another attempt at label agnostic rotation (not using
-%             % velocity, but actually some info about the skeleton
-%             % having Left and Right)
-%             % l1 = cellfun(@(x) x(1:end - 1), ts.getLabelsThatMatch('^L'), 'UniformOutput', false);
-%             % l2 = cellfun(@(x) x(1:end - 1), ts.getLabelsThatMatch('^R'), 'UniformOutput', false);
-%             % relDataOTS = newTS.computeDifferenceOTS([], [], l1(1:3:end), l2(1:3:end));
-%             % relData = squeeze(nanmedian(relDataOTS.getOrientedData, 2)); % Need to work on this
+% % Another attempt at label agnostic rotation (not using
+% % velocity, but actually some info about the skeleton
+% % having Left and Right)
+% % l1 = cellfun(@(x) x(1:end - 1), ts.getLabelsThatMatch('^L'), 'UniformOutput', false);
+% % l2 = cellfun(@(x) x(1:end - 1), ts.getLabelsThatMatch('^R'), 'UniformOutput', false);
+% % relDataOTS = newTS.computeDifferenceOTS([], [], l1(1:3:end), l2(1:3:end));
+% % relData = squeeze(nanmedian(relDataOTS.getOrientedData, 2)); % Need to work on this
 %
+% % Try to fit a 2-cluster model, to see if some marker
+% % labels are switched at some point during experiment
 %
-%
-%             % Try to fit a 2-cluster model, to see if some marker
-%             % labels are switched at some point during experiment
-%
-%             % Assuming single mode/cluster, find outliers by getting
-%             % stats on distribution of positions/distance and
-%             % velocities.
+% % Assuming single mode/cluster, find outliers by getting
+% % stats on distribution of positions/distance and
+% % velocities.
 end
 
--- classes/dataStructs/@labData/split.m
+++ classes/dataStructs/@labData/split.m
@@ -21,75 +21,71 @@ function newThis = split(this, t0, t1, newClass)
 %
 %   Outputs:
 %       newThis - new object containing split data, of type
-%                 specified by newClass or same as input
+%                 specified by newClass or same as input (must be subclass)
 %
 %   See also: labTimeSeries/split
 
 % Split the data into [t0, t1).
 % Args:
-%   -t0: time in seconds (relative to trial start) of where to
-%        start the split (inclusive) when given NaN, default to
-%        start of the trial.
-%   -t1: time in seconds (relative to trial start) of where to
-%        stop (exclusive). OPTIONAL, when not provided or got
-%        NaN, default to end of the trial
+%   -t0: time in seconds (relative to trial start) of where to start the
+%        split (inclusive) when given NaN, default to start of the trial.
+%   -t1: time in seconds (relative to trial start) of where to stop
+%        (exclusive). OPTIONAL, when not provided or got NaN, default to
+%        end of the trial
 %   -newClass: string representing class/object type to return.
 %        OPTIONAL, default return the same type
+
 newThis = []; % Just to avoid Matlab saying this is not defined
 cname = class(this);
 if nargin < 4
     % (ID, date, experimenter, desc, obs, refLeg, parentMeta)
     metaData = derivedMetaData(labDate.genIDFromClock, ...
         labDate.getCurrent, 'labData.split', ...
-        ['Splice of ' this.metaData.description], ...
-        'Auto-generated', this.metaData.refLeg, ...
-        this.metaData); % HH removed 'Partial Interval' after
-    % labdata.split since 'type' propery
-    % was eliminated.
-    eval(['newThis = ' cname '(metaData);']); % Call empty
-    % constructor of
-    % same class
+        ['Splice of ' this.metaData.description], 'Auto-generated', ...
+        this.metaData.refLeg, this.metaData);
+    % HH removed 'Partial Interval' after labdata.split since 'type'
+    % property was eliminated.
+    eval(['newThis = ' cname '(metaData);']);
+    % Call empty constructor of same class
 else
     % Should I call a different metaData constructor
     metaData = strideMetaData(labDate.genIDFromClock, ...
         labDate.getCurrent, 'labData.split', ...
-        ['Splice of ' this.metaData.description], ...
-        'Auto-generated', this.metaData.refLeg, ...
-        this.metaData);
-    eval(['newThis = ' newClass '(metaData);']); % Call empty
-    % constructor
-    % of same class
+        ['Splice of ' this.metaData.description], 'Auto-generated', ...
+        this.metaData.refLeg, this.metaData);
+    eval(['newThis = ' newClass '(metaData);']);
+    % Call empty constructor of same class
 end
+
 auxLst = properties(cname);
 for i = 1:length(auxLst)
-    % Should try to do this only if the property is not
-    % dependent, otherwise, I'm computing things I don't need
+    % Should try to do this only if the property is not dependent;
+    % otherwise, I'm computing things I don't need
     eval(['oldVal = this.' auxLst{i} ';'])
     if isa(oldVal, 'labTimeSeries') && ...
             ~isa(oldVal, 'parameterSeries')
-        % no end time point provided, assume spliting from t0 to
-        % end of the trial
+        % no end time point provided, assume spliting from t0 to end of the
+        % trial
         if nargin < 3 || isnan(t1)
             tEnd = oldVal.Time(end) + oldVal.sampPeriod;
         else % end point provided use it
             tEnd = t1;
         end
-        % a flag/fake initial time provided, assuming split from
-        % beginning to to t1
+        % a flag/fake initial time provided, assuming split from beginning
+        % to t1
         if isnan(t0)
             % start is inclusive, no need to pad
             tStart = oldVal.Time(1);
         else % end point provided use it.
             tStart = t0;
         end
-        % Calling labTS.split (or one of the subclass'
-        % implementation)
+        % Calling labTS.split (or one of the subclass' implementation)
         newVal = oldVal.split(tStart, tEnd);
     elseif ~isa(oldVal, 'labMetaData')
         newVal = oldVal; % Not a labTS object, not splitting
     end
     try
-        % If this fails is because the property is not settable
+        % If this fails it is because the property is not settable
         eval(['newThis.' auxLst{i} ' = newVal;'])
     catch
 
