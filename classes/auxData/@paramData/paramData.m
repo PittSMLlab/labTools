ec26446410b4134d3ea3523b752e5094bf5d714c
nwb18 <nwbrantly@pitt.edu>
2026-02-05 11:04:15 -0500

    Update class formatting
    Use Claude AI and MATLAB's autoformatting tool to consistently format the 'paramData' class using conventional MATLAB whitespace formatting for consistency and clarity. Add class documentation comment block. Use MATLAB autoformatting on class file to facilitate future reorganization. NOTE that there are no functional changes in this commit, only aesthetic changes. NWB

--- classes/auxData/@paramData/paramData.m
+++ classes/auxData/@paramData/paramData.m
@@ -1,20 +1,42 @@
 classdef paramData
-    %This may no longer be in use...
-    
+    %paramData  Container for parameter data across trials
+    %
+    %   paramData stores adaptation parameters organized by label, along
+    %   with trial indices and types. This class may no longer be in
+    %   active use.
+    %
+    %paramData properties:
+    %   labels - cell array of parameter names
+    %   Data - matrix of parameter values (strides/samples x parameters)
+    %   indsInTrial - cell array of indices indicating which data points
+    %                 belong to each trial
+    %   trialTypes - cell array of trial type identifiers
+    %
+    %paramData methods:
+    %   paramData - constructor for parameter data
+    %   getParameter - retrieves data for specified parameter(s)
+    %   isaParameter - checks if parameter(s) exist in dataset
+    %   isaLabel - checks if label(s) exist in dataset
+    %   appendData - adds new parameters to dataset
+    %
+    %Note: This class may no longer be in use
+    %
+    %See also: parameterSeries, adaptationData
+
     %%
     properties %(SetAccess=private)
         labels={''};
-        Data;       
+        Data;
         indsInTrial={};
         trialTypes={};
     end
     properties(Dependent)
         %could include things here like 'learning' or 'transfer'...
     end
-    
+
     %%
     methods
-        
+
         %Constructor:
         function this=paramData(data,labels,inds,types)
             if (length(labels)==size(data,2)) && isa(labels,'cell')
@@ -31,24 +53,24 @@ classdef paramData
                 throw(ME)
             end
             if nargin>3 && isa(types,'cell')
-                this.trialTypes=types;            
+                this.trialTypes=types;
             end
         end
-        
+
         %-------------------
-        
+
         %Other I/O functions:
         function [data,auxLabel]=getParameter(this,label)
             if isa(label,'char')
                 auxLabel={label};
             else
                 auxLabel=label;
-            end            
-            [boolFlag,labelIdx]=this.isaParameter(auxLabel);                      
+            end
+            [boolFlag,labelIdx]=this.isaParameter(auxLabel);
             data=this.Data(:,labelIdx(boolFlag==1));
             auxLabel=this.labels(labelIdx(boolFlag==1));
-        end        
-        
+        end
+
         function [boolFlag,labelIdx]=isaParameter(this,label)
             if isa(label,'char')
                 auxLabel{1}=label;
@@ -56,41 +78,41 @@ classdef paramData
                 auxLabel=label;
             end
             [boolFlag, labelIdx]=isaLabel(this,label);
-             for i=1:length(boolFlag)
+            for i=1:length(boolFlag)
                 if boolFlag(i)==0
                     warning(['Label ' auxLabel{i} ' is not a parameter in this dataset.'])
                 end
-            end 
-        end          
-       
-        function [boolFlag,labelIdx]=isaLabel(this,label)
-        if isa(label,'char')
-            auxLabel{1}=label;
-        elseif isa(label,'cell')
-            auxLabel=label;
-        end
-
-        N=length(auxLabel);
-        boolFlag=false(N,1);
-        labelIdx=zeros(N,1);
-        for j=1:N
-            for i=1:length(this.labels)
-                 if strcmpi(auxLabel{j},this.labels{i})
-                   boolFlag(j)=true;
-                   labelIdx(j)=i;
-                   break;
-                 end
             end
         end
+
+        function [boolFlag,labelIdx]=isaLabel(this,label)
+            if isa(label,'char')
+                auxLabel{1}=label;
+            elseif isa(label,'cell')
+                auxLabel=label;
+            end
+
+            N=length(auxLabel);
+            boolFlag=false(N,1);
+            labelIdx=zeros(N,1);
+            for j=1:N
+                for i=1:length(this.labels)
+                    if strcmpi(auxLabel{j},this.labels{i})
+                        boolFlag(j)=true;
+                        labelIdx(j)=i;
+                        break;
+                    end
+                end
+            end
         end
-        
+
         % Modifiers:
         function newThis=appendData(this,newData,newLabels)
             newThis=paramData([this.Data newData],[this.labels newLabels],this.indsInTrial,this.trialTypes);
         end
     end
-    
-    
 
-        
+
+
+
 end
