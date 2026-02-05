f13d692c365143a419b6137bdc43faae74625909
nwb18 <nwbrantly@pitt.edu>
2026-02-05 10:12:24 -0500

    Update class formatting
    Use Claude AI and MATLAB's autoformatting tool to consistently format the 'cognitiveData' class using conventional MATLAB whitespace formatting for consistency and clarity. Remove method code from main class file after verifying separate method files contain the same exact code reformatted, and replace with the corresponding method prototypes. NOTE that there are no functional changes in this commit, only aesthetic changes. NWB

--- classes/auxData/@cognitiveData/cognitiveData.m
+++ classes/auxData/@cognitiveData/cognitiveData.m
@@ -55,43 +55,9 @@ classdef cognitiveData
 
     %% Data Access Methods
     methods
-        function [score,auxLabel]=getScore(this,label)
-            if nargin<2 || isempty(label)
-                label=this.labels;
-            end
-            if isa(label,'char')
-                auxLabel={label};
-            else
-                auxLabel=label;
-            end
-            [boolFlag,labelIdx]=this.isaLabel(auxLabel);
-            for i=1:length(boolFlag)
-                if boolFlag(i)==0
-                    warning(['Label ' auxLabel{i} ' is not a labeled value in this data set.'])
-                end
-            end
+        [score, auxLabel] = getScore(this, label)
 
-            score=this.scores(:,labelIdx(boolFlag==1));
-            auxLabel=this.labels(labelIdx(boolFlag==1));
-        end
-
-        function [boolFlag,labelIdx]=isaLabel(this,label)
-            if isa(label,'char')
-                auxLabel{1}=label;
-            elseif isa(label,'cell')
-                auxLabel=label;
-            else
-                error('labTimeSeries:isaLabel','label input argument has to be a string or a cell array containing strings.')
-            end
-            N=length(auxLabel);
-            boolFlag=false(N,1);
-            labelIdx=zeros(N,1);
-            for j=1:N
-                aux=strcmp(auxLabel{j},this.labels);
-                boolFlag(j)=any(aux);
-                labelIdx(j)=find(aux);
-            end
-        end
+        [boolFlag, labelIdx] = isaLabel(this, label)
     end
 
 end
