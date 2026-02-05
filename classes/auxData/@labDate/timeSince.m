5b6d6f1a98653c6885b56f2fa869d66cf634b582
nwb18 <nwbrantly@pitt.edu>
2026-02-05 10:15:55 -0500

    Add separate method files
    Add separate files for the major methods of the 'labDate' class to improve organization and facilitate future code maintenance. NWB

---
+++ classes/auxData/@labDate/timeSince.m
@@ -0,0 +1,31 @@
+function timeInMonths = timeSince(this, other)
+%timeSince  Calculates elapsed time from another date
+%
+%   timeInMonths = timeSince(this, other) calculates the time elapsed
+%   between two dates in months
+%
+%   Inputs:
+%       this - labDate object (later date)
+%       other - labDate object (earlier date)
+%
+%   Outputs:
+%       timeInMonths - elapsed time in months (approximate, assumes
+%                      30 days per month)
+%
+%   Note: This method approximates fractional months using 30 days per
+%         month
+%
+%   Example:
+%       d1 = labDate(1, 'jan', 2015);
+%       d2 = labDate(1, 'jun', 2015);
+%       months = d2.timeSince(d1)  % returns 5
+%
+%   See also: isempty
+
+% Suggested method: find number of years/months/days that separate two
+% dates. The method could be called like
+% Returns elapsed time in MONTHS
+timeInMonths = 12 * (this.year - other.year) + ...
+    (this.month - other.month) + (this.day - other.day) / 30;
+end
+
