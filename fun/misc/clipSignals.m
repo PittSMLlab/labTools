933371efd21245d1ce5706e8c9054b0ea52d1ad7
nwb18 <nwbrantly@pitt.edu>
2026-03-15 16:12:50 -0400

    Update formatting
    Update 'clipSignals' function formatting for consistent whitespace and readability. NOTE that there are no function code changes in this commit, only aesthetic changes. NWB

--- fun/misc/clipSignals.m
+++ fun/misc/clipSignals.m
@@ -25,12 +25,11 @@ function signals = clipSignals(signals, percentile)
 %
 %   See also: loadTrials, prctile
 
-for i=1:size(signals,2)
-    lims=prctile(signals(:,i),[percentile,100-percentile]);
-    signals(signals(:,i)<lims(1),i)=lims(1);
-    signals(signals(:,i)>lims(2),i)=lims(2);
+for i = 1:size(signals, 2)
+    lims = prctile(signals(:, i), [percentile, 100 - percentile]);
+    signals(signals(:, i) < lims(1), i) = lims(1);
+    signals(signals(:, i) > lims(2), i) = lims(2);
 end
-
 
 end
 
