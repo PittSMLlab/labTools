2915158bef6819db826423314d1e79545c1debe2
nwb18 <nwbrantly@pitt.edu>
2026-03-31 14:40:49 -0400

    Add arguments block
    Add 'arguments' block for input argument type validation. Update function documentation comment block to be more comprehensive and use the same formatting as other recently updated functions. Update function formatting for consistency. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/parameterCalculation/computeAngleParameters.m
+++ fun/parameterCalculation/computeAngleParameters.m
@@ -1,17 +1,41 @@
-function out = computeAngleParameters(angleData,gaitEvents,slowLeg, ...
+function out = computeAngleParameters(angleData, gaitEvents, slowLeg, ...
     eventTypes)
-%This function computes summary parameters per stride for the angle data
-%   This function outputs a 'parameterSeries' object, which can be
-% concatenated with other 'parameterSeries' objects, for example, with
-% those from 'computeTemporalParameters'. While this function is used for
-% angle parameters exclusively, it should work for any 'labTS' object.
+% computeAngleParameters  Compute joint angle parameters per stride.
 %
-% See also computeSpatialParameters, computeTemporalParameters,
-% computeForceParameters, parameterSeries
+%   Computes stride-by-stride joint angle parameters and returns a
+% parameterSeries object that can be concatenated with other parameter
+% series objects (e.g., from computeTemporalParameters).
+%
+%   Inputs:
+%     angleData  - labTimeSeries containing limb angle data, with
+%                  channel labels prefixed by leg side ('L' or 'R')
+%     gaitEvents - labTimeSeries of gait events for the trial
+%     slowLeg    - Char specifying the slow-belt leg ('L' or 'R')
+%     eventTypes - Cell array of gait event type strings as constructed
+%                  in calcParameters (e.g., {'LHS','RTO','RHS','LTO'})
+%
+%   Outputs:
+%     out - parameterSeries object containing all angle parameters,
+%           including discrete stride-level measures and heel strike
+%           angle parameters
+%
+%   Toolbox Dependencies:
+%     None
+%
+%   See also: computeTemporalParameters, computeSpatialParameters,
+%     computeForceParameters, computeTSdiscreteParameters,
+%     computeHSparameters, parameterSeries, calcParameters
 
 %% Preprocess Data
 % update labeling as slow vs. fast rather than left vs. right
 lS = angleData.getLabelsThatMatch(['^' slowLeg]);
+arguments
+    angleData  (1,1)
+    gaitEvents (1,1)
+    slowLeg    (1,:) char
+    eventTypes (1,:) cell
+end
+
 fastLeg = getOtherLeg(slowLeg);
 lF = angleData.getLabelsThatMatch(['^' fastLeg]);
 % temporarily silence 'renameLabels' warning message
