95bac85f16b579e8e9940c011ef5fc4f7cc50547
nwb18 <nwbrantly@pitt.edu>
2026-05-07 16:29:13 -0400

    Reformat function
    Reformat function using Claude Code for consistency with all other recently formatted code. NOTE that this code has been tested to verify that there are no differences in the output parameters after compared to before the change. NWB

--- fun/eventExtraction/getPerceptualEventsFromCues.m
+++ fun/eventExtraction/getPerceptualEventsFromCues.m
@@ -1,4 +1,5 @@
-function [LHSstartCue, LHSstopCue, RHSstartCue, RHSstopCue] = getPerceptualEventsFromCues(datlog, infoLHSevent, infoRHSevent) 
+function [LHSstartCue, LHSstopCue, RHSstartCue, RHSstopCue] = ...
+    getPerceptualEventsFromCues(datlog, infoLHSevent, infoRHSevent)
 %GETPERCEPTUALEVENTSFROMCUES Map auditory cue times to nearest gait events.
 %
 %   Extracts perceptual trial start/stop cue times from the datalog and
@@ -23,27 +24,24 @@ function [LHSstartCue, LHSstopCue, RHSstartCue, RHSstopCue] = getPerceptualEvent
 
 % grab auditory cue times from the datalog, offset by synchronization
 startCue = datlog.audioCues.start + datlog.dataLogTimeOffsetBest;
-stopCue = datlog.audioCues.stop + datlog.dataLogTimeOffsetBest;
+stopCue  = datlog.audioCues.stop  + datlog.dataLogTimeOffsetBest;
 
-%% Compare the start and stop cue times to the events data to match the start and stop of perceptual trial 
-LHSstartCue = zeros(length(infoLHSevent),1); 
-RHSstartCue = zeros(length(infoRHSevent),1); 
-LHSstopCue = zeros(length(infoLHSevent),1); 
-RHSstopCue = zeros(length(infoRHSevent),1); 
+%% Match cue times to nearest gait events
+LHSstartCue = zeros(length(infoLHSevent), 1);
+RHSstartCue = zeros(length(infoRHSevent), 1);
+LHSstopCue  = zeros(length(infoLHSevent), 1);
+RHSstopCue  = zeros(length(infoRHSevent), 1);
 
-    for t=1:length(startCue)
-    
-        [~,LidxI]=min(abs(infoLHSevent - startCue(t)));
-        [~,LidxF]=min(abs(infoLHSevent - stopCue(t)));
-        LHSstartCue(LidxI) = infoLHSevent(LidxI);
-        LHSstopCue(LidxF) = infoLHSevent(LidxF);
-    
-        [~,RidxI]=min(abs(infoRHSevent - startCue(t)));
-        [~,RidxF]=min(abs(infoRHSevent - stopCue(t)));
-        RHSstartCue(RidxI) =  infoRHSevent(RidxI);
-        RHSstopCue(RidxF) = infoRHSevent(RidxF);
-    
-    end
+for ii = 1:length(startCue)
+    [~, LidxI] = min(abs(infoLHSevent - startCue(ii)));
+    [~, LidxF] = min(abs(infoLHSevent - stopCue(ii)));
+    LHSstartCue(LidxI) = infoLHSevent(LidxI);
+    LHSstopCue(LidxF)  = infoLHSevent(LidxF);
 
+    [~, RidxI] = min(abs(infoRHSevent - startCue(ii)));
+    [~, RidxF] = min(abs(infoRHSevent - stopCue(ii)));
+    RHSstartCue(RidxI) = infoRHSevent(RidxI);
+    RHSstopCue(RidxF)  = infoRHSevent(RidxF);
 end
 
+end
