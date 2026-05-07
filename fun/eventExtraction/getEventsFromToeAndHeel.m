0b9b9b28514cc28244705fe4ad1bf155c802c611
nwb18 <nwbrantly@pitt.edu>
2026-05-07 15:15:30 -0400

    Reformat function
    Reformat function using Claude Code for consistency with all other recently formatted code. Rename 'aux' placeholder variable names for clarity. NOTE that this code has been tested to verify that there are no differences in the output parameters after compared to before the change. NWB

--- fun/eventExtraction/getEventsFromToeAndHeel.m
+++ fun/eventExtraction/getEventsFromToeAndHeel.m
@@ -1,5 +1,5 @@
-function [LHS,RHS,LTO,RTO] = ...
-    getEventsFromToeAndHeel(Ltoe,Lheel,Rtoe,Rheel,fsample)
+function [LHS, RHS, LTO, RTO] = ...
+    getEventsFromToeAndHeel(Ltoe, Lheel, Rtoe, Rheel, fsample)
 %GETEVENTSFROMTOENANDHEEL Detect gait events from toe and heel marker data.
 %
 %   Estimates stance phases from heel and toe marker kinematics, derives
@@ -23,27 +23,30 @@ function [LHS,RHS,LTO,RTO] = ...
 %
 % See also GETSTANCEFROMTOENANDHEEL, GETEVENTSFROMSTANCE.
 
-% retrieve stance gait phases from toe and heel
-stanceL = getStanceFromToeAndHeel(Lheel,Ltoe,fsample);
-stanceR = getStanceFromToeAndHeel(Rheel,Rtoe,fsample);
+% retrieve stance gait phases from toe and heel markers
+stanceL = getStanceFromToeAndHeel(Lheel, Ltoe, fsample);
+stanceR = getStanceFromToeAndHeel(Rheel, Rtoe, fsample);
 
 % retrieve gait events from stance phase
-[LHS,RHS,LTO,RTO] = getEventsFromStance(stanceL,stanceR);
-badInds = any(isnan(Lheel')) | any(isnan(Ltoe')) | ...
-    any(isnan(Rheel')) | any(isnan(Rtoe'));
+[LHS, RHS, LTO, RTO] = getEventsFromStance(stanceL, stanceR);
+badInds = any(isnan(Lheel')) | any(isnan(Ltoe')) ...
+    | any(isnan(Rheel')) | any(isnan(Rtoe'));
 LHS(badInds) = false;
 RHS(badInds) = false;
 LTO(badInds) = false;
 RTO(badInds) = false;
 
-% eliminate any events that ocurr prior to actual activity in the trial
-aux = Rheel(:,1) - Rheel(1,1);  % zero initial value (right leg)
-aux2 = cumsum(aux .^ 2);        % cumulative energy
-% first element whose cumulative energy is at least 0.1% of total energy
-begin_index1 = find(aux2 > 0.001*aux2(end),1);
-aux = Lheel(:,1) - Lheel(1,1);  % zero initial value (left leg)
-aux2 = cumsum(aux .^ 2);        % cumulative energy
-begin_index2 = find(aux2 > 0.001*aux2(end),1);
+% eliminate any events that occur prior to actual activity in the trial
+ENERGY_FRAC = 0.001; % 0.1% of total cumulative energy used as onset threshold
+
+rHeelShifted   = Rheel(:, 1) - Rheel(1, 1); % zero initial value (right)
+rHeelCumEnergy = cumsum(rHeelShifted .^ 2);
+begin_index1   = find(rHeelCumEnergy > ENERGY_FRAC * rHeelCumEnergy(end), 1);
+
+lHeelShifted   = Lheel(:, 1) - Lheel(1, 1); % zero initial value (left)
+lHeelCumEnergy = cumsum(lHeelShifted .^ 2);
+begin_index2   = find(lHeelCumEnergy > ENERGY_FRAC * lHeelCumEnergy(end), 1);
+
 begin_index = max([begin_index1 begin_index2]);
 
 LHS(1:begin_index) = false;
@@ -55,4 +58,3 @@ RTO(1:begin_index) = false;
 % consistent = checkEventConsistency(LHS,RHS,LTO,RTO);
 
 end
-
