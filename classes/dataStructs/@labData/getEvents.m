fb0eb4f02021c7f951b1370ee80a383818bd3797
nwb18 <nwbrantly@pitt.edu>
2026-05-08 14:43:09 -0400

    Reformat functions
    Reformat functions using Claude Code for consistency with all other recently formatted code. Fix bug with 'RtoePos' variable incorrectly set with 'LTOE' marker. NOTE that this code has been tested to verify that there are no differences in the output parameters after compared to before the change. NWB

--- classes/dataStructs/@labData/getEvents.m
+++ classes/dataStructs/@labData/getEvents.m
@@ -1,39 +1,3 @@
-    if isempty(trialData.GRFData) || isempty(trialData.GRFData.Data) %No force data, default events calculatd from marker data (not labeled as kin events though!!)
-        disp(['No ground reaction forces data in ' file '. Using marker data to compute events.'])
-
-        %         LtoePos=trialData.getMarkerData({['LTOE' orientation.foreaftAxis],['LTOE' orientation.updownAxis],['LTOE' orientation.sideAxis]});
-        %         LtoePos=[orientation.foreaftSign* LtoePos(:,1),orientation.updownSign*LtoePos(:,2),orientation.sideSign*LtoePos(:,3)];
-        %         RtoePos=trialData.getMarkerData({['RTOE' orientation.foreaftAxis],['RTOE' orientation.updownAxis],['RTOE' orientation.sideAxis]});
-        %         RtoePos=[orientation.foreaftSign* RtoePos(:,1),orientation.sideSign*RtoePos(:,2),orientation.updownSign*RtoePos(:,3)];
-
-        LtoePos = trialData.markerData.getDataAsVector({'LTOE'});
-        RtoePos = trialData.markerData.getDataAsVector({'LTOE'});
-
-        %         LanklePos=trialData.getMarkerData({['LANK' orientation.foreaftAxis],['LANK' orientation.updownAxis],['LANK' orientation.sideAxis]});
-        %         LanklePos=[orientation.foreaftSign* LanklePos(:,1),orientation.sideSign*LanklePos(:,2),orientation.updownSign*LanklePos(:,3)];
-        %         RanklePos=trialData.getMarkerData({['RANK' orientation.foreaftAxis],['RANK' orientation.updownAxis],['RANK' orientation.sideAxis]});
-        %         RanklePos=[orientation.foreaftSign* RanklePos(:,1),orientation.sideSign*RanklePos(:,2),orientation.updownSign*RanklePos(:,3)];
-
-        LanklePos = trialData.markerData.getDataAsVector({'LANK'});
-        RanklePos = trialData.markerData.getDataAsVector({'RANK'});
-
-        if trialData.markerData.isaLabel('LHEEx')
-            %             LheelPos=trialData.getMarkerData({['LHEE' orientation.foreaftAxis],['LHEE' orientation.updownAxis],['LHEE' orientation.sideAxis]});
-            %             LheelPos=[orientation.foreaftSign* LheelPos(:,1),orientation.sideSign*LheelPos(:,2),orientation.updownSign*LheelPos(:,3)];
-            %             RheelPos=trialData.getMarkerData({['RHEE' orientation.foreaftAxis],['RHEE' orientation.updownAxis],['RHEE' orientation.sideAxis]});
-            %             RheelPos=[orientation.foreaftSign* RheelPos(:,1),orientation.sideSign*RheelPos(:,2),orientation.updownSign*RheelPos(:,3)];
-
-            LheelPos = trialData.markerData.getDataAsVector({'LHEE'});
-            RheelPos = trialData.markerData.getDataAsVector({'RHEE'});
-
-        else
-            disp(['No heel markers in ' file '. Using ankle markers instead to compute events.'])
-            LheelPos=LanklePos;
-            RheelPos=RanklePos;
-        end
-        fs_kin=trialData.markerData.sampFreq;
-        [LHSevent,RHSevent,LTOevent,RTOevent] = getEventsFromToeAndHeel(LtoePos,LheelPos,RtoePos,RheelPos,fs_kin); %EVENTS from a mix of kinematics;
-
         LHSeventForce=false(length(LHSevent),1); %No force events, fill with logical 0's
         RHSeventForce=false(length(RHSevent),1);
         LTOeventForce=false(length(LTOevent),1);
@@ -209,4 +173,43 @@ if strcmpi(trialData.metaData.type, 'OG') ...
     %     t0=trialData.GRFData.Time(1);
     %     Ts=trialData.GRFData.sampPeriod;
 else % treadmill trial
+
+    if isempty(trialData.GRFData) || isempty(trialData.GRFData.Data) % no force data: use marker kinematics
+        disp(['No ground reaction forces data in ' trialFileName ...
+            '. Using marker data to compute events.'])
+
+        %         LtoePos=trialData.getMarkerData({['LTOE' orientation.foreaftAxis],['LTOE' orientation.updownAxis],['LTOE' orientation.sideAxis]});
+        %         LtoePos=[orientation.foreaftSign* LtoePos(:,1),orientation.updownSign*LtoePos(:,2),orientation.sideSign*LtoePos(:,3)];
+        %         RtoePos=trialData.getMarkerData({['RTOE' orientation.foreaftAxis],['RTOE' orientation.updownAxis],['RTOE' orientation.sideAxis]});
+        %         RtoePos=[orientation.foreaftSign* RtoePos(:,1),orientation.sideSign*RtoePos(:,2),orientation.updownSign*RtoePos(:,3)];
+
+        LtoePos = trialData.markerData.getDataAsVector({'LTOE'});
+        RtoePos = trialData.markerData.getDataAsVector({'RTOE'}); % was incorrectly 'LTOE' before
+
+        %         LanklePos=trialData.getMarkerData({['LANK' orientation.foreaftAxis],['LANK' orientation.updownAxis],['LANK' orientation.sideAxis]});
+        %         LanklePos=[orientation.foreaftSign* LanklePos(:,1),orientation.sideSign*LanklePos(:,2),orientation.updownSign*LanklePos(:,3)];
+        %         RanklePos=trialData.getMarkerData({['RANK' orientation.foreaftAxis],['RANK' orientation.updownAxis],['RANK' orientation.sideAxis]});
+        %         RanklePos=[orientation.foreaftSign* RanklePos(:,1),orientation.sideSign*RanklePos(:,2),orientation.updownSign*RanklePos(:,3)];
+
+        LanklePos = trialData.markerData.getDataAsVector({'LANK'});
+        RanklePos = trialData.markerData.getDataAsVector({'RANK'});
+
+        if trialData.markerData.isaLabel('LHEEx')
+            %             LheelPos=trialData.getMarkerData({['LHEE' orientation.foreaftAxis],['LHEE' orientation.updownAxis],['LHEE' orientation.sideAxis]});
+            %             LheelPos=[orientation.foreaftSign* LheelPos(:,1),orientation.sideSign*LheelPos(:,2),orientation.updownSign*LheelPos(:,3)];
+            %             RheelPos=trialData.getMarkerData({['RHEE' orientation.foreaftAxis],['RHEE' orientation.updownAxis],['RHEE' orientation.sideAxis]});
+            %             RheelPos=[orientation.foreaftSign* RheelPos(:,1),orientation.sideSign*RheelPos(:,2),orientation.updownSign*RheelPos(:,3)];
+
+            LheelPos = trialData.markerData.getDataAsVector({'LHEE'});
+            RheelPos = trialData.markerData.getDataAsVector({'RHEE'});
+
+        else
+            disp(['No heel markers in ' trialFileName ...
+                '. Using ankle markers instead to compute events.'])
+            LheelPos = LanklePos;
+            RheelPos = RanklePos;
+        end
+        fs_kin = trialData.markerData.sampFreq;
+        [LHSevent, RHSevent, LTOevent, RTOevent] = ...
+            getEventsFromToeAndHeel(LtoePos, LheelPos, RtoePos, RheelPos, fs_kin);
 
