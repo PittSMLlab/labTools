2f9fa8efe5e00195adc1a7f7d18c1b5b95b1d061
nwb18 <nwbrantly@pitt.edu>
2026-05-12 09:41:53 -0400

    Remove deprecated code
    Remove deprecated, unused (since overwritten by final line) code using Claude Code to simplify function. NWB

--- fun/ext/pitools/signalProcessing/filtfilthd.m
+++ fun/ext/pitools/signalProcessing/filtfilthd.m
@@ -1,18 +1,3 @@
-%Deprecate on May 6th 2018
-
-%Filters data along dim=1 with filterObj first forwards, and then
-%backwards.
-%It is an implementation of filtfilt that works with filter objects from
-%the DSP toolbox.
-%Uses 'reflect' method for dealing with borders.
-
-if size(data,1)==1 
-    warning('filtfiltHD expects input data to be entered as columns, transposing')
-    data=data';
-end
-if size(data,1)<size(data,2)
-    warning('Input data seems to be organized as rows, and filtfilthd filters along columns.')
-end
 function filteredData = filtfilthd(filterObj, data, method)
 %FILTFILTHD Zero-phase filter with reflective boundary handling.
 %
@@ -31,25 +16,12 @@ function filteredData = filtfilthd(filterObj, data, method)
 %
 % See also FILTFILTHD_SHORT.
 
-M=size(data,1);
 warning('filtfilthd:deprecated', ...
     'Use filtfilthd_short directly. filtfilthd will be removed.');
 
 if nargin < 3
     method = 'reflect';
 end
-    switch method
-        case 'reflect'
-            pre=[data(end:-1:1,:)];
-            post=[data(end:-1:1,:)];
-        otherwise         
-            pre=[];
-            post=[];
-    end
-filteredData=filter(filterObj,[pre;data;post]);
-filteredData=filter(filterObj,filteredData(end:-1:1,:));
-filteredData=filteredData(end:-1:1,:);
-filteredData=filteredData([size(pre,1)+1:size(pre,1)+M],:);
 filteredData = filtfilthd_short(filterObj, data, method, []);
 
 end
