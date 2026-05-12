2353caa8671c71f8dc35a1699c46746d6e5af019
nwb18 <nwbrantly@pitt.edu>
2026-05-11 20:39:24 -0400

    Delete commented code
    Delete code block that has been commented and unused since 2015 (over ten years ago) using Claude Code and update explanatory comment block. NWB

--- fun/eventExtraction/getEventsFromForces.m
+++ fun/eventExtraction/getEventsFromForces.m
@@ -38,28 +38,10 @@ stanceR = getStanceFromForces(FzR, forceThresh, fsample);
 %% Get events from stance
 [LHS, RHS, LTO, RTO] = getEventsFromStance(stanceL, stanceR);
 
-%% Eliminate any events that occur prior to actual activity in the trial
-% This section was commented out by Pablo on 2/20/2015 because it led to
-% the very first step not being properly detected, which can be a big
-% problem.
-
-% Rheel=FzR;
-% Lheel=FzL;
-% aux=Rheel(:,1)-Rheel(1,1); %Zero initial value
-% aux2=cumsum(aux.^2); %Cumulative energy
-% begin_index1=find(aux2>.001*aux2(end),1); %First element whose cumulative energy is at least .1% of total energy
-% aux=Lheel(:,1)-Lheel(1,1); %Zero initial value
-% aux2=cumsum(aux.^2); %Cumulative energy
-% begin_index2=find(aux2>.001*aux2(end),1); %First element whose cumulative energy is at least .1% of total energy
-%
-% begin_index=max([begin_index1,begin_index2]);
-%
-% LHS(1:begin_index)=false;
-% RHS(1:begin_index)=false;
-% LTO(1:begin_index)=false;
-% RTO(1:begin_index)=false;
-
 %% Check consistency
 %[consistent] = checkEventConsistency(LHS,RHS,LTO,RTO);
+% NOTE: energy-onset masking (removing events before first GRF activity)
+% was removed because it suppressed the very first step of a trial, which
+% is often the most important event for adaptation analysis (Pablo, 2015).
 
 end
