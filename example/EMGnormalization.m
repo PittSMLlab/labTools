b8734ca902258a103de5c83a58875a8bde7aaad8
nwb18 <nwbrantly@pitt.edu>
2026-05-17 20:14:05 -0400

    Reformat script
    Reformat 'example' script using Claude Code for consistency with other recently reformatted code. NWB

--- example/EMGnormalization.m
+++ example/EMGnormalization.m
@@ -4,25 +4,25 @@
 % adaptationData.normalizeToBaseline. Assumes an adaptationData object
 % named 'adaptData' exists in the workspace with EMG parameter data.
 
-%% Load adaptData object
+%% Define muscle list
+mList = {'TA', 'PER', 'MG', 'LG', 'SOL', 'BF', 'SEMT', 'SEMB', ...
+    'VM', 'VL', 'RF', 'ADM', 'HIP', 'TFL', 'GLU'};
 
-%From here on we assume that an adaptationData object of name 'adaptData'
-%exists in workspace, and contains some EMG data
+% Expand to both sides of body; 's' suffix selects the 12-phase EMG
+% amplitude parameters (see computeEMGparameters for parameter names).
+expandedMList = [strcat('f', strcat(mList, 's')) ...
+    strcat('s', strcat(mList, 's'))];
 
-%%
-%Define list of muscles I care about:
-mList={'TA','PER','MG','LG','SOL','BF','SEMT','SEMB','VM','VL','RF','ADM','HIP','TFL','GLU'};
+%% Normalize to baseline
+test = adaptData.normalizeToBaseline(expandedMList);
 
-%Expand list to include both sides of body, and append 's' suffix to
-%clarify I want the amplitude of EMG divided in 12 phases (see
-%computeEMGparameters to get the details on each EMG parameter in
-%existence)
-expandedMList=[strcat('f',strcat(mList,'s')) strcat('s',strcat(mList,'s'))];
+%% Plot normalized data
+aa  = test.data.getDataAsVector( ...
+    test.data.getLabelsThatMatch('^Norm'));
+idx = test.getIndsInCondition('TM base');
 
-%Do the normalization:
-test=adaptData.normalizeToBaseline(expandedMList);
-
-%Plot to check:
-aa=test.data.getDataAsVector(test.data.getLabelsThatMatch('^Norm'));
-idx=test.getIndsInCondition('TM base');
-figure; surf(reshape(nanmean(aa(idx{1},:)),[12,30]),'EdgeColor','none'); caxis([-1 1]);
+nPhases = 12;  % 12-phase EMG amplitude (see computeEMGparameters)
+figure;
+surf(reshape(mean(aa(idx{1}, :), 'omitnan'), ...
+    [nPhases, numel(expandedMList)]), 'EdgeColor', 'None');
+caxis([-1 1]);
