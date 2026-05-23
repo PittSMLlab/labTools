2895054e11c22255941733f8f7b1ab008b5a4a12
nwb18 <nwbrantly@pitt.edu>
2026-05-23 13:59:43 -0400

    Reformat function
    Reformat function using Claude Code to align with coding style conventions for clarity and readability. Suppress unused function call output. Simplify code by combining code lines outside of the conditional statement. Remove unnecessary workspace variable clearing at the end of the function (all variables are local in scope regardless). NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/calcangle.m
+++ fun/misc/calcangle.m
@@ -1,23 +1,5 @@
-function angle=calcangle(jt1, jt2, jt3)
-      vector1=jt2-jt1;
-      vector2=jt2-jt3;
-      dotprod=dot(vector1,vector2,2); %find dot product in 2D
-      [rr, cc]=size(jt1);
-      
-      if cc==3 %3D
-       len1=sqrt((vector1(:,1).^2)+(vector1(:,2).^2)+(vector1(:,3).^2));
-       len2=sqrt((vector2(:,1).^2)+(vector2(:,2).^2)+(vector2(:,3).^2));
-       theangle=acos(dotprod./(len1.*len2));
-      else %2D
-       len1=sqrt((vector1(:,1).^2)+(vector1(:,2).^2));
-       len2=sqrt((vector2(:,1).^2)+(vector2(:,2).^2));
-       theangle=acos(dotprod./(len1.*len2));
-      end;
-      angle=theangle*(180/pi);
-              
-      
-
-    clear vector1 vector2 dotprod jt1 jt2 jt3 rr cc len1 len2 tempang theangle%CALCANGLE Compute the joint angle at the vertex joint.
+function angle = calcangle(jt1, jt2, jt3)
+%CALCANGLE Compute the joint angle at the vertex joint.
 %
 %   Calculates the angle in degrees between vectors formed by three
 % joint positions. JT2 is the vertex. Accepts both 2D (N×2) and 3D
@@ -34,3 +16,17 @@ function angle=calcangle(jt1, jt2, jt3)
 % Toolbox Dependencies: None
 %
 % See also DOT, ACOS.
+vector1 = jt2 - jt1;
+vector2 = jt2 - jt3;
+dotprod = dot(vector1, vector2, 2);
+[~, cc] = size(jt1);
+
+if cc == 3                              % 3D positions
+    len1 = sqrt(vector1(:,1).^2 + vector1(:,2).^2 + vector1(:,3).^2);
+    len2 = sqrt(vector2(:,1).^2 + vector2(:,2).^2 + vector2(:,3).^2);
+else                                    % 2D positions
+    len1 = sqrt(vector1(:,1).^2 + vector1(:,2).^2);
+    len2 = sqrt(vector2(:,1).^2 + vector2(:,2).^2);
+end
+angle = acos(dotprod ./ (len1 .* len2)) * (180 / pi);
+end
