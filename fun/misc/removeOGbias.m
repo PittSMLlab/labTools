f3ead1f7bb14a86aed5bf43674e692f7e2393219
nwb18 <nwbrantly@pitt.edu>
2026-05-23 14:34:36 -0400

    Reformat function
    Reformat function using Claude Code to align with coding style conventions for clarity and readability. Rename loop indexing variables for consistency. Update comments. Suppress MATLAB warning messages. Add section header comments. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/removeOGbias.m
+++ fun/misc/removeOGbias.m
@@ -1,52 +1,4 @@
-function newData = removeOGbias(adaptData,OGtrials,OGbaseTrials)
-labels=adaptData.data.labels;
-newData=nan(size(adaptData.data.Data));
-%seperate data based on walking direction
-baseOG1=[];
-baseOG2=[];
-baseData=adaptData.getParamInTrial(labels,OGbaseTrials);
-baseHipVel=adaptData.getParamInTrial('direction',OGbaseTrials);
-baseHipPos=adaptData.getParamInTrial('hipPos',OGbaseTrials);
-for i=1:size(baseData,1)
-    if baseHipVel(i)<0
-        baseOG1 = [baseOG1; i baseHipPos(i) baseData(i,:)];
-    else
-        baseOG2 = [baseOG2; i baseHipPos(i) baseData(i,:)];
-    end
-end
-allOG1=[];
-allOG2=[];
-[allData,inds]=adaptData.getParamInTrial(labels,OGtrials);
-allHipVel=adaptData.getParamInTrial('direction',OGtrials);
-allHipPos=adaptData.getParamInTrial('hipPos',OGtrials);
-for i=1:size(allData,1)
-    if allHipVel(i)<0
-        allOG1 = [allOG1; i allHipPos(i) allData(i,:)];
-    else
-        allOG2 = [allOG2; i allHipPos(i) allData(i,:)];
-    end
-end
-
-%Model baseline tendency and subtract from data
-baseOG1=sortrows(baseOG1,2); %ordered based on hip pos
-baseOG1Fit=bin_dataV1(baseOG1,5); %runnning average of 5 data points
-baseOG2=sortrows(baseOG2,2);
-baseOG2Fit=bin_dataV1(baseOG2,5);
-
-for i=1:size(allOG1,1)
-    [~, ind]=min(abs(baseOG1Fit(:,2)-allOG1(i,2)));%find Fit point that is closest to data point i spatially
-    bias=baseOG1Fit(ind,:);
-    allOG1(i,3:end)=allOG1(i,3:end)-bias(3:end); %do not remove bias from time index or position (hence '3:end')
-end
-for i=1:size(allOG2,1)
-    [~, ind]=min(abs(baseOG2Fit(:,2)-allOG2(i,2)));
-    bias=baseOG2Fit(ind,:);
-    allOG2(i,3:end)=allOG2(i,3:end)-bias(3:end);
-end
-newOG=sortrows([allOG1; allOG2],1); %re-order based on time points
-newData(inds,:)=newOG(:,3:end); %first two columns are just for book keeping, do not get saved.
-
-end
+function newData = removeOGbias(adaptData, OGtrials, OGbaseTrials)
 %REMOVEOGBIAS Remove spatial bias from overground gait data.
 %
 %   Orders overground baseline data by spatial position in the lab,
@@ -67,3 +19,58 @@ end
 %
 % See also ADAPTATIONDATA, BIN_DATAV1.
 
+%% Separate baseline strides by walking direction
+labels  = adaptData.data.labels;
+newData = nan(size(adaptData.data.Data));
+
+baseOG1 = [];
+baseOG2 = [];
+baseData    = adaptData.getParamInTrial(labels, OGbaseTrials);
+baseHipVel  = adaptData.getParamInTrial('direction', OGbaseTrials);
+baseHipPos  = adaptData.getParamInTrial('hipPos', OGbaseTrials);
+
+for ii = 1:size(baseData, 1)
+    if baseHipVel(ii) < 0
+        baseOG1 = [baseOG1; ii baseHipPos(ii) baseData(ii,:)]; %#ok<AGROW>
+    else
+        baseOG2 = [baseOG2; ii baseHipPos(ii) baseData(ii,:)]; %#ok<AGROW>
+    end
+end
+
+%% Separate all OG strides by walking direction
+allOG1 = [];
+allOG2 = [];
+[allData, inds] = adaptData.getParamInTrial(labels, OGtrials);
+allHipVel       = adaptData.getParamInTrial('direction', OGtrials);
+allHipPos       = adaptData.getParamInTrial('hipPos', OGtrials);
+
+for ii = 1:size(allData, 1)
+    if allHipVel(ii) < 0
+        allOG1 = [allOG1; ii allHipPos(ii) allData(ii,:)]; %#ok<AGROW>
+    else
+        allOG2 = [allOG2; ii allHipPos(ii) allData(ii,:)]; %#ok<AGROW>
+    end
+end
+
+%% Fit baseline spatial trend and subtract from data
+baseOG1     = sortrows(baseOG1, 2);         % sort by hip position
+baseOG1Fit  = bin_dataV1(baseOG1, 5);       % 5-point running average
+baseOG2     = sortrows(baseOG2, 2);
+baseOG2Fit  = bin_dataV1(baseOG2, 5);
+
+for ii = 1:size(allOG1, 1)
+    % find fit point closest to data point spatially
+    [~, ind] = min(abs(baseOG1Fit(:,2) - allOG1(ii,2)));
+    bias = baseOG1Fit(ind, :);
+    allOG1(ii, 3:end) = allOG1(ii, 3:end) - bias(3:end);
+end
+for ii = 1:size(allOG2, 1)
+    [~, ind] = min(abs(baseOG2Fit(:,2) - allOG2(ii,2)));
+    bias = baseOG2Fit(ind, :);
+    allOG2(ii, 3:end) = allOG2(ii, 3:end) - bias(3:end);
+end
+
+% re-order by time, then store (skip index and position columns)
+newOG = sortrows([allOG1; allOG2], 1);
+newData(inds, :) = newOG(:, 3:end);
+end
