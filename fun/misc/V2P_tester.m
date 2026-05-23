fcafe0eccd5fca4cf6581209c77bfc7e65172ced
nwb18 <nwbrantly@pitt.edu>
2026-05-23 14:17:00 -0400

    Reformat script
    Reformat script using Claude Code to align with coding style conventions for clarity and readability. Add section header comments. Suppress warning messages. Remove commented, unused debugging 'disp' lines. Remove obsolete, commented code lines. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/V2P_tester.m
+++ fun/misc/V2P_tester.m
@@ -6,39 +6,42 @@ clear
 clc
 close all
 
+%% Load file
 filename = uigetfile('*.*');
 
 f = fopen(filename);
 g = fgetl(f);
 fclose(f);
 
-if strcmp(g(1),'[')
-    [header,data] = JSONtxt2cell(filename);
+if strcmp(g(1), '[')
+    [header, data] = JSONtxt2cell(filename); %#ok<ASGLU>
 else
-    S = importdata(filename,',',1);
-    data = S.data;
-    Header = S.textdata;
+    S      = importdata(filename, ',', 1);
+    data   = S.data;
+    Header = S.textdata; %#ok<NASGU>
 end
 
-frame = data(:,1)-data(1,1);
-% disp(['% data received: ' num2str(length(frame)/frame(end)*100)]);
-Rz = data(:,2);
-Lz = data(:,3);
-RHS = data(:,4);
-LHS = data(:,5);
+%% Extract signals
+frame = data(:, 1) - data(1, 1);
+Rz  = data(:, 2);
+Lz  = data(:, 3);
+RHS = data(:, 4);
+LHS = data(:, 5);
 
-bframe = frame(10:end);
-% bframe = frame;
+%% Compute timing statistics
+bframe    = frame(10:end);
 framediff = diff(bframe);
-mean(framediff)
-disp(['% data received: ' num2str(length(bframe)/(bframe(end)-bframe(1))*100)]);
-timeelap = (bframe(end)-bframe(1))/100;
-freq = length(bframe)/timeelap
+mean(framediff)                 % display mean frame interval (diagnostic)
+disp(['% data received: ' ...
+    num2str(length(bframe) / (bframe(end) - bframe(1)) * 100)]);
+timeelap = (bframe(end) - bframe(1)) / 100;
+freq = length(bframe) / timeelap   % display computed frequency (diagnostic)
 
+%% Plot
 figure(1)
 plot(framediff);
 ylim([-4 6])
 
-RHS(isnan(RHS))=[];
+RHS(isnan(RHS)) = [];
 figure(2)
-plot(frame,Rz,1:length(RHS),RHS);
+plot(frame, Rz, 1:length(RHS), RHS);
