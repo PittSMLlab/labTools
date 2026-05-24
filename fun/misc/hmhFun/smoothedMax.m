aefe383ce04126e47ae36df4ab7497f295b1f5be
nwb18 <nwbrantly@pitt.edu>
2026-05-24 12:28:12 -0400

    Reformat function
    Reformat function using Claude Code to align with coding style conventions for clarity and readability. Replace 'nan*' family functions with appropriate call with 'omitnan' flag. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/hmhFun/smoothedMax.m
+++ fun/misc/hmhFun/smoothedMax.m
@@ -1,33 +1,4 @@
-function Y = smoothedMax(X,N,vector)
-if nargin>2
-    if isempty(X)
-        newX=NaN(1,size(X,2));
-        newVector=NaN;
-    elseif size(X,1)<N
-        newX=nanmean(X);
-        newVector=nanmean(vector);
-    else
-        [newX,~]=binData(X,N);
-        [newVector,~]=binData(vector,N);
-    end
-    [~,maxLoc]=max(abs(newVector),[],1);
-    Y=newX(maxLoc,:);
-else
-    if isempty(X)
-        newX=NaN(1,size(X,2));        
-    elseif size(X,1)<N
-        newX=nanmean(X,1);        
-    else
-        [newX,~]=binData(X,N);        
-    end
-    [~,maxLoc]=max(abs(newX),[],1);
-    ind=sub2ind(size(newX),maxLoc*ones(1,size(X,2)),1:size(X,2));
-    Y=newX(ind,:);
-end
-
-
-end
-
+function Y = smoothedMax(X, N, vector)
 %SMOOTHEDMAX Find the maximum of an N-point running average.
 %
 %   For vectors, smoothedMax(X, N) returns the largest value of the
@@ -53,3 +24,31 @@ end
 %
 % See also BINDATA, BIN_DATAV1.
 
+if nargin > 2
+    if isempty(X)
+        newX      = NaN(1, size(X, 2));
+        newVector = NaN;
+    elseif size(X, 1) < N
+        newX      = mean(X, 'omitnan');
+        newVector = mean(vector, 'omitnan');
+    else
+        [newX, ~]      = binData(X, N);
+        [newVector, ~] = binData(vector, N);
+    end
+    [~, maxLoc] = max(abs(newVector), [], 1);
+    Y = newX(maxLoc, :);
+else
+    if isempty(X)
+        newX = NaN(1, size(X, 2));
+    elseif size(X, 1) < N
+        newX = mean(X, 1, 'omitnan');
+    else
+        [newX, ~] = binData(X, N);
+    end
+    [~, maxLoc] = max(abs(newX), [], 1);
+    ind = sub2ind(size(newX), maxLoc * ones(1, size(X, 2)), ...
+        1:size(X, 2));
+    Y = newX(ind, :);
+end
+
+end
