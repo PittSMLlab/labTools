edac135033950b67eb4b245ec2e2eb32a12b49b3
nwb18 <nwbrantly@pitt.edu>
2026-05-24 15:41:14 -0400

    Reformat function
    Reformat function using Claude Code to align with coding style conventions for clarity and readability. Rename loop indexing variables for consistency. Suppress MATLAB warning messages. Replace 'nan*' family functions with appropriate call with 'omitnan' flag. Delete unused, commented code lines. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/hmhFun/binData.m
+++ fun/misc/hmhFun/binData.m
@@ -1,38 +1,4 @@
-function [xnew, xnewstd]=binData(x,binwidth)
-
-if ~isempty(x)
-    if binwidth==1
-        xnew=x;
-        xnewstd=zeros(size(x,1),size(x,2));
-    elseif binwidth>size(x,1)
-        ME=MException('binData:InvalidInput','binwidth is larger than x.');
-        throw(ME);
-    else
-        bstart=1:size(x,1)-(binwidth-1);
-        bend=bstart+binwidth-1;
-        if any(bend>size(x,1))
-            bend(bend>size(x,1))=size(x,1);
-            warning('The end elements of binned data may not be averages of binwidth elements')
-        end
-        
-        for i=1:length(bstart)
-            t1 = bstart(i);
-            t2 = bend(i);
-            if t2==t1; %would this ever happen?
-                xnew(i,:)=NaN*zeros(1,size(x,2));
-                xnewstd(i,:)=NaN*zeros(1,size(x,2));
-                %xnew(l,:)=x(t1,:);
-                %xnewstd(l,:)=zeros(1,size(x,2));
-            else
-                xnew(i,:) = nanmean(x(t1:t2,:));
-                xnewstd(i,:) = nanstd(x(t1:t2,:));
-            end
-        end
-    end
-else
-    xnew=[];
-    xnewstd=[];
-end
+function [xnew, xnewstd] = binData(x, binwidth)
 %BINDATA Compute a running average and standard deviation of columns.
 %
 %   Averages binwidth-element windows of the columns in x and returns
@@ -50,3 +16,39 @@ end
 % Toolbox Dependencies: None
 %
 % See also BIN_DATAV1, SMOOTHEDMAX.
+
+if ~isempty(x)
+    if binwidth == 1
+        xnew    = x;
+        xnewstd = zeros(size(x, 1), size(x, 2));
+    elseif binwidth > size(x, 1)
+        ME = MException('binData:InvalidInput', ...
+            'binwidth is larger than x.');
+        throw(ME);
+    else
+        bstart = 1:size(x, 1) - (binwidth - 1);
+        bend   = bstart + binwidth - 1;
+        if any(bend > size(x, 1))
+            bend(bend > size(x, 1)) = size(x, 1);
+            warning(['The end elements of binned data may not be ' ...
+                'averages of binwidth elements'])
+        end
+
+        for ii = 1:length(bstart)
+            t1 = bstart(ii);
+            t2 = bend(ii);
+            if t2 == t1
+                xnew(ii, :)    = NaN * zeros(1, size(x, 2)); %#ok<AGROW>
+                xnewstd(ii, :) = NaN * zeros(1, size(x, 2)); %#ok<AGROW>
+            else
+                xnew(ii, :)    = mean(x(t1:t2, :), 'omitnan'); %#ok<AGROW>
+                xnewstd(ii, :) = std(x(t1:t2, :), 0, 'omitnan'); %#ok<AGROW>
+            end
+        end
+    end
+else
+    xnew    = [];
+    xnewstd = [];
+end
+
+end
