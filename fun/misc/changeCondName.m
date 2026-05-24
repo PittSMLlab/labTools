b5d94b1d6a77b7b3bcaec716bda3848b86fae7d7
nwb18 <nwbrantly@pitt.edu>
2026-05-24 18:20:51 -0400

    Reformat function
    Reformat function using Claude Code to align with coding style conventions for clarity and readability. Remove unnecessary brackets. Update section header comments. Fix mistake of incorrect 'oldNames' reference in 'warning' message. Rename loop indexing variables for consistency. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/changeCondName.m
+++ fun/misc/changeCondName.m
@@ -1,53 +1,5 @@
-function changeCondName(subID,oldNames,newNames)
-%Check inputs.
-
-if isa(subID,'char')
-    subID={subID};
-elseif ~(isa(subID,'cell') && isa(subID{1},'char'))       
-    ME=MException('changeCondName:inputMismatch','subID needs to be a string or cell array of strings.');
-    throw(ME);
-end
-
-if isa(oldNames,'char')
-    oldNames={oldNames};
-elseif ~(isa(oldNames,'cell') && isa(oldNames{1},'char'))       
-   ME=MException('changeCondName:inputMismatch','oldNames needs to be a string or cell array of strings.');
-   throw(ME);
-end
-
-if isa(newNames,'char')
-    newNames={newNames};
-elseif ~(isa(newNames,'cell') && isa(newNames{1},'char'))       
-   ME=MException('changeCondName:inputMismatch','oldNames needs to be a string or cell array of strings.');
-   throw(ME);
-end
-
-if length(oldNames) ~= length(newNames)
-   ME=MException('changeCondName:badInput','oldNames and newNames inputs must be the same length.');
-   throw(ME);
-end
-
-for s=1:length(subID)
-    try
-        load([subID{s} 'params.mat'])
-    catch
-        ME=MException('changeCondName:badInput',['The params file for ' subID{s} ' does not appear to be in your current folder.']);
-        throw(ME);
-    end
-    
-    for c=1:length(oldNames)
-        ind=find(ismember(adaptData.metaData.conditionName,oldNames(c)));
-        if isempty(ind)
-            warning([subID{s}  '''s file does not contain condition ''' oldNames{c} ''' and was not replaced with ''' newNames{c} ''''])
-            continue
-        else
-            adaptData.metaData.conditionName{ind}=newNames{c};        
-        end
-    end
-    save([subID{s} 'params.mat'],'adaptData','-v7.3')
-end
-    
-    %CHANGECONDNAME Rename conditions in an adaptationData params file.
+function changeCondName(subID, oldNames, newNames)
+%CHANGECONDNAME Rename conditions in an adaptationData params file.
 %
 %   For each subject in SUBID, loads the corresponding params file,
 % replaces every condition name in OLDNAMES with the corresponding
@@ -67,3 +19,60 @@ end
 %
 % See also ADAPTATIONDATA.
 
+%% Validate inputs
+if isa(subID, 'char')
+    subID = {subID};
+elseif ~(isa(subID, 'cell') && isa(subID{1}, 'char'))
+    ME = MException('changeCondName:inputMismatch', ...
+        'subID needs to be a string or cell array of strings.');
+    throw(ME);
+end
+
+if isa(oldNames, 'char')
+    oldNames = {oldNames};
+elseif ~(isa(oldNames, 'cell') && isa(oldNames{1}, 'char'))
+    ME = MException('changeCondName:inputMismatch', ...
+        'oldNames needs to be a string or cell array of strings.');
+    throw(ME);
+end
+
+if isa(newNames, 'char')
+    newNames = {newNames};
+elseif ~(isa(newNames, 'cell') && isa(newNames{1}, 'char'))
+    ME = MException('changeCondName:inputMismatch', ...
+        'newNames needs to be a string or cell array of strings.');
+    throw(ME);
+end
+
+if length(oldNames) ~= length(newNames)
+    ME = MException('changeCondName:badInput', ...
+        'oldNames and newNames inputs must be the same length.');
+    throw(ME);
+end
+
+%% Rename conditions in each subject's params file
+for ii = 1:length(subID)
+    try
+        load([subID{ii} 'params.mat'])
+    catch
+        ME = MException('changeCondName:badInput', ...
+            ['The params file for ' subID{ii} ' does not appear ' ...
+            'to be in your current folder.']);
+        throw(ME);
+    end
+
+    for jj = 1:length(oldNames)
+        ind = find(ismember( ...
+            adaptData.metaData.conditionName, oldNames(jj)));
+        if isempty(ind)
+            warning([subID{ii} '''s file does not contain condition ''' ...
+                oldNames{jj} ''' and was not replaced with ''' ...
+                newNames{jj} ''''])
+            continue
+        else
+            adaptData.metaData.conditionName{ind} = newNames{jj};
+        end
+    end
+    save([subID{ii} 'params.mat'], 'adaptData', '-v7.3')
+end
+end
