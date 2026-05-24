7eeb944749ac2d0041dd1190cdc7dcaabbe6f6a1
nwb18 <nwbrantly@pitt.edu>
2026-05-24 15:30:41 -0400

    Reformat function
    Reformat function using Claude Code to align with coding style conventions for clarity and readability. Rename loop indexing variables for consistency. Remove unhelpful inline comments. Remove commented out code lines. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/hmhFun/moveParamFilesHere.m
+++ fun/misc/hmhFun/moveParamFilesHere.m
@@ -1,28 +1,5 @@
 function moveParamFilesHere(path)
-if nargin<1
-    path='./';
-end
-
-things=dir(path);
-
-for i=1:length(things)
-    if things(i).isdir
-        if ~strcmp(things(i).name(1),'.') %not a folder or file if false
-            thingsInFolder=dir([path things(i).name]);
-            for j=1:length(thingsInFolder)
-                if ~strcmp(thingsInFolder(j).name(1),'.')
-                    if strcmp(thingsInFolder(j).name(max([1 end-9]):end),'params.mat')
-                        copyfile([path things(i).name filesep thingsInFolder(j).name],'./')
-%                     elseif thingsInFolder(j).isdir
-%                         moveParamFilesHere([path things(i).name filesep])
-                    end
-                end
-            end
-        end                          
-    end
-end
-
-end%MOVEPARAMFILESHERE Copy params.mat files from subfolders to the current
+%MOVEPARAMFILESHERE Copy params.mat files from subfolders to the current
 % directory.
 %
 %   Searches all immediate subdirectories of path for files whose names
@@ -40,3 +17,28 @@ end%MOVEPARAMFILESHERE Copy params.mat files from subfolders to the current
 %
 % See also SUBFILELIST, GETSUBSFROMFOLDERS, COPYFILE.
 
+if nargin < 1
+    path = './';
+end
+
+things = dir(path);
+
+for ii = 1:length(things)
+    if things(ii).isdir
+        if ~strcmp(things(ii).name(1), '.')
+            thingsInFolder = dir([path things(ii).name]);
+            for jj = 1:length(thingsInFolder)
+                if ~strcmp(thingsInFolder(jj).name(1), '.')
+                    nameEnd = thingsInFolder(jj).name( ...
+                        max([1 end-9]):end);
+                    if strcmp(nameEnd, 'params.mat')
+                        copyfile([path things(ii).name filesep ...
+                            thingsInFolder(jj).name], './')
+                    end
+                end
+            end
+        end
+    end
+end
+
+end
