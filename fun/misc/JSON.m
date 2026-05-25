f165bc01a14fe8395fdf8e8513b73acc34bb71da
nwb18 <nwbrantly@pitt.edu>
2026-05-25 11:26:32 -0400

    Reformat function
    Reformat function using Claude Code to align with coding style conventions for clarity and readability. Add empty parentheses to make function calls with no input arguments explicit. Add inline comment. Remove unhelpful comments. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/JSON.m
+++ fun/misc/JSON.m
@@ -25,14 +25,14 @@ classdef JSON < handle
         
         function value = getValue(this)
             % get the next value in the string
-            [token,tokenType] = this.getNextToken;
+            [token, tokenType] = this.getNextToken();
             value = token;
-            
-            if strcmp(tokenType,'Special')
-                if strcmp(token,'{')
-                    value = this.getObject;
-                elseif strcmp(token,'[')
-                    value = this.getArray;
+
+            if strcmp(tokenType, 'Special')
+                if strcmp(token, '{')
+                    value = this.getObject();
+                elseif strcmp(token, '[')
+                    value = this.getArray();
                 end
             end
         end
@@ -41,26 +41,27 @@ classdef JSON < handle
             % an array is [ value, ... ]
             array = {};
             
-            value = this.getValue;
-            while ~strcmp(value,']')
+            value = this.getValue();
+            while ~strcmp(value, ']')
                 % got a value
                 array{end+1} = value; %#ok<AGROW> final size is unknowable
-                
+
                 % followed by a comma or a "]"
-                value = this.getValue;
-                
-                if strcmp(value,',')
-                    value = this.getValue;
-                elseif strcmp(value,']')
+                value = this.getValue();
+
+                if strcmp(value, ',')
+                    value = this.getValue();
+                elseif strcmp(value, ']')
                     continue
                 else
-                    error('JSON parser requires commas between array elements');
+                    error(['JSON parser requires commas between ' ...
+                        'array elements']);
                 end
             end
-            
-            % Arrays of all numbers are turned into numeric arrays
+
+            % arrays of all numbers are turned into numeric arrays
             fcn = @(x) isnumeric(x) && ~isscalar(x);
-            if all(cellfun(fcn,array))
+            if all(cellfun(fcn, array))
                 array = [array{:}];
             end
         end
@@ -68,188 +69,187 @@ classdef JSON < handle
         function obj = getObject(this)
             % an object is { string : value, ... }
             obj = struct;
-            value = this.getValue;
-            while ~strcmp(value,'}')
-                
+            value = this.getValue();
+            while ~strcmp(value, '}')
+
                 fieldname = value;
-                % make sure its a valid structure field name
-                fieldname = strrep(fieldname,':','_');
-                fieldname = strrep(fieldname,'-','_');
-                % fix for field names that start with numbers
-                % Thanks to Guy Ziv!
-                fieldname = regexprep(fieldname,'(^\d)','s$1');
-                
+                % make sure it is a valid structure field name
+                fieldname = strrep(fieldname, ':', '_');
+                fieldname = strrep(fieldname, '-', '_');
+                % fix for field names that start with numbers (Guy Ziv)
+                fieldname = regexprep(fieldname, '(^\d)', 's$1');
+
                 % check for colon
-                value = this.getValue;
-                if ~strcmp(value,':')
-                    error('JSON parser requires colons between object names and values');
+                value = this.getValue();
+                if ~strcmp(value, ':')
+                    error(['JSON parser requires colons between ' ...
+                        'object names and values']);
                 end
-                
+
                 % get the value
-                value = this.getValue;
+                value = this.getValue();
                 obj.(fieldname) = value;
-                
-                value = this.getValue;
-                if strcmp(value,',')
-                    value = this.getValue;
-                elseif strcmp(value,'}')
+
+                value = this.getValue();
+                if strcmp(value, ',')
+                    value = this.getValue();
+                elseif strcmp(value, '}')
                     continue
                 else
-                    error('JSON parser requires commas between object elements');
+                    error(['JSON parser requires commas between ' ...
+                        'object elements']);
                 end
             end
             
         end
         
-        function [token,tokenType] = getNextToken(this)
+        function [token, tokenType] = getNextToken(this)
             % get whatever is next in the string
-            
+
             % skip whitespace
             ch = this.json(this.index);
             while isWhitespace(ch)
                 this.index = this.index + 1;
                 ch = this.json(this.index);
             end
-            
+
             % is it a special character?
             if isSpecial(ch)
-                token = ch;
+                token     = ch;
                 tokenType = 'Special';
                 this.index = this.index + 1;
                 return
             end
-            
+
             % is it one of the three keywords?
-            switch(ch)
+            switch ch
                 case 't'
-                    match(this,'true');
-                    token = true;
+                    match(this, 'true');
+                    token     = true;
                     tokenType = 'Logical';
                     return;
                 case 'f'
-                    match(this,'false');
+                    match(this, 'false');
                     tokenType = 'Logical';
-                    token = false;
+                    token     = false;
                     return;
                 case 'n'
-                    match(this,'null');
+                    match(this, 'null');
                     tokenType = 'Null';
-                    token = [];
+                    token     = [];
                     return;
             end
-            
+
             % is it a string?
-            if(ch == '"')
-                token = getString(this);
+            if ch == '"'
+                token     = getString(this);
                 tokenType = 'String';
                 return;
             end
-            
-            % well, then it better be a number
-            token = getNumber(this);
+
+            % otherwise it must be a number
+            token     = getNumber(this);
             tokenType = 'Number';
-            
-            function match(this,str)
-                % find and consume exactly str at the current location of error
-                n = length(str);
+
+            function match(this, str)
+                % consume exactly str at current position or error
+                n     = length(str);
                 range = this.index:(this.index + n - 1);
                 found = this.json(range);
-                if strcmp(str,found)
+                if strcmp(str, found)
                     this.index = this.index + n;
                 else
-                    error('The JSON parser expected "%s" but found %s',str,found)
+                    error('The JSON parser expected "%s" but found %s', ...
+                        str, found)
                 end
             end
-            
+
             function tf = isWhitespace(aChar)
-                % space, carrage return, linefeed, horizontal tab
-                tf = aChar == 32 || aChar == 10 || aChar == 13 || aChar == 9;
+                % space (32), carriage return (13), linefeed (10), tab (9)
+                tf = aChar == 32 || aChar == 10 || ...
+                     aChar == 13 || aChar == 9;
             end
-            
+
             function tf = isSpecial(aChar)
-                % the special characters in the JSON "language"
-                tf = aChar == '{' || aChar == '}' || aChar == '['|| aChar == ']'|| aChar == ':' || aChar == ',';
+                % the special characters in the JSON grammar
+                tf = aChar == '{' || aChar == '}' || aChar == '[' || ...
+                     aChar == ']' || aChar == ':' || aChar == ',';
             end
-                     
+
             function string = getString(this)
                 first = this.index + 1;
-                last = first;
-                str = this.json;
-                
+                last  = first;
+                str   = this.json;
+
                 ch = str(last);
                 while ch ~= '"'
-                    if(ch == '\\') %#ok<STCMP> We KNOW both are single chars
+                    if ch == '\\' %#ok<STCMP> We KNOW both are single chars
                         last = last + 2;
                     else
                         last = last + 1;
                     end
                     ch = str(last);
                 end
-                
-                % get the string without it's quotes
-                string = str(first:(last-1));
-                this.index = last + 1; % skip the trailing "
+
+                string     = str(first:(last-1));   % string without quotes
+                this.index = last + 1;              % skip the trailing "
             end
-            
+
             function number = getNumber(this)
                 first = this.index;
-                last = first;
-                ch = charAt(this,first);
-                
-                if(ch == '-')
+                last  = first;
+                ch    = charAt(this, first);
+
+                if ch == '-'
                     last = last + 1;
-                    ch = charAt(this,last);
+                    ch   = charAt(this, last);
                 end
-                
+
                 while isDigit(ch)
                     last = last + 1;
-                    ch = charAt(this,last);
+                    ch   = charAt(this, last);
                 end
-                
-                if(ch == '.')
+
+                if ch == '.'
                     last = last + 1;
-                    ch = charAt(this,last);
+                    ch   = charAt(this, last);
                     while isDigit(ch)
                         last = last + 1;
-                        ch = charAt(this,last);
+                        ch   = charAt(this, last);
                     end
                 end
-                
+
                 if ch == 'e' || ch == 'E'
                     last = last + 1;
-                    ch = charAt(this,last);
-                    if ismember(ch,'+-')
+                    ch   = charAt(this, last);
+                    if ismember(ch, '+-')
                         last = last + 1;
-                        ch = charAt(this,last);
+                        ch   = charAt(this, last);
                     end
                     while isDigit(ch)
                         last = last + 1;
-                        ch = charAt(this,last);
+                        ch   = charAt(this, last);
                     end
                 end
-                
-                % pull out the string
-                str = this.json(first:(last-1));
+
+                str    = this.json(first:(last-1));
                 number = str2double(str);
-                
-                % move past it
                 this.index = last;
-                
-                % helper functions
-                function char = charAt(this,position)
-                    if(position > length(this.json))
+
+                function char = charAt(this, position)
+                    if position > length(this.json)
                         char = 0;
                     else
                         char = this.json(position);
                     end
                 end
-                
+
                 function tf = isDigit(aChar)
-                    tf = aChar > 47 && aChar < 58;
+                    tf = aChar > 47 && aChar < 58;  % ASCII '0'-'9'
                 end
-                
+
             end
-            
+
         end
         
     end
@@ -260,7 +260,7 @@ classdef JSON < handle
         % programmers
         function value = parse(JSONstring)
             jsonObject = JSON(JSONstring);
-            value = jsonObject.getValue;
+            value = jsonObject.getValue();
         end
     end
     
