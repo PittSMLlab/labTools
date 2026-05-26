410af17f2696dedf519a5da6926540a873a7c1b9
nwb18 <nwbrantly@pitt.edu>
2026-05-26 13:28:28 -0400

    Reformat script
    Reformat script using Claude Code to align with coding style conventions for clarity and readability. Update section header comment lines. Rename loop indexing variables for consistency. Replace calls to 'nan*' family functions with call to appropriate function with 'omitnan' flag. Define named constant variables rather than using hard-coded values throughout the script for readability. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/SL_Realtime.m
+++ fun/misc/SL_Realtime.m
@@ -43,19 +43,19 @@ for jj = 1:length(fieldList)   % parse analog channels by force/moment
                 'that does not correspond to any expected direction ' ...
                 '(x, y or z). Discarding channel ' fld])
         else
-            switch fieldList{j}(end)%parse devices
-                case '1' %Forces/moments ending in '1' area assumed to be of left treadmill belt
-                    forceLabels{end+1} = ['L',fieldList{j}(end-2:end-1)];
-                    units{end+1}=eval(['analogsInfo.units.',fieldList{j}]);
-                    relData=[relData,analogs.(fieldList{j})];
-                case '2' %Forces/moments ending in '2' area assumed to be of right treadmill belt
-                    forceLabels{end+1} = ['R',fieldList{j}(end-2:end-1)];
-                    units{end+1}=eval(['analogsInfo.units.',fieldList{j}]);
-                    relData=[relData,analogs.(fieldList{j})];
-                case '4'%Forces/moments ending in '4' area assumed to be of handrail
-                    forceLabels{end+1} = ['H',fieldList{j}(end-2:end-1)];
-                    units{end+1}=eval(['analogsInfo.units.',fieldList{j}]);
-                    relData=[relData,analogs.(fieldList{j})];
+            switch fld(end)     % parse by device number
+                case '1'        % left treadmill belt
+                    forceLabels{end+1} = ['L', fld(end-2:end-1)]; %#ok<AGROW>
+                    units{end+1} = eval(['analogsInfo.units.' fld]); %#ok<AGROW>
+                    relData = [relData, analogs.(fld)]; %#ok<AGROW>
+                case '2'        % right treadmill belt
+                    forceLabels{end+1} = ['R', fld(end-2:end-1)]; %#ok<AGROW>
+                    units{end+1} = eval(['analogsInfo.units.' fld]); %#ok<AGROW>
+                    relData = [relData, analogs.(fld)]; %#ok<AGROW>
+                case '4'        % handrail
+                    forceLabels{end+1} = ['H', fld(end-2:end-1)]; %#ok<AGROW>
+                    units{end+1} = eval(['analogsInfo.units.' fld]); %#ok<AGROW>
+                    relData = [relData, analogs.(fld)]; %#ok<AGROW>
                 otherwise
                     showWarning = true; % warn outside loop to reduce output
             end
@@ -70,28 +70,33 @@ if showWarning
 end
 
 forces = relData;
-%Get marker data
-[markers,markerInfo]=btkGetMarkers(H);
-relData=[];
-fieldList=fields(markers);
-markerList={};
-
-%Check marker labels are good in .c3d files
-mustHaveLabels={'LHIP','RHIP','LANK','RANK','RHEE','LHEE','LTOE','RTOE','RKNE','LKNE'};%we don't really care if there is RPSIS RASIS LPSIS LASIS or anything else really
-labelPresent=false(1,length(mustHaveLabels));
-for i=1:length(fieldList)
-    newFieldList{i}=findLabel(fieldList{i});
-    labelPresent=labelPresent+ismember(mustHaveLabels,newFieldList{i});
 clear analogs* relData
 
+%% Load Marker Data
+[markers, markerInfo] = btkGetMarkers(H); %#ok<ASGLU>
+relData   = [];
+fieldList = fields(markers);
+markerList = {};
+
+% verify required marker labels are present
+mustHaveLabels = {'LHIP','RHIP','LANK','RANK','RHEE','LHEE', ...
+    'LTOE','RTOE','RKNE','LKNE'};
+labelPresent = false(1, length(mustHaveLabels));
+for ii = 1:length(fieldList)
+    newFieldList{ii} = findLabel(fieldList{ii}); %#ok<AGROW>
+    labelPresent = labelPresent + ...
+        ismember(mustHaveLabels, newFieldList{ii});
 end
-for j=1:length(fieldList);
-    if length(fieldList{j})>2 && ~strcmp(fieldList{j}(1:2),'C_')  %Getting fields that do NOT start with 'C_' (they correspond to unlabeled markers in Vicon naming)
-        relData=[relData,markers.(fieldList{j})];
-        markerLabel=findLabel(fieldList{j});%make sure that the markers are always named the same after this point (ex - if left hip marker is labeled LGT, LHIP, or anyhting else it always becomes LHIP.)
-        markerList{end+1}=[markerLabel 'x'];
-        markerList{end+1}=[markerLabel 'y'];
-        markerList{end+1}=[markerLabel 'z'];
+
+for jj = 1:length(fieldList)
+    fld = fieldList{jj};
+    % skip unlabeled markers (Vicon names them with 'C_' prefix)
+    if length(fld) > 2 && ~strcmp(fld(1:2), 'C_')
+        relData = [relData, markers.(fld)]; %#ok<AGROW>
+        markerLabel = findLabel(fld);   % normalize marker name
+        markerList{end+1} = [markerLabel 'x']; %#ok<AGROW>
+        markerList{end+1} = [markerLabel 'y']; %#ok<AGROW>
+        markerList{end+1} = [markerLabel 'z']; %#ok<AGROW>
     end
     markers = rmfield(markers, fld);    % free memory
 end
@@ -131,8 +136,10 @@ RHS = find(RHS);
 LHS = find(LHS);
 
 short = min([length(RHS) length(LHS)]);
-if short<0.75*max([length(RHS) length(LHS)])%check to see if one of the legs has significantly less data than the other
-    disp(['Warning!! Missing significant # of steps. Large disparity in # of steps calculated between limbs, please verify data is accurate.']);
+stepDisparityThresh = 0.75;     % flag if short leg has <75% of other's steps
+if short < stepDisparityThresh * max([length(RHS) length(LHS)])
+    disp(['Warning!! Missing significant # of steps. Large disparity ' ...
+        'in # of steps between limbs. Please verify data.']);
 end
 
 Rgamma(1:5)=[];
@@ -172,42 +179,37 @@ elseif length(forces) / forceHz2 == length(markers) / markerHz
 else
     disp('Warning: Unknown sampling frequency in analog data!');
 end
-    
-
-time(end)=[];
-Rcadence = 60./diff(time(RHS));
-Lcadence = 60./diff(time(LHS));
-
-%remove erroneous data
 Rcadence(1:5) = [];
 Lcadence(1:5) = [];
 Rcadence(end-5:end)=[];
 Lcadence(end-5:end)=[];
-Rcadence(Rcadence<0)=[];
-Lcadence(Lcadence<0)=[];
-Rcadence(Rcadence>75)=[];
-Lcadence(Lcadence>75)=[];
 
-Rcadmean = nanmean(Rcadence);
-Lcadmean = nanmean(Lcadence);
-Rcadstd = nanstd(Rcadence);
-Lcadstd = nanstd(Lcadence);
+time(end)  = [];
+Rcadence   = 60 ./ diff(time(RHS));
+Lcadence   = 60 ./ diff(time(LHS));
 
-%*****************************************
-%Step times
-HS = [RHS; LHS];
-HS = sort(HS);
+Rcadence(Rcadence < 0)  = [];
+Lcadence(Lcadence < 0)  = [];
+maxCadence = 75;                % discard obviously erroneous cadence (steps/min)
+Rcadence(Rcadence > maxCadence) = [];
+Lcadence(Lcadence > maxCadence) = [];
 
-[~,rind,~] = intersect(HS,RHS);
-[~,lind,~] = intersect(HS,LHS);
+Rcadmean = mean(Rcadence, 'omitnan');
+Lcadmean = mean(Lcadence, 'omitnan');
+Rcadstd  = std(Rcadence, 0, 'omitnan');
+Lcadstd  = std(Lcadence, 0, 'omitnan');
 
-%check to make sure events are spliced in the correct alternating order
-%???
+%% Step Times
+HS = sort([RHS; LHS]);
 
-if HS(1) == RHS(1)%first event is RHS
-    for z = 1:length(HS)-2
-        Lsteptime(z) = time(HS(z+1))-time(HS(z));
-        Rsteptime(z) = time(HS(z+2))-time(HS(z+1));
+[~, rind, ~] = intersect(HS, RHS); %#ok<ASGLU>
+[~, lind, ~] = intersect(HS, LHS); %#ok<ASGLU>
+
+edgeTrimST = 4;                 % strides to remove from step-time ends
+if HS(1) == RHS(1)              % first event is RHS
+    for ii = 1:length(HS)-2
+        Lsteptime(ii) = time(HS(ii+1)) - time(HS(ii));   %#ok<AGROW>
+        Rsteptime(ii) = time(HS(ii+2)) - time(HS(ii+1)); %#ok<AGROW>
     end
 else
     for ii = 1:length(HS)-2
@@ -267,28 +269,25 @@ disp(mesg)
 
 %% Save Data
 try
-RTdata = struct();%initialize save structure
-RTdata.trialname = filename;
-RTdata.path = path;
-RTdata.creationdate = clock;
-RTdata.forcedata = forces;
-RTdata.forcelabels = forceLabels;
-RTdata.markerdata = markers;
-RTdata.markerlabels = markerList;
-RTdata.Rsteplengths = Rgamma;
-RTdata.Lsteplengths = Lgamma;
-RTdata.Rcadence = Rcadence;
-RTdata.Lcadence = Lcadence;
-RTdata.Rsteptime = Rsteptime;
-RTdata.Lsteptime = Lsteptime;
-RTdata.time = time;
+    RTdata             = struct();
+    RTdata.trialname   = filename;
+    RTdata.path        = path;
+    RTdata.creationdate = clock;
+    RTdata.forcedata   = forces;
+    RTdata.forcelabels = forceLabels;
+    RTdata.markerdata  = markers;
+    RTdata.markerlabels = markerList;
+    RTdata.Rsteplengths = Rgamma;
+    RTdata.Lsteplengths = Lgamma;
+    RTdata.Rcadence    = Rcadence;
+    RTdata.Lcadence    = Lcadence;
+    RTdata.Rsteptime   = Rsteptime;
+    RTdata.Lsteptime   = Lsteptime;
+    RTdata.time        = time;
 
-fn = strrep(datestr(clock),'-','');
-
-filesave = [path fn(1:9) '_' filename(1:end-4) '_SL_Realtime'];
-
-save(filesave,'RTdata');
-
+    fn       = strrep(datestr(clock), '-', '');
+    filesave = [path fn(1:9) '_' filename(1:end-4) '_SL_Realtime'];
+    save(filesave, 'RTdata');
 catch ME
     throw(ME)
 end
