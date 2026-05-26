91662fcabe0d2e500c1eafc443000da4705f6e57
nwb18 <nwbrantly@pitt.edu>
2026-05-26 14:02:09 -0400

    Reformat script
    Reformat script using Claude Code to align with coding style conventions for clarity and readability. Rename loop indexing variables for consistency. Delete unhelpful inline comments. Update section header comment lines. Replace calls to 'nan*' family functions with call to appropriate function with 'omitnan' flag. Delete obsolete, commented out code lines. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/misc/SL_RealtimeAlpha.m
+++ fun/misc/SL_RealtimeAlpha.m
@@ -20,10 +20,6 @@ vicon = ViconNexus();
 [path, filename] = vicon.GetTrialName;  % ask Nexus which trial is open
 filename = [filename '.c3d'];
 
-%use these two lines when processing c3d files not open in Nexus
-% commandwindow()
-% [filename,path] = uigetfile('*.c3d','Please select the c3d file of interest:');
-
 H = btkReadAcquisition([path filename]);
 
 % use same method as labTools to determine analog data
@@ -90,14 +86,16 @@ for ii = 1:length(fieldList)
     labelPresent = labelPresent + ...
         ismember(mustHaveLabels, newFieldList{ii});
 end
-for j=1:length(fieldList);
-    if length(fieldList{j})>2 && ~strcmp(fieldList{j}(1:2),'C_')  %Getting fields that do NOT start with 'C_' (they correspond to unlabeled markers in Vicon naming)
-        relData=[relData,markers.(fieldList{j})];
-        markerLabel=findLabel(fieldList{j});%make sure that the markers are always named the same after this point (ex - if left hip marker is labeled LGT, LHIP, or anyhting else it always becomes LHIP.)
-        markerList{end+1}=[markerLabel 'x'];
-        markerList{end+1}=[markerLabel 'y'];
-        markerList{end+1}=[markerLabel 'z'];
 
+for jj = 1:length(fieldList)
+    fld = fieldList{jj};
+    % skip unlabeled markers (Vicon names them with 'C_' prefix)
+    if length(fld) > 2 && ~strcmp(fld(1:2), 'C_')
+        relData = [relData, markers.(fld)]; %#ok<AGROW>
+        markerLabel = findLabel(fld);       % normalize marker name
+        markerList{end+1} = [markerLabel 'x']; %#ok<AGROW>
+        markerList{end+1} = [markerLabel 'y']; %#ok<AGROW>
+        markerList{end+1} = [markerLabel 'z']; %#ok<AGROW>
     end
     markers = rmfield(markers, fld);        % free memory
 end
@@ -114,11 +112,14 @@ clear H
 
 %% Compute Parameters
 
-if length(forces)/1000 == length(markers)/100
-    markers1000 = interp1(markers,[1:1/10:length(markers)]);
-elseif length(forces)/2000 == length(markers)/100
-    markers1000 = interp1(markers,[1:1/20:length(markers)]);
 % upsample markers to match force plate sampling rate
+markerHz = 100;                 % marker capture rate (Hz)
+forceHz1 = 1000;                % possible force plate rate 1 (Hz)
+forceHz2 = 2000;                % possible force plate rate 2 (Hz)
+if length(forces) / forceHz1 == length(markers) / markerHz
+    markers1000 = interp1(markers, 1:1/10:length(markers));
+elseif length(forces) / forceHz2 == length(markers) / markerHz
+    markers1000 = interp1(markers, 1:1/20:length(markers));
 else
     disp('Warning: Unknown sampling frequency in analog data!');
 end
@@ -138,87 +139,81 @@ RHS = find(RHS);
 LHS = find(LHS);
 
 short = min([length(RHS) length(LHS)]);
-if short<0.75*max([length(RHS) length(LHS)])%check to see if one of the legs has significantly less data than the other
-    disp(['Warning!! Missing significant # of steps. Large disparity in # of steps calculated between limbs, please verify data is accurate.']);
+stepDisparityThresh = 0.75;     % flag if short leg has <75% of other's steps
+if short < stepDisparityThresh * max([length(RHS) length(LHS)])
+    disp(['Warning!! Missing significant # of steps. Large disparity ' ...
+        'in # of steps between limbs. Please verify data.']);
 end
 
-%step lengths*********************************************
-Rgamma = LANKY(RHS(1:short))-RANKY(RHS(1:short));
-Lgamma = RANKY(LHS(1:short))-LANKY(LHS(1:short));
-
-%remove erroneous data
-Rgamma(Rgamma==0)=[];
-Lgamma(Lgamma==0)=[];
-Rgamma(Rgamma<0)=[];
-Lgamma(Lgamma<0)=[];
 Rgamma(1:5)=[];
 Lgamma(1:5)=[];
 Rgamma(end-5:end)=[];
 Lgamma(end-5:end)=[];
+%% Step Lengths
+Rgamma = LANKY(RHS(1:short)) - RANKY(RHS(1:short));
+Lgamma = RANKY(LHS(1:short)) - LANKY(LHS(1:short));
 
-Rgammamean = nanmean(Rgamma);
-Rgammastd = nanstd(Rgamma);
-Lgammamean = nanmean(Lgamma);
-Lgammastd = nanstd(Lgamma);
+Rgamma(Rgamma == 0) = [];
+Lgamma(Lgamma == 0) = [];
+Rgamma(Rgamma < 0)  = [];
+Lgamma(Lgamma < 0)  = [];
+
+Rgammamean = mean(Rgamma, 'omitnan');
+Rgammastd  = std(Rgamma, 0, 'omitnan');
+Lgammamean = mean(Lgamma, 'omitnan');
+Lgammastd  = std(Lgamma, 0, 'omitnan');
 
 figure(2)
-subplot(6,1,1)
-plot(Rgamma,'Color',[195/255,4/255,4/255]);
+subplot(6, 1, 1)
+plot(Rgamma, 'Color', [195/255, 4/255, 4/255]);
 title('Right Leg Step Lengths (mm)');
-subplot(6,1,2)
-plot(Lgamma,'Color',[15/255,129/255,6/255]);
+subplot(6, 1, 2)
+plot(Lgamma, 'Color', [15/255, 129/255, 6/255]);
 title('Left Leg Step Lengths (mm)');
 
-%alpha *********************************************
-Ralpha = ((LHIPY(RHS(1:short))+RHIPY(RHS(1:short)))./2)-RANKY(RHS(1:short)); 
-Lalpha = ((LHIPY(LHS(1:short))+RHIPY(LHS(1:short)))./2)-LANKY(LHS(1:short));
-
-%remove erroneous data
-Ralpha(Ralpha==0)=[];
-Lalpha(Lalpha==0)=[];
-Ralpha(Ralpha<0)=[];
-Lalpha(Lalpha<0)=[];
 Ralpha(1:5)=[];
 Lalpha(1:5)=[];
 Ralpha(end-5:end)=[];
 Lalpha(end-5:end)=[];
-
-Ralphamean = nanmean(Ralpha);
-Ralphastd = nanstd(Ralpha);
-Lalphamean = nanmean(Lalpha);
-Lalphastd = nanstd(Lalpha);
-
-%X *********************************************
-RX = ((LHIPY(LHS(1:short))+RHIPY(LHS(1:short)))./2)-RANKY(LHS(1:short)); 
-LX = ((LHIPY(RHS(1:short))+RHIPY(RHS(1:short)))./2)-LANKY(RHS(1:short)); 
-
-
-%remove erroneous data
-RX(RX==0)=[];
-LX(LX==0)=[];
-RX(RX>0)=[];
-LX(LX>0)=[];
 RX(1:5)=[];
 LX(1:5)=[];
 RX(end-5:end)=[];
 LX(end-5:end)=[];
-
-RXmean = nanmean(RX);
-RXstd = nanstd(RX);
-LXmean = nanmean(LX);
-LXstd = nanstd(LX);
-% 
-% figure(2)
-% subplot(6,1,1)
-% plot(Rgamma,'Color',[195/255,4/255,4/255]);
-% title('Right Leg Step Lengths (mm)');
-% subplot(6,1,2)
-% plot(Lgamma,'Color',[15/255,129/255,6/255]);
-% title('Left Leg Step Lengths (mm)');
-
-%Cadence**********************************************
 if length(forces)/1000 == length(markers)/100
     duration = length(RANKY)/1000;
+%% Alpha (hip COM to leading ankle at heel strike)
+Ralpha = ((LHIPY(RHS(1:short)) + RHIPY(RHS(1:short))) ./ 2) ...
+    - RANKY(RHS(1:short));
+Lalpha = ((LHIPY(LHS(1:short)) + RHIPY(LHS(1:short))) ./ 2) ...
+    - LANKY(LHS(1:short));
+
+Ralpha(Ralpha == 0) = [];
+Lalpha(Lalpha == 0) = [];
+Ralpha(Ralpha < 0)  = [];
+Lalpha(Lalpha < 0)  = [];
+
+Ralphamean = mean(Ralpha, 'omitnan');
+Ralphastd  = std(Ralpha, 0, 'omitnan');
+Lalphamean = mean(Lalpha, 'omitnan');
+Lalphastd  = std(Lalpha, 0, 'omitnan');
+
+%% X (cross-limb hip COM to trailing ankle at heel strike)
+RX = ((LHIPY(LHS(1:short)) + RHIPY(LHS(1:short))) ./ 2) ...
+    - RANKY(LHS(1:short));
+LX = ((LHIPY(RHS(1:short)) + RHIPY(RHS(1:short))) ./ 2) ...
+    - LANKY(RHS(1:short));
+
+RX(RX == 0) = [];
+LX(LX == 0) = [];
+RX(RX > 0)  = [];
+LX(LX > 0)  = [];
+
+RXmean = mean(RX, 'omitnan');
+RXstd  = std(RX, 0, 'omitnan');
+LXmean = mean(LX, 'omitnan');
+LXstd  = std(LX, 0, 'omitnan');
+
+%% Cadence
     time = 0:0.001:duration;
 elseif length(forces)/2000 == length(markers)/100
     duration = length(RANKY)/2000;
@@ -226,156 +221,129 @@ elseif length(forces)/2000 == length(markers)/100
 else
     disp('Warning: Unknown sampling frequency in analog data!');
 end
-    
-
-time(end)=[];
-Rcadence = 60./diff(time(RHS));
-Lcadence = 60./diff(time(LHS));
-
-%remove erroneous data
 Rcadence(1:5) = [];
 Lcadence(1:5) = [];
 Rcadence(end-5:end)=[];
 Lcadence(end-5:end)=[];
-Rcadence(Rcadence<0)=[];
-Lcadence(Lcadence<0)=[];
-Rcadence(Rcadence>75)=[];
-Lcadence(Lcadence>75)=[];
 
-Rcadmean = nanmean(Rcadence);
-Lcadmean = nanmean(Lcadence);
-Rcadstd = nanstd(Rcadence);
-Lcadstd = nanstd(Lcadence);
+time(end)  = [];
+Rcadence   = 60 ./ diff(time(RHS));
+Lcadence   = 60 ./ diff(time(LHS));
 
-%*****************************************
-%Step times
-HS = [RHS; LHS];
-HS = sort(HS);
+Rcadence(Rcadence < 0) = [];
+Lcadence(Lcadence < 0) = [];
+maxCadence = 75;                % discard obviously erroneous cadence (steps/min)
+Rcadence(Rcadence > maxCadence) = [];
+Lcadence(Lcadence > maxCadence) = [];
 
-[~,rind,~] = intersect(HS,RHS);
-[~,lind,~] = intersect(HS,LHS);
+Rcadmean = mean(Rcadence, 'omitnan');
+Lcadmean = mean(Lcadence, 'omitnan');
+Rcadstd  = std(Rcadence, 0, 'omitnan');
+Lcadstd  = std(Lcadence, 0, 'omitnan');
 
-%check to make sure events are spliced in the correct alternating order
-%???
+%% Step Times
+HS = sort([RHS; LHS]);
 
-if HS(1) == RHS(1)%first event is RHS
-    for z = 1:length(HS)-2
-        Lsteptime(z) = time(HS(z+1))-time(HS(z));
-        Rsteptime(z) = time(HS(z+2))-time(HS(z+1));
+[~, rind, ~] = intersect(HS, RHS); %#ok<ASGLU>
+[~, lind, ~] = intersect(HS, LHS); %#ok<ASGLU>
+
+if HS(1) == RHS(1)              % first event is RHS
+    for ii = 1:length(HS)-2
+        Lsteptime(ii) = time(HS(ii+1)) - time(HS(ii));   %#ok<AGROW>
+        Rsteptime(ii) = time(HS(ii+2)) - time(HS(ii+1)); %#ok<AGROW>
     end
 else
-    for z = 1:length(HS)-2
-        Rsteptime(z) = time(HS(z+1))-time(HS(z));
-        Lsteptime(z) = time(HS(z+2))-time(HS(z+1));
+    for ii = 1:length(HS)-2
+        Rsteptime(ii) = time(HS(ii+1)) - time(HS(ii));   %#ok<AGROW>
+        Lsteptime(ii) = time(HS(ii+2)) - time(HS(ii+1)); %#ok<AGROW>
     end
 end
 
-Rsteptime(Rsteptime<=0)=[];
-Lsteptime(Lsteptime<=0)=[];
 Rsteptime(1:4) = [];
 Lsteptime(1:4) = [];
 Rsteptime(end-4:end)=[];
 Lsteptime(end-4:end)=[];
+Rsteptime(Rsteptime <= 0)     = [];
+Lsteptime(Lsteptime <= 0)     = [];
 
-rstmean = nanmean(Rsteptime);
-lstmean = nanmean(Lsteptime);
-rststd = nanstd(Rsteptime);
-lststd = nanstd(Lsteptime);
+rstmean = mean(Rsteptime, 'omitnan');
+lstmean = mean(Lsteptime, 'omitnan');
+rststd  = std(Rsteptime, 0, 'omitnan');
+lststd  = std(Lsteptime, 0, 'omitnan');
 
 figure(2)
-subplot(6,1,3)
-plot(Rcadence,'.-','Color',[195/255,4/255,4/255]);
+subplot(6, 1, 3)
+plot(Rcadence, '.-', 'Color', [195/255, 4/255, 4/255]);
 title('Right Leg Cadence (steps/min)');
-subplot(6,1,4)
-plot(Lcadence,'.-','Color',[15/255,129/255,6/255]);
+subplot(6, 1, 4)
+plot(Lcadence, '.-', 'Color', [15/255, 129/255, 6/255]);
 title('Left Leg Cadence (steps/min)');
-subplot(6,1,5)
-plot(Rsteptime,'Color',[195/255,4/255,4/255]);
+subplot(6, 1, 5)
+plot(Rsteptime, 'Color', [195/255, 4/255, 4/255]);
 title('Right Leg Step Time (s)');
 ylim([0 1]);
-subplot(6,1,6)
-plot(Lsteptime,'Color',[15/255,129/255,6/255]);
+subplot(6, 1, 6)
+plot(Lsteptime, 'Color', [15/255, 129/255, 6/255]);
 title('Left Leg Step Time (s)');
 ylim([0 1]);
 
-%report the data
+%% Report Results
 mesg = 'Mean R Step Length: ';
 mesg = [mesg num2str(Rgammamean) ' stdev: ' num2str(Rgammastd)];
 mesg = [mesg sprintf('\n')];
-mesg = [mesg 'Mean L Step Length: ' num2str(Lgammamean) ' stdev: ' num2str(Lgammastd)];
+mesg = [mesg 'Mean L Step Length: ' ...
+    num2str(Lgammamean) ' stdev: ' num2str(Lgammastd)];
 mesg = [mesg sprintf('\n\n')];
-mesg = [mesg 'Mean R Cadence: ' num2str(Rcadmean) ' stdev: ' num2str(Rcadstd)];
+mesg = [mesg 'Mean R Cadence: ' ...
+    num2str(Rcadmean) ' stdev: ' num2str(Rcadstd)];
 mesg = [mesg sprintf('\n')];
-mesg = [mesg 'Mean L Cadence: ' num2str(Lcadmean) ' stdev: ' num2str(Lcadstd)];
+mesg = [mesg 'Mean L Cadence: ' ...
+    num2str(Lcadmean) ' stdev: ' num2str(Lcadstd)];
 mesg = [mesg sprintf('\n\n')];
-mesg = [mesg 'Mean R Step Time: ' num2str(rstmean) ' stedev: ' num2str(rststd)];
+mesg = [mesg 'Mean R Step Time: ' ...
+    num2str(rstmean) ' stdev: ' num2str(rststd)];
 mesg = [mesg sprintf('\n')];
-mesg = [mesg 'Mean L Step Time: ' num2str(lstmean) ' stedev: ' num2str(lststd)];
-
+mesg = [mesg 'Mean L Step Time: ' ...
+    num2str(lstmean) ' stdev: ' num2str(lststd)];
 mesg = [mesg sprintf('\n\n')];
-mesg = [mesg 'Mean R alpha: ' num2str(Ralphamean) ' stdev: ' num2str(Ralphastd)];
+mesg = [mesg 'Mean R alpha: ' ...
+    num2str(Ralphamean) ' stdev: ' num2str(Ralphastd)];
 mesg = [mesg sprintf('\n')];
-mesg = [mesg 'Mean L alpha: ' num2str(Lalphamean) ' stdev: ' num2str(Lalphastd)];
-
+mesg = [mesg 'Mean L alpha: ' ...
+    num2str(Lalphamean) ' stdev: ' num2str(Lalphastd)];
 mesg = [mesg sprintf('\n\n')];
 mesg = [mesg 'Mean R X: ' num2str(RXmean) ' stdev: ' num2str(RXstd)];
 mesg = [mesg sprintf('\n')];
 mesg = [mesg 'Mean L X: ' num2str(LXmean) ' stdev: ' num2str(LXstd)];
-
 mesg = [mesg sprintf('\n\n')];
-mesg = [mesg 'Mean R R: ' num2str(RXmean/Ralphamean)];
+mesg = [mesg 'Mean R R: ' num2str(RXmean / Ralphamean)];
 mesg = [mesg sprintf('\n')];
-mesg = [mesg 'Mean L R: ' num2str(LXmean/Lalphamean)];
+mesg = [mesg 'Mean L R: ' num2str(LXmean / Lalphamean)];
 
-
-
-H = msgbox(mesg,'Metrics');
-
+H = msgbox(mesg, 'Metrics');    %#ok<NASGU>
 disp(mesg)
 
-%% Save data
-
+%% Save Data
 try
-RTdata = struct();%initialize save structure
-RTdata.trialname = filename;
-RTdata.path = path;
-RTdata.creationdate = clock;
-RTdata.forcedata = forces;
-RTdata.forcelabels = forceLabels;
-RTdata.markerdata = markers;
-RTdata.markerlabels = markerList;
-RTdata.Rsteplengths = Rgamma;
-RTdata.Lsteplengths = Lgamma;
-RTdata.Rcadence = Rcadence;
-RTdata.Lcadence = Lcadence;
-RTdata.Rsteptime = Rsteptime;
-RTdata.Lsteptime = Lsteptime;
-RTdata.time = time;
+    RTdata              = struct();
+    RTdata.trialname    = filename;
+    RTdata.path         = path;
+    RTdata.creationdate = clock;
+    RTdata.forcedata    = forces;
+    RTdata.forcelabels  = forceLabels;
+    RTdata.markerdata   = markers;
+    RTdata.markerlabels = markerList;
+    RTdata.Rsteplengths = Rgamma;
+    RTdata.Lsteplengths = Lgamma;
+    RTdata.Rcadence     = Rcadence;
+    RTdata.Lcadence     = Lcadence;
+    RTdata.Rsteptime    = Rsteptime;
+    RTdata.Lsteptime    = Lsteptime;
+    RTdata.time         = time;
 
-fn = strrep(datestr(clock),'-','');
-
-filesave = [path fn(1:9) '_' filename(1:end-4) '_SL_Realtime'];
-
-save(filesave,'RTdata');
-
+    fn       = strrep(datestr(clock), '-', '');
+    filesave = [path fn(1:9) '_' filename(1:end-4) '_SL_Realtime'];
+    save(filesave, 'RTdata');
 catch ME
-    
     throw(ME)
-    
 end
-
-
-% clear all
-
-
-
-
-
-
-
-
-
-
-
-
-
