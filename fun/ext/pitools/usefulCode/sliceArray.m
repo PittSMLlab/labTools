aaed39dc81692d16fe71f69e03d537696bad9c56
nwb18 <nwbrantly@pitt.edu>
2026-05-26 21:00:49 -0400

    Reformat function
    Reformat function using Claude Code for style consistency and readability. Remove unnecessary brackets around single output argument. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/ext/pitools/usefulCode/sliceArray.m
+++ fun/ext/pitools/usefulCode/sliceArray.m
@@ -1,4 +1,4 @@
-function [slices] = sliceArray(data,inds,dim)
+function slices = sliceArray(data, inds, dim)
 %SLICEARRAY Extract slices of an array along a given dimension.
 %
 %   Returns the subset of slices indexed by inds along dimension dim.
@@ -17,16 +17,15 @@ function [slices] = sliceArray(data,inds,dim)
 %
 % See also PERMUTE, SQUEEZE.
 
-nd=ndims(data);
-if dim>nd
+nd = ndims(data);
+if dim > nd
     error('')
 end
-N=size(data,dim);
-if any(inds>N | inds<1)
+N = size(data, dim);
+if any(inds > N | inds < 1)
     error()
 end
-prefix=repmat(':,',1,dim-1);
-suffix=repmat(',:',1,nd-dim);
-eval(['slices=data(' prefix 'inds' suffix ');'])
+prefix = repmat(':,', 1, dim - 1);
+suffix = repmat(',:', 1, nd - dim);
+eval(['slices = data(' prefix 'inds' suffix ');'])
 end
-
