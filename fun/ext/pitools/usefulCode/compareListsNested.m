e8487c63d0aafdcdb5b94c31f13647d19985bf10
nwb18 <nwbrantly@pitt.edu>
2026-05-26 21:34:06 -0400

    Reformat function
    Reformat function using Claude Code for style consistency and readability. Remove unnecessary brackets around single output argument. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/ext/pitools/usefulCode/compareListsNested.m
+++ fun/ext/pitools/usefulCode/compareListsNested.m
@@ -1,4 +1,4 @@
-function [bool,idxs] = compareListsNested(list1,list2)
+function [bool, idxs] = compareListsNested(list1, list2)
 %COMPARELISTSNESTED Search list2 strings against list1 (supports nesting).
 %
 %   For each element of list2, checks whether it matches any string in
@@ -21,34 +21,34 @@ function [bool,idxs] = compareListsNested(list1,list2)
 %
 % See also COMPARELISTS, STRCMP.
 
-if isa(list2,'char')
-    list2={list2};
+if isa(list2, 'char')
+    list2 = {list2};
 end
-if ~isa(list2,'cell') || ~all(cellfun(@(x) isa(x,'char'),list2))
+if ~isa(list2, 'cell') || ~all(cellfun(@(x) isa(x, 'char'), list2))
     error('List2 has to be a cell array containing strings.');
 end
-if all(cellfun(@(x) isa(x,'char'),list1)) %Shortcut for when list1 and list2 are both cells of strings:
-    [bool,idxs] = compareListsFast(list1,list2);
+
+% Shortcut for when list1 and list2 are both cells of strings:
+if all(cellfun(@(x) isa(x, 'char'), list1))
+    [bool, idxs] = compareListsFast(list1, list2);
 else
-%TO DO: make this more efficient by running as the first part (above this line) for all the
-%chars in list1, and only doing the second part (from this line on) for the non-chars in list1
-%[currently we do the first part only if they are ALL chars, and if not we
-%go ahead element by element, which is inefficient]
-idxs=nan(size(list2));
-    bool=false(size(list2));
-    for i=1:length(list1)
-        if isa(list1{i},'cell')
-            [aux,~] = compareListsNested(list1{i},list2);
-        elseif isa(list1{i},'char')
-            aux=strcmp(list1{i},list2); %Compares the full list2 to one element of list1
+    % TODO: make this more efficient by running the fast path for all
+    % chars in list1 and only the element-by-element path for non-chars.
+    % Currently we use the fast path only when ALL entries are chars.
+    idxs = nan(size(list2));
+    bool = false(size(list2));
+    for ii = 1:length(list1)
+        if isa(list1{ii}, 'cell')
+            [aux, ~] = compareListsNested(list1{ii}, list2);
+        elseif isa(list1{ii}, 'char')
+            aux = strcmp(list1{ii}, list2);
         else
-            error('List1 has to be a cell array containing strings or nested cell-arrays of strings');
+            error(['List1 has to be a cell array containing strings ' ...
+                'or nested cell-arrays of strings']);
         end
-        idxs(aux)=i;
-        bool=bool | aux;
+        idxs(aux) = ii;
+        bool = bool | aux;
     end
 end
 
-
 end
-
