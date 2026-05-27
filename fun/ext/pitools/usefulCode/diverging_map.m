55172e333580d7bac65258f58ba5c353c80eb929
nwb18 <nwbrantly@pitt.edu>
2026-05-27 14:35:58 -0400

    Reformat function
    Reformat function using Claude Code for style consistency and readability. Remove unnecessary brackets around single output argument. Remove unnecessary parentheses. Make comment block more concise. NOTE that there are no functional code changes in this commit, only aesthetic changes. NWB

--- fun/ext/pitools/usefulCode/diverging_map.m
+++ fun/ext/pitools/usefulCode/diverging_map.m
@@ -230,38 +230,34 @@ if r < 0, r = 0; end
 if g < 0, g = 0; end
 if b < 0, b = 0; end
 
-%-----------------------------------------------------------------------------
-function[xyz] = RGBToXYZ(rgb)
-
-  r = rgb(1); g = rgb(2); b = rgb(3);
-
-  % The following performs a "gamma correction" specified by the sRGB color
-  % space.  sRGB is defined by a canonical definition of a display monitor and
-  % has been standardized by the International Electrotechnical Commission (IEC
-  % 61966-2-1).  The nonlinearity of the correction is designed to make the
-  % colors more perceptually uniform.  This color space has been adopted by
-  % several applications including Adobe Photoshop and Microsoft Windows color
-  % management.  OpenGL is agnostic on its RGB color space, but it is reasonable
-  % to assume it is close to this one.
-  if ( r > 0.04045 ), r = (( r + 0.055 ) / 1.055)^2.4;
-  else                r = r / 12.92; end
-  if ( g > 0.04045 ), g = (( g + 0.055 ) / 1.055)^2.4;
-  else                g = g / 12.92; end
-  if ( b > 0.04045 ), b = (( b + 0.055 ) / 1.055)^2.4;
-  else                b = b / 12.92; end
-
-  %Observer. = 2 deg, Illuminant = D65
-  x = r * 0.4124 + g * 0.3576 + b * 0.1805;
-  y = r * 0.2126 + g * 0.7152 + b * 0.0722;
-  z = r * 0.0193 + g * 0.1192 + b * 0.9505;
-  
-  xyz = [x y z];
 rgb = [r g b];
 end
 
 function[rgb] = LabToRGB(Lab)
   xyz = LabToXYZ(Lab);
   rgb = XYZToRGB(xyz);
+% ---------------------------------------------------------------------------
+function xyz = RGBToXYZ(rgb)
+%RGBTOXYZ Convert sRGB to XYZ with inverse gamma correction.
+
+r = rgb(1);
+g = rgb(2);
+b = rgb(3);
+
+% Inverse sRGB gamma correction (IEC 61966-2-1).
+if r > 0.04045, r = ((r + 0.055) / 1.055)^2.4;
+else            r = r / 12.92; end
+if g > 0.04045, g = ((g + 0.055) / 1.055)^2.4;
+else            g = g / 12.92; end
+if b > 0.04045, b = ((b + 0.055) / 1.055)^2.4;
+else            b = b / 12.92; end
+
+% Observer = 2 deg, Illuminant = D65
+x = r * 0.4124 + g * 0.3576 + b * 0.1805;
+y = r * 0.2126 + g * 0.7152 + b * 0.0722;
+z = r * 0.0193 + g * 0.1192 + b * 0.9505;
+
+xyz = [x y z];
 end
 
 function[Lab] = RGBToLab(rgb)
